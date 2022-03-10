5cea2398b9dfd78a541b82563f3e2a438417b504
danylott <d.tuytuyshkin@gmail.com>
2022-03-10 21:49:31 +0200

    Removed `app` folder & upgraded tests

--- db/tests/test_main.py
+++ db/tests/test_main.py
@@ -1,6 +1,6 @@
 import pytest
 
-from app.main import main, Race, Skill, Player, Guild
+from main import main, Race, Skill, Player, Guild
 
 
 @pytest.mark.django_db
@@ -38,33 +38,27 @@ def test_races():
         ("elf", "The magic race"),
         ("human", "Human race"),
     ]
-    assert list(
-        Race.objects.get(name="elf").skill_set.values_list("name", "bonus")
-    ) == [
-        (
-            "Teleportation",
-            "The ability to move so fast they look like they're teleporting. "
-            "Could be considered to technically be Teleportation.",
-        ),
-        (
-            "Reality Warping",
-            "The ability to Warp Reality. Make the impossible become possible "
-            "but can't warp anything containing the structure that holds "
-            "everything together (Which are many creatures.)",
-        ),
+    assert list(Race.objects.get(name="elf").skill_set.values_list("name")) == [
+        ("Teleportation",),
+        ("Reality Warping",),
     ]
-    assert list(
-        Race.objects.get(name="human").skill_set.values_list("name", "bonus")
-    ) == []
+    assert (
+        list(Race.objects.get(name="human").skill_set.values_list("name", "bonus"))
+        == []
+    )
 
 
 @pytest.mark.django_db
 def test_players():
     main()
-    assert list(Player.objects.values_list("nickname", "email", "bio")) == [
-        ("john", "john@gmail.com", "Hello, I'm John, elf ranger"),
-        ("max", "max@gmail.com", "Hello, I'm Max, elf mag"),
-        ("arthur", "arthur@gmail.com", "Arthur, elf mag"),
-        ("andrew", "andrew@gmail.com", "Hello, I'm Andrew"),
-        ("nick", "nick@gmail.com", "Hello, I'm Nick"),
+    assert list(
+        Player.objects.values_list(
+            "nickname", "email", "bio", "race__name", "guild__name"
+        )
+    ) == [
+        ("john", "john@gmail.com", "Hello, I'm John, elf ranger", "elf", "archers"),
+        ("max", "max@gmail.com", "Hello, I'm Max, elf mag", "elf", "mags"),
+        ("arthur", "arthur@gmail.com", "Arthur, elf mag", "elf", "mags"),
+        ("andrew", "andrew@gmail.com", "Hello, I'm Andrew", "human", "blacksmiths"),
+        ("nick", "nick@gmail.com", "Hello, I'm Nick", "human", None),
     ]
---
+++ main.py
@@ -0,0 +1,11 @@
+import init_django_orm  # noqa: F401
+
+from db.models import Race, Skill, Player, Guild
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
