6b5467f9c65d933c784e193588f3a4dae9fc43d9
Taras Nester <tarasnester414@gmail.com>
2022-06-17 09:55:45 +0300

    changed create methods

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ def main():
     players_data = get_player_data("players.json")
 
     for player_name, player_data in players_data.items():
-        race = create_and_get_race(player_data["race"])
+        race = create_race(player_data["race"])
 
         create_skills(race, player_data["race"]["skills"])
 
         if player_data["guild"]:
-            guild = create_and_get_guild(player_data["guild"])
+            guild = create_guild(player_data["guild"])
         else:
             guild = None
 
@@ -25,14 +25,11 @@ def main():
         )
 
 
-def create_and_get_race(race_data: dict) -> Race:
-    if not Race.objects.filter(name=race_data["name"]).exists():
-        race = Race.objects.create(
-            name=race_data["name"],
-            description=race_data["description"]
-        )
-    else:
-        race = Race.objects.get(name=race_data["name"])
+def create_race(race_data: dict) -> Race:
+    race, _ = Race.objects.get_or_create(
+        name=race_data["name"],
+        description=race_data["description"]
+    )
 
     return race
 
@@ -47,15 +44,11 @@ def create_skills(race: Race, skills_list: list):
             )
 
 
-def create_and_get_guild(guild_data: dict) -> Guild:
-    if not Guild.objects.filter(
-            name=guild_data["name"]).exists():
-        guild = Guild.objects.create(
-            name=guild_data["name"],
-            description=guild_data["description"]
-        )
-    else:
-        guild = Guild.objects.get(name=guild_data["name"])
+def create_guild(guild_data: dict) -> Guild:
+    guild, _ = Guild.objects.get_or_create(
+        name=guild_data["name"],
+        description=guild_data["description"]
+    )
 
     return guild
 
