68e2c6d9452fe344fcf7e755c7ab202a36df85b7
Serhiy Zhyhadlo <sergeyz.dev1996@gmail.com>
2022-06-20 21:12:41 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,35 @@
 from django.db import models
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name}, {self.description}"
+
+
+class Skill(models.Model):
+    name = models.CharField(unique=True, max_length=255)
+    bonus = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}, {self.bonus}, {self.race}"
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+
+    def __str__(self):
+        return f"{self.name}, {self.description}"
+
+
+class Player(models.Model):
+    nickname = models.CharField(unique=True, max_length=255)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    guild = models.ForeignKey(Guild, on_delete=models.CASCADE, null=True)
+    created_at = models.DateField(auto_now=True)
--- main.py
+++ main.py
@@ -1,10 +1,60 @@
 import init_django_orm  # noqa: F401
-
+import json
 from db.models import Race, Skill, Player, Guild
 
 
 def main():
-    pass
+    with open('players.json', 'r') as json_input:
+        players = json.load(json_input)
+    guilds = {}
+    # Guild Creation
+    for player in players:
+        player_dict = players[player]
+        if player_dict["guild"]:
+            if player_dict["guild"]["description"]:
+                description = player_dict["guild"]["description"]
+            else:
+                description = None
+            name = player_dict["guild"]["name"]
+            if Guild.objects.filter(name=name).exists() is False:
+                creation = \
+                    Guild.objects.create(name=name, description=description)
+                guilds[name] = creation.id
+    races = {}
+    # Race creation
+    for player in players:
+        player_dict = players[player]
+        name = player_dict["race"]["name"]
+        description = player_dict["race"]["description"]
+        if Race.objects.filter(name=name).exists() is False:
+            creation = Race.objects.create(name=name, description=description)
+            races[name] = creation.id
+    # Skills creation
+    for player in players:
+        player_dict = players[player]
+        skills = player_dict["race"]["skills"]
+        race_name = player_dict["race"]["name"]
+        for skill in skills:
+            skill_name = skill["name"]
+            bonus = skill["bonus"]
+            if Skill.objects.filter(name=skill_name).exists() is False:
+                Skill.objects.create(name=skill_name,
+                                     bonus=bonus, race_id=races[race_name])
+    # Players creation
+    for player in players:
+        player_dict = players[player]
+        nickname = player
+        email = player_dict["email"]
+        bio = player_dict["bio"]
+        race_name = player_dict["race"]["name"]
+        race = races[race_name]
+        if player_dict["guild"]:
+            guild_name = player_dict["guild"]["name"]
+            guild = guilds[guild_name]
+        else:
+            guild = None
+        Player.objects.create(nickname=nickname, email=email,
+                              bio=bio, race_id=race, guild_id=guild)
 
 
 if __name__ == "__main__":
