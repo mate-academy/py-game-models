7c6c1f8c055b52de2978e10c4447d915e3849508
Denis Pidlubniy <denispodlubnyj@gmail.com>
2022-08-15 23:10:06 +0300

    Solution

--- main.py
+++ main.py
@@ -1,11 +1,52 @@
 import init_django_orm  # noqa: F401
+import json
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main():
-    pass
+    with open("players.json") as file:
+        players_data = json.load(file)
+
+        for user_name, value in players_data.items():
+            print("key:", user_name, "value:", value)
+            if not Player.objects.filter(nickname=user_name).exists():
+                temp_race = None
+                if not Race.objects.filter(
+                        name=value['race']['name']).exists():
+                    temp_race = Race.objects.create(
+                        name=value['race']['name'],
+                        description=value['race']['description'])
+                else:
+                    temp_race = Race.objects.get(
+                        name=value['race']['name'])
+
+                for skill in value['race']['skills']:
+                    if not Skill.objects.filter(
+                            name=skill['name']).exists():
+                        Skill.objects.create(name=skill['name'],
+                                             bonus=skill['bonus'],
+                                             race=temp_race)
+
+                if value['guild'] is not None \
+                        and not Guild.objects.filter(
+                            name=value['guild']['name']).exists():
+                    temp_guild = Guild.objects.create(
+                        name=value['guild']['name'],
+                        description=value['guild']['description'])
+
+                Player.objects.create(nickname=user_name,
+                                      email=value['email'],
+                                      bio=value['bio'],
+                                      race=temp_race,
+                                      guild=temp_guild
+                                      if value['guild'] is not None
+                                      else None)
 
 
 if __name__ == "__main__":
+    Skill.objects.all().delete()
+    Race.objects.all().delete()
+    Guild.objects.all().delete()
+    Player.objects.all().delete()
     main()
