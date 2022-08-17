4bfb4417e88cacd36b42888d86fb0db7066108a3
Oleksii Bodnarchuk <olbody12@gmail.com>
2022-08-17 19:52:37 +0300

    solution

--- db/models.py
+++ db/models.py
@@ -22,5 +22,6 @@ class Player(models.Model):
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
-    guild = models.ForeignKey(Guild, on_delete=models.CASCADE, null=True, blank=True)
+    guild = models.ForeignKey(
+        Guild, on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now=True)
