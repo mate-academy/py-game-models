e8f847ec76d44621871eac618f3c4c0549cf7b38
stanislavbublinsky <bublinsky.s.m@gmail.com>
2022-10-06 19:11:38 +0300

    created models for a game

--- main.py
+++ main.py
@@ -1,10 +1,77 @@
+import json
+
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
-def main():
-    pass
+def main() -> None:
+    with open("players.json", "r") as file:
+        players = json.load(file)
+
+    for player_name in players:
+        player_data = players[player_name]
+
+        race = create_race(player_data["race"])
+        create_skill_from_list(player_data["race"]["skills"], race)
+
+        if player_data["guild"] is None:
+            guild = None
+        else:
+            guild = create_guild(player_data["guild"])
+
+        create_player(
+            player_name,
+            player_data,
+            race,
+            guild
+        )
+
+
+def create_player(
+        player_name: str,
+        player: dict,
+        race: int,
+        guild: int,
+) -> None:
+    if len(Player.objects.filter(nickname=player_name)) == 0:
+        Player.objects.create(
+            nickname=player_name,
+            email=player["email"],
+            bio=player["bio"],
+            race_id=race,
+            guild_id=guild
+        )
+
+
+def create_guild(guild: dict) -> int:
+    if len(Guild.objects.filter(name=guild["name"])) == 0:
+        Guild.objects.create(
+            name=guild["name"],
+            description=guild["description"]
+        )
+
+    return Guild.objects.get(name=guild["name"]).id
+
+
+def create_race(race: dict) -> int:
+    if len(Race.objects.filter(name=race["name"])) == 0:
+        Race.objects.create(
+            name=race["name"],
+            description=race["description"]
+        )
+
+    return Race.objects.get(name=race["name"]).id
+
+
+def create_skill_from_list(skills: list, race: int) -> None:
+    for skill in skills:
+        if len(Skill.objects.filter(name=skill["name"])) == 0:
+            Skill.objects.create(
+                name=skill["name"],
+                bonus=skill["bonus"],
+                race_id=race
+            )
 
 
 if __name__ == "__main__":
