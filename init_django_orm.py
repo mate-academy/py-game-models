d9cb66945364ef00a2362a213f6379ba55681751
danylott <d.tuytuyshkin@gmail.com>
2022-10-07 13:27:58 +0300

    Added flake8 plugins

--- init_django_orm.py
+++ init_django_orm.py
@@ -3,5 +3,5 @@ import os
 import django
 
 sys.dont_write_bytecode = True
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 django.setup()
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import init_django_orm  # noqa: F401
 from db.models import Race, Skill, Player, Guild
 
 
-def main():
+def main() -> None:
     pass
 
 
