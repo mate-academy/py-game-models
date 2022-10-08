2e432121d24b199bdc08cbd98d9015ad2644f0bb
Davyd Kryvko <davidkrivko@gmail.com>
2022-10-08 17:38:34 +0200

    'Solution'

--- main.py
+++ main.py
@@ -1,10 +1,54 @@
 import init_django_orm  # noqa: F401
+import json
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r") as file_in:
+        data = json.load(file_in)
+    for player, info in data.items():
+        for key, value in info.items():
+            if key == "race":
+                if not Race.objects.filter(name=value["name"]).exists():
+                    Race.objects.create(
+                        name=value["name"],
+                        description=value["description"],
+                    )
+                if value["skills"] is not None:
+                    for skill in value["skills"]:
+                        i_ = Skill.objects.filter(name=skill["name"]).exists()
+                        if not i_:
+                            Skill.objects.create(name=skill["name"],
+                                                 bonus=skill["bonus"])
+            if key == "guild" and value is not None:
+                if not Guild.objects.filter(name=value["name"]).exists():
+                    if value["description"] is None:
+                        Guild.objects.create(
+                            name=value["name"],
+                            description="",
+                        )
+                    Guild.objects.create(
+                        name=value["name"],
+                        description=value["description"],
+                    )
+    for name, player in data.items():
+        if player["guild"] is None:
+            Player.objects.create(
+                nickname=name,
+                email=player["email"],
+                bio=player["bio"],
+                race_id=Race.objects.get(name=player["race"]["name"]).id,
+                guild_id=None,
+            )
+        else:
+            Player.objects.create(
+                nickname=name,
+                email=player["email"],
+                bio=player["bio"],
+                race_id=Race.objects.get(name=player["race"]["name"]).id,
+                guild_id=Guild.objects.get(name=player["guild"]["name"]).id,
+            )
 
 
 if __name__ == "__main__":
