2440dc94912337b89d33f8d64bd7806810b34488
Alexander <alexzubkoff123@gmail.com>
2022-10-19 14:34:29 +0300

    Created models with migrations with diff fields params

--- db/models.py
+++ db/models.py
@@ -1,1 +1,35 @@
 from django.db import models
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> None:
+        return "%s %s" % (self.name, self.description)
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, null=True)
+
+    def __str__(self) -> None:
+        return "%s %s %s" % (self.name, self.bonus, self.race)
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> None:
+        return "%s %s" % (self.name, self.description)
+
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, null=True)
+    guild = models.ForeignKey(Guild, on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(auto_now=True)
--- main.py
+++ main.py
@@ -1,10 +1,52 @@
+import json
+from datetime import datetime
 import init_django_orm  # noqa: F401
-
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json") as json_file:
+        players_data = json.load(json_file)
+
+    for nickname, player_data in players_data.items():
+        if player_data["race"]:
+            if not Race.objects.filter(
+                name=player_data["race"]["name"]
+            ).exists():
+                Race.objects.create(
+                    name=player_data["race"]["name"],
+                    description=player_data["race"]["description"],
+                )
+            race = Race.objects.get(name=player_data["race"]["name"])
+            if player_data["race"]["skills"]:
+                for skill in player_data["race"]["skills"]:
+                    if not Skill.objects.filter(name=skill["name"]).exists():
+                        Skill.objects.create(
+                            name=skill["name"],
+                            bonus=skill["bonus"],
+                            race=race
+                        )
+        if player_data["guild"]:
+            if not Guild.objects.filter(
+                name=player_data["guild"]["name"]
+            ).exists():
+                Guild.objects.create(
+                    name=player_data["guild"]["name"],
+                    description=player_data["guild"]["description"],
+                )
+            guild = Guild.objects.get(name=player_data["guild"]["name"])
+        else:
+            guild = None
+
+        if not Player.objects.filter(nickname=nickname).exists():
+            Player.objects.create(
+                nickname=nickname,
+                email=player_data["email"],
+                bio=player_data["bio"],
+                race=race,
+                guild=guild,
+                created_at=datetime.now()
+            )
 
 
 if __name__ == "__main__":
