7c603662009d2fc4dba6b793bf871d6424eb1a08
Katrin Lazarenko <katrin903@gmail.com>
2022-10-20 15:01:03 +0300

    Deleted null from Race, on_delete changed to PROTECTED

--- db/models.py
+++ db/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    description = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True)
 
 
 class Skill(models.Model):
@@ -21,6 +21,6 @@ class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.SET_NULL, null=True)
-    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
+    race = models.ForeignKey(Race, on_delete=models.PROTECT)
+    guild = models.ForeignKey(Guild, on_delete=models.PROTECT, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
