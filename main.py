1b711810ffceb5ead9b423378f241966f3ead479
Illia Artnaiev <ilya.artnaev@gmail.com>
2022-10-20 13:54:32 +0300

    Solution

--- main.py
+++ main.py
@@ -1,10 +1,50 @@
-import init_django_orm  # noqa: F401
+import json
 
+import init_django_orm  # noqa: F401
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r") as file:
+        data = json.load(file)
+
+    for player_name, player_info in data.items():
+
+        guild = player_info["guild"]
+        race = player_info["race"]
+        skills = player_info["race"]["skills"]
+
+        if guild:
+            if not Guild.objects.filter(name=guild["name"]).exists():
+                Guild.objects.create(
+                    name=guild["name"],
+                    description=guild["description"]
+                )
+        guild_in_db = Guild.objects.get(name=guild["name"]) if guild else None
+
+        if not Race.objects.filter(name=race["name"]).exists():
+            Race.objects.create(
+                name=race["name"],
+                description=race["description"]
+            )
+        race_in_db = Race.objects.get(name=race["name"])
+
+        for skill in skills:
+            if not Skill.objects.filter(name=skill["name"]).exists():
+                Skill.objects.create(
+                    name=skill["name"],
+                    bonus=skill["bonus"],
+                    race=race_in_db
+                )
+
+        if not Player.objects.filter(nickname=player_name).exists():
+            Player.objects.create(
+                nickname=player_name,
+                email=player_info["email"],
+                bio=player_info["bio"],
+                race=race_in_db,
+                guild=guild_in_db
+            )
 
 
 if __name__ == "__main__":
