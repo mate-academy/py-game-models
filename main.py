b8f47aec7af20cdc9bffff1300245d18a6e865a6
Zakhar Podporinov <zp162162@gmail.com>
2022-10-22 01:32:35 +0300

    Correct temporary variable <player_race>

--- main.py
+++ main.py
@@ -10,18 +10,16 @@ def main() -> None:
         players = json.load(file)
 
     for player_name, player_data in players.items():
-
-        player_race_name = player_data["race"]["name"]
-        player_race_description = player_data["race"]["description"]
+        player_race = Race.race_import(
+            race_name=player_data["race"]["name"],
+            race_description=player_data["race"]["description"]
+        )
 
         for skill in player_data["race"]["skills"]:
             Skill.skill_import(
                 skill_name=skill["name"],
                 skill_bonus=skill["bonus"],
-                skill_race=Race.race_import(
-                    race_name=player_race_name,
-                    race_description=player_race_description
-                )
+                skill_race=player_race
             )
 
         if player_data["guild"]:
@@ -36,10 +34,7 @@ def main() -> None:
             nickname=player_name,
             email=player_data["email"],
             bio=player_data["bio"],
-            race=Race.race_import(
-                race_name=player_race_name,
-                race_description=player_race_description
-            ),
+            race=player_race,
             guild=player_guild
         )
 
