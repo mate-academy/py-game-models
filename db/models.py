f73f6d623a38c5d5b1ce90c474c702111adb8040
Ivan Antonishak <jus1stored@gmail.com>
2022-10-21 19:55:41 +0300

    Fix fields

--- db/models.py
+++ db/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    description = models.TextField(null=True)
+    description = models.TextField(blank=True, default="")
 
 
 class Skill(models.Model):
@@ -22,5 +22,5 @@ class Player(models.Model):
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
-    guild = models.ForeignKey(Guild, on_delete=models.CASCADE, null=True)
+    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
