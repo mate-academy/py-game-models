d15849c70f74aa80c77735e807c296da2b1673cc
Zakhar Podporinov <zp162162@gmail.com>
2022-10-22 00:35:38 +0300

    Solution!

--- db/models.py
+++ db/models.py
@@ -1,1 +1,89 @@
+from __future__ import annotations
+
 from django.db import models
+
+
+class Race(models.Model):
+    RACE_CHOICE = (
+        ("Elf", "Elf"),
+        ("Dwarf", "Dwarf"),
+        ("Human", "Human"),
+        ("Ork", "Ork")
+    )
+
+    name = models.CharField(
+        max_length=255,
+        choices=RACE_CHOICE,
+        unique=True
+    )
+    description = models.TextField(blank=True, null=True)
+
+    @classmethod
+    def race_import(
+            cls,
+            race_name: str,
+            race_description: str
+    ) -> Race:
+
+        if cls.objects.filter(name=race_name).exists():
+            return cls.objects.get(name=race_name)
+
+        return cls.objects.create(name=race_name,
+                                  description=race_description)
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+
+    @classmethod
+    def skill_import(
+            cls,
+            skill_name: str,
+            skill_bonus: str,
+            skill_race: Race
+    ) -> Skill:
+
+        if cls.objects.filter(name=skill_name).exists():
+            return cls.objects.get(name=skill_name)
+
+        return cls.objects.create(
+            name=skill_name,
+            bonus=skill_bonus,
+            race=skill_race
+        )
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    @classmethod
+    def guild_import(
+            cls,
+            guild_name: str,
+            guild_description: str
+    ) -> Guild:
+
+        if cls.objects.filter(name=guild_name).exists():
+            return cls.objects.get(name=guild_name)
+
+        return cls.objects.create(
+            name=guild_name,
+            description=guild_description
+        )
+
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    bio = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    guild = models.ForeignKey(
+        Guild,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="players"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
--- main.py
+++ main.py
@@ -1,10 +1,43 @@
+import json
+
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json") as file:
+        players = json.load(file)
+
+    for player_name, player_data in players.items():
+        for skill in player_data["race"]["skills"]:
+            Skill.skill_import(
+                skill_name=skill["name"],
+                skill_bonus=skill["bonus"],
+                skill_race=Race.race_import(
+                    race_name=player_data["race"]["name"],
+                    race_description=player_data["race"]["description"]
+                )
+            )
+
+        if player_data["guild"]:
+            player_guild = Guild.guild_import(
+                guild_name=player_data["guild"]["name"],
+                guild_description=player_data["guild"]["description"]
+            )
+        else:
+            player_guild = None
+
+        Player.objects.create(
+            nickname=player_name,
+            email=player_data["email"],
+            bio=player_data["bio"],
+            race=Race.race_import(
+                race_name=player_data["race"]["name"],
+                race_description=player_data["race"]["description"]
+            ),
+            guild=player_guild
+        )
 
 
 if __name__ == "__main__":
