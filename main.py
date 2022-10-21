504c3fda8bfa5647f65fdedba9651e5a47f78b39
vadymsabadyr <Sabadyrvv94@gmail.com>
2022-10-21 11:43:38 +0300

    Refactoring

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
 import init_django_orm  # noqa: F401
 
-import json
+from json import load
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
     with open("players.json") as file:
-        gamers = json.load(file)
+        gamers = load(file)
 
-    for key, value in gamers.items():
-        races = value["race"]
+    for gamer_nickname, gamer_data in gamers.items():
+        races = gamer_data["race"]
         skills = races["skills"]
-        guilds = value["guild"]
+        guilds = gamer_data["guild"]
 
         if not Race.objects.filter(name=races["name"]).exists():
             Race.objects.create(
                 name=races["name"],
                 description=races["description"]
             ),
-        race_ = Race.objects.get(name=races["name"])
+        race = Race.objects.get(name=races["name"])
 
         for skill in skills:
             if not Skill.objects.filter(name=skill["name"]).exists():
                 Skill.objects.create(
                     name=skill["name"],
                     bonus=skill["bonus"],
-                    race=race_
+                    race=race
 
                 ),
         if guilds:
@@ -39,13 +39,12 @@ def main() -> None:
         else:
             guild = None
 
-        if not Player.objects.filter(nickname=key).exists():
-            Player.objects.create(
-                nickname=key,
-                email=value["email"],
-                bio=value["bio"],
-                race=race_,
-                guild=guild
+        Player.objects.create(
+            nickname=gamer_nickname,
+            email=gamer_data["email"],
+            bio=gamer_data["bio"],
+            race=race,
+            guild=guild
             )
 
 
