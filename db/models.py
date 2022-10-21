ca9d7a0bad11ed4a0acc2720fee9e08e6344fb33
Oleksandr Murynskyi <a.murinskiy@gmail.com>
2022-10-21 13:24:00 +0300

    Change on_delete

--- db/models.py
+++ db/models.py
@@ -23,6 +23,6 @@ class Player(models.Model):
     bio = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
     guild = models.ForeignKey(
-        Guild, on_delete=models.CASCADE, null=True
+        Guild, on_delete=models.SET_NULL, null=True
     )
     created_at = models.DateTimeField(auto_now_add=True)
