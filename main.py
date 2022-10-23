aaadcd271d598955c3933ff13980cf4a7bc8d1b5
Pavlo Kapitanets <aixpavlik"gmail.com>
2022-10-23 20:29:23 +0300

    Solution

--- main.py
+++ main.py
@@ -14,20 +14,16 @@ def main() -> None:
             user_skills = information["race"]["skills"]
             user_guild = information["guild"]
 
-            if not Race.objects.filter(name=user_race["name"]).exists():
-                Race.objects.create(
-                    name=user_race["name"],
-                    description=user_race["description"]
-                )
-                race_id = Race.objects.get(name=user_race["name"])
+            race_of_user, _ = Race.objects.get_or_create(
+                name=user_race["name"],
+                description=user_race["description"]
+            )
 
             if user_guild:
-                if not Guild.objects.filter(name=user_guild["name"]).exists():
-                    Guild.objects.create(
-                        name=user_guild["name"],
-                        description=user_guild["description"]
-                    )
-                guild_id = Guild.objects.get(name=user_guild["name"])
+                guild_id, _ = Guild.objects.get_or_create(
+                    name=user_guild["name"],
+                    description=user_guild["description"]
+                )
             else:
                 guild_id = None
 
@@ -36,7 +32,7 @@ def main() -> None:
                     Skill.objects.create(
                         name=skill["name"],
                         bonus=skill["bonus"],
-                        race=race_id
+                        race=race_of_user
                     )
 
             Player.objects.create(
@@ -44,7 +40,7 @@ def main() -> None:
                 email=information["email"],
                 bio=information["bio"],
                 guild=guild_id,
-                race=race_id
+                race=race_of_user
             )
 
 
