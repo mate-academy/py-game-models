80d1b95af0cb71947f677a6c4441cf2f2c067118
Taras Andruschenko <And999110@gmail.com>
2022-10-23 22:31:18 +0300

    Fix on_delete statements and remove unnecessary description

--- db/models.py
+++ db/models.py
@@ -9,11 +9,9 @@ class Race(models.Model):
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(
-        "This field describes what kind of bonus "
-        "players can get from it",
         max_length=255
     )
-    race = models.ForeignKey(Race, on_delete=models.DO_NOTHING)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
 
 
 class Guild(models.Model):
@@ -25,10 +23,8 @@ class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255)
     bio = models.CharField(
-        "It stores a short description "
-        "provided by a user about himself/herself",
         max_length=255
     )
-    race = models.ForeignKey(Race, on_delete=models.DO_NOTHING)
-    guild = models.ForeignKey(Guild, on_delete=models.DO_NOTHING, null=True)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
