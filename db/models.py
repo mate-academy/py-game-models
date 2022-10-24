26619a91db4410059ad813526b1ccd3acecfb9ce
Yevhenii Davydenko <akazhulich@gmail.com>
2022-10-24 16:25:35 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,47 @@
-from django.db import models
+from django.db import models as m
+
+
+class Race(m.Model):
+    races = (
+        ("E", "Elf"),
+        ("D", "Dwarf"),
+        ("H", "Human"),
+        ("O", "Ork")
+    )
+
+    name = m.CharField(max_length=255, unique=True, choices=races)
+    description = m.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return f"The race is:{self.name}"
+
+
+class Skill(m.Model):
+    name = m.CharField(max_length=255, unique=True)
+    bonus = m.CharField(max_length=255)
+    race = m.ForeignKey(Race, on_delete=m.PROTECT)
+
+    def __str__(self) -> str:
+        return f"Skill: {self.name}, belongs to {self.race.name}"
+
+
+class Guild(m.Model):
+    name = m.CharField(max_length=255, unique=True)
+    description = m.TextField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return f"Guild {self.name}!"
+
+
+class Player(m.Model):
+    nickname = m.CharField(max_length=255, unique=True)
+    email = m.EmailField(max_length=255)
+    bio = m.CharField(max_length=255)
+    race = m.ForeignKey(Race, on_delete=m.PROTECT)
+    guild = m.ForeignKey(Guild, on_delete=m.PROTECT, null=True)
+    create_at = m.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return (f"Name: {self.nickname}, "
+                f"race: {self.race.name}, "
+                f"guild: {self.guild.name}")
--- main.py
+++ main.py
@@ -1,10 +1,53 @@
 import init_django_orm  # noqa: F401
-
+import json
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r") as f:
+        date = json.loads(f.read())
+
+    def create_race(date: dict) -> Race:
+        date = date.get("race")
+        if not Race.objects.filter(name=date.get("name")).exists():
+            Race.objects.create(
+                name=date.get("name"),
+                description=date.get("description")
+            )
+        return Race.objects.get(name=date.get("name"))
+
+    def create_skill(date: dict) -> None:
+        for skill in date.get("race").get("skills"):
+            if not Skill.objects.filter(name=skill.get("name")).exists():
+                Skill.objects.create(
+                    name=skill.get("name"),
+                    bonus=skill.get("bonus"),
+                    race=create_race(date)
+                )
+
+    def create_guild(date: dict) -> Guild | None:
+        if date.get("guild"):
+            date = date.get("guild")
+            if not Guild.objects.filter(name=date.get("name")).exists():
+                Guild.objects.create(
+                    name=date.get("name"),
+                    description=date.get("description")
+                )
+            return Guild.objects.get(name=date.get("name"))
+        return None
+
+    for name, info in date.items():
+        race = create_race(info)
+        guild = create_guild(info)
+        create_skill(info)
+
+        Player.objects.create(
+            nickname=name,
+            email=info.get("email"),
+            bio=info.get("bio"),
+            race=race,
+            guild=guild
+        )
 
 
 if __name__ == "__main__":
