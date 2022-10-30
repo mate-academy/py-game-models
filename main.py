ffcc83313e36860bf454f9b28af3d702e999c593
Maks Danylchuk <maxitosm00@gmail.com>
2022-10-30 03:37:54 +0200

    rdy

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
 import init_django_orm  # noqa: F401
 
+import json
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r") as file:
+        players_data = json.load(file)
+
+    for player_nickname, info in players_data.items():
+        if not Race.objects.filter(name=info["race"]["name"]).exists():
+            Race.objects.create(
+                name=info["race"]["name"],
+                description=info["race"]["description"]
+            )
+        race = Race.objects.get(name=info["race"]["name"])
+
+        for skill in info["race"]["skills"]:
+            if not Skill.objects.filter(name=skill["name"]).exists():
+                Skill.objects.create(
+                    name=skill["name"],
+                    bonus=skill["bonus"],
+                    race=race
+                )
+
+        if info["guild"]:
+            if not Guild.objects.filter(name=info["guild"]["name"]).exists():
+                Guild.objects.create(
+                    name=info["guild"]["name"],
+                    description=info["guild"]["description"]
+                )
+            guild = Guild.objects.get(name=info["guild"]["name"])
+        else:
+            guild = None
+
+        if not Player.objects.filter(nickname=player_nickname).exists():
+            Player.objects.create(
+                nickname=player_nickname,
+                email=info["email"],
+                bio=info["bio"],
+                race=race,
+                guild=guild
+            )
 
 
 if __name__ == "__main__":
