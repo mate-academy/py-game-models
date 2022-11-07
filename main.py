484c425d85914ecfadbdb5ca5d100643739daa86
IvanGLS <gladiolus0707@gmail.com>
2022-11-07 17:26:43 +0200

    'check'

--- main.py
+++ main.py
@@ -21,36 +21,40 @@ def main() -> None:
             race = Race_class(
                 name=data.get(checker).get("race").get("name"),
                 description=data.get(checker).get("race").get("description"))
-            if Race.objects.filter(name=race.name).exists() is False:
+            if not Race.objects.filter(name=race.name).exists():
                 race_hero = Race.objects.create(name=race.name,
                                                 description=race.description)
             skills = data.get(checker).get("race").get("skills")
             skills_update(skills, race_hero)
-            if data.get(checker).get("guild") is not None:
-                descript = data.get(checker).get("guild").get("description")
-                # object class Guild from guild.py
-                guild = Guild_class(
-                    description=descript,
-                    name=data.get(checker).get("guild").get("name"))
+            if not data.get(checker).get("guild") is None:
+                guild_name = guild_update(data, checker)
             if data.get(checker).get("guild") is None:
                 guild_name = None
-            if Guild.objects.filter(name=guild.name).exists() is False:
-                guild_name = Guild.objects.create(
-                    name=guild.name,
-                    description=guild.description)
             # object class Player from player.py
             player = Player_class(nickname=checker,
                                   email=data.get(checker).get("email"),
                                   bio=data.get(checker).get("bio"),
                                   race=race_hero,
                                   guild=guild_name)
-            if Player.objects.filter(nickname=checker).exists() is False:
+            if not Player.objects.filter(nickname=checker).exists():
                 Player.objects.create(
                     nickname=player.nickname,
                     email=player.email,
                     bio=player.bio,
                     race=player.race,
                     guild=player.guild)
+
+
+def guild_update(players_data, player_check) -> [str, None]:
+    descript = players_data.get(player_check).get("guild").get("description")
+    # object class Guild from guild.py
+    guild = Guild_class(
+        description=descript,
+        name=players_data.get(player_check).get("guild").get("name"))
+    if not Guild.objects.filter(name=guild.name).exists():
+        return Guild.objects.create(
+            name=guild.name,
+            description=guild.description)
 
 
 def skills_update(skills: list, race_hero: callable) -> None:
@@ -60,8 +64,8 @@ def skills_update(skills: list, race_hero: callable) -> None:
             name=skill.get("name"),
             bonus=skill.get("bonus"),
             race=race_hero)
-        if Skill.objects.filter(
-                name=hero_skill.name).exists() is False:
+        if not Skill.objects.filter(
+                name=hero_skill.name).exists():
             Skill.objects.create(
                 name=hero_skill.name,
                 bonus=hero_skill.bonus,
