b37044d793b6905dff0f73d6b9cf7f7f75ef4397
Bohdan2001007 <bogdan1991007@gmail.com>
2022-11-11 22:52:24 +0100

    Solution

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    with open('players.json') as f:
+    with open("players.json") as f:
         all_info = json.load(f)
 
     for key, value in all_info.items():
         race_info = value["race"]
         if not Race.objects.filter(name=race_info["name"]).exists():
-            race = Race.objects.create(name=race_info["name"], description=race_info["description"])
+            race = Race.objects.create(name=race_info["name"],
+                                       description=race_info["description"])
         else:
             race = Race.objects.get(name=race_info["name"])
 
@@ -24,13 +25,19 @@ def main() -> None:
         guild_info = value["guild"]
         guild = None
         if guild_info:
-            if not Guild.objects.filter(name=guild_info["name"]).exists():
-                guild = Guild.objects.create(name=guild_info["name"], description=guild_info["description"])
+            if not Guild.objects.filter(
+                    name=guild_info["name"]).exists():
+                guild = Guild.objects.create(
+                    name=guild_info["name"],
+                    description=guild_info["description"])
             else:
                 guild = Guild.objects.get(name=guild_info["name"])
 
-        if not Player.objects.filter(nickname=key).exists():
-            Player.objects.create(nickname=key, email=value["email"], bio=value["bio"], race=race,
+        if not Player.objects.filter(
+                nickname=key).exists():
+            Player.objects.create(nickname=key,
+                                  email=value["email"], bio=value["bio"],
+                                  race=race,
                                   guild=guild)
 
 
