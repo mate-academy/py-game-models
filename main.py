37e9bff5b2bbd7a2feea5da3d1de767514403523
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2022-11-19 20:48:58 +0200

    Solution №2

--- main.py
+++ main.py
@@ -7,45 +7,52 @@ import json
 def main() -> None:
     with open("players.json") as players:
         data = json.load(players)
-        list_race = []
-        for name, info in data.items():
-            if info["race"]["name"] not in list_race:
-                list_race.append(info["race"]["name"])
-                Race.objects.create(
-                    name=info["race"]["name"],
-                    description=info["race"]["description"]
-                )
-
-        skills = list(data.values())[1]["race"]["skills"]
-        for skill in skills:
-            Skill.objects.create(
-                name=skill["name"],
-                bonus=skill["bonus"],
-                race=Race.objects.get(name="elf")
+    list_of_race = []
+    for name, info in data.items():
+        name_of_race = info["race"]["name"]
+        if name_of_race not in list_of_race:
+            list_of_race.append(name_of_race)
+            Race.objects.create(
+                name=name_of_race,
+                description=info["race"]["description"]
             )
 
-        list_guils = []
-        for name, info in data.items():
-            if isinstance(info["guild"], dict) and\
-                    info["guild"]["name"] not in list_guils:
-                list_guils.append(info["guild"]["name"])
-                Guild.objects.create(
-                    name=info["guild"]["name"],
-                    description=info["guild"]["description"]
-                )
-        for name, info in data.items():
-            if info["guild"] is not None:
-                Player.objects.create(
-                    nickname=name,
-                    email=info["email"],
-                    bio=info["bio"],
-                    race=Race.objects.get(name=info["race"]["name"]),
-                    guild=Guild.objects.get(name=info["guild"]["name"])
-                )
-            else:
-                Player.objects.create(
-                    nickname=name,
-                    email=info["email"],
-                    bio=info["bio"],
-                    race=Race.objects.get(name=info["race"]["name"])
-                )
+    skills = list(data.values())[1]["race"]["skills"]
+    for skill in skills:
+        Skill.objects.create(
+            name=skill["name"],
+            bonus=skill["bonus"],
+            race=Race.objects.get(name="elf")
+        )
+
+    list_of_guils = []
+    for name, info in data.items():
+        guild = info["guild"]
+        if isinstance(guild, dict) and\
+                guild["name"] not in list_of_guils:
+            list_of_guils.append(guild["name"])
+            Guild.objects.create(
+                name=guild["name"],
+                description=guild["description"]
+            )
+    for name, info in data.items():
+        name_of_race = info["race"]["name"]
+        if info["guild"] is not None:
+            Player.objects.create(
+                nickname=name,
+                email=info["email"],
+                bio=info["bio"],
+                race=Race.objects.get(name=name_of_race),
+                guild=Guild.objects.get(name=info["guild"]["name"])
+            )
+        else:
+            Player.objects.create(
+                nickname=name,
+                email=info["email"],
+                bio=info["bio"],
+                race=Race.objects.get(name=name_of_race)
+            )
+
+
+if __name__ == "__main__":
+    main()
