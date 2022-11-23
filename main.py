a01384d053696b6bf91d12fc320ac63f59da32a4
Mykola Zakharov <kolyazakharov94@gmail.com>
2022-11-23 23:26:54 +0200

    solution1'

--- main.py
+++ main.py
@@ -24,20 +24,19 @@ def main() -> None:
                                      bonus=skill_bonus,
                                      race=Race.objects.get(name=race_name))
 
-        if info["guild"]:
+        if not info["guild"]:
+            guild_obj = None
+        else:
             guild_name = info["guild"]["name"]
             guild_description = info["guild"]["description"]
             if not Guild.objects.filter(name=guild_name).exists():
                 Guild.objects.create(name=guild_name,
                                      description=guild_description)
+            guild_obj = Guild.objects.get(name=guild_name)
 
-            Player.objects.create(nickname=name,
-                                  email=info["email"],
-                                  bio=info["bio"],
-                                  race=Race.objects.get(name=race_name),
-                                  guild=Guild.objects.get(name=guild_name))
-        else:
-            Player.objects.create(nickname=name,
-                                  email=info["email"],
-                                  bio=info["bio"],
-                                  race=Race.objects.get(name=race_name))
+        Player.objects.create(
+            nickname=name,
+            email=info["email"],
+            bio=info["bio"],
+            race=Race.objects.get(name=race_name),
+            guild=guild_obj)
