a12220d7ca31b93a065ae7d645c838cddf3eed1b
Your Name <y>
2022-12-04 16:09:15 +0200

    Solution added

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
 import init_django_orm  # noqa: F401
 
+import json
+
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json") as file:
+        players_info = json.load(file)
+
+    for player, info in players_info.items():
+        email = info["email"]
+        bio = info["bio"]
+        race_name = info["race"]["name"]
+        skills = info["race"]["skills"]
+        guild = info["guild"]["name"] if info["guild"] else None
+
+        if not Race.objects.filter(name=race_name).exists():
+            race_description = info["race"]["description"]
+            Race.objects.create(name=race_name, description=race_description)
+
+        race = Race.objects.get(name=race_name)
+
+        for skill in skills:
+            skill_name = skill["name"]
+            if not Skill.objects.filter(name=skill_name).exists():
+                skill_bonus = skill["bonus"]
+                Skill.objects.create(
+                    name=skill_name, bonus=skill_bonus, race_id=race.id
+                )
+
+        if guild:
+            if not Guild.objects.filter(name=guild).exists():
+                guild_info = info["guild"]["description"]
+                Guild.objects.create(name=guild, description=guild_info)
+
+            guild = Guild.objects.get(name=guild)
+
+        if not Player.objects.filter(nickname=player).exists():
+            Player.objects.create(
+                nickname=player, email=email, bio=bio,
+                race_id=race.id, guild=guild
+            )
 
 
 if __name__ == "__main__":
