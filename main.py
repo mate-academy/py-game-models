0cf598ba8e3c95767a16de27cdc18f9c4851e960
Mykyta Moshchenko <mykytamoshchenko@outlook.com>
2022-12-18 12:47:59 +0100

    Solution Updated with better Race and Guild instances creation

--- main.py
+++ main.py
@@ -8,26 +8,35 @@ def main() -> None:
         players = json.load(players_data)
 
     for player, player_info in players.items():
+        if Race.objects.filter(name=player_info["race"]["name"]):
+            players_race = Race.objects.get(name=player_info["race"]["name"])
+        else:
+            players_race = Race.objects.create(
+                name=player_info["race"]["name"],
+                description=player_info["race"]["description"]
+                if player_info["race"]["description"] else None)
+
+        if not player_info["guild"]:
+            players_guild = None
+
+        else:
+            if Guild.objects.filter(name=player_info["guild"]["name"]):
+                players_guild = Guild.objects.get(
+                    name=player_info["guild"]["name"]
+                )
+            else:
+                players_guild = Guild.objects.create(
+                    name=player_info["guild"]["name"],
+                    description=player_info["guild"]["description"] if
+                    player_info["guild"]["description"] else None
+                )
+
         player_instance = Player.objects.create(
             nickname=player,
             email=player_info["email"],
             bio=player_info["bio"],
-            race=Race.objects.get(name=player_info["race"]["name"])
-            if Race.objects.filter(name=player_info["race"]["name"])
-            else Race.objects.create(
-                name=player_info["race"]["name"],
-                description=player_info["race"]["description"] if
-                player_info["race"]["description"] else None
-            ),
-            guild=None if not player_info["guild"] else
-            (
-                Guild.objects.get(name=player_info["guild"]["name"])
-                if Guild.objects.filter(name=player_info["guild"]["name"])
-                else Guild.objects.create(
-                    name=player_info["guild"]["name"],
-                    description=player_info["guild"]["description"]
-                    if player_info["guild"]["description"] else None
-                )),
+            race=players_race,
+            guild=players_guild
         )
         for skill in player_info["race"]["skills"]:
             if not Skill.objects.filter(name=skill["name"]):
