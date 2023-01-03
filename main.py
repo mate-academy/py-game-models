81bfb4c222f8e3c2d62d810be11fe77a1e840531
Kostyantyn Chernyk <chernykko@gmail.com>
2023-01-03 16:46:23 +0000

    Solution

--- main.py
+++ main.py
@@ -11,23 +11,26 @@ def main() -> None:
     for player_name, character in players_data.items():
 
         if not Player.objects.filter(nickname=player_name).exists():
+            race_name = character["race"]["name"]
             Player.objects.create(
                 nickname=player_name,
                 email=character["email"],
                 bio=character["bio"],
                 race=Race.objects.create(
-                    name=character["race"]["name"],
+                    name=race_name,
                     description=character["race"]["description"],
                 )
-                if not Race.objects.filter(name=character["race"]["name"]).exists()
-                else Race.objects.get(name=character["race"]["name"]),
+                if not Race.objects.filter(name=race_name).exists()
+                else Race.objects.get(name=race_name),
 
                 guild=None if character.get("guild") is None else
                 Guild.objects.create(
                     name=character["guild"]["name"],
                     description=character["guild"]["description"],
                 )
-                if not Guild.objects.filter(name=character["guild"]["name"]).exists()
+                if not Guild.objects.filter(
+                    name=character["guild"]["name"]
+                ).exists()
                 else Guild.objects.get(name=character["guild"]["name"])
             )
 
@@ -36,9 +39,11 @@ def main() -> None:
                     Skill.objects.create(
                         name=skill["name"],
                         bonus=skill["bonus"],
-                        race=Race.objects.get(name=character["race"]["name"])
+                        race=Race.objects.get(name=race_name)
                     ) \
-                        if not Skill.objects.filter(name=skill["name"]).exists() \
+                        if not Skill.objects.filter(
+                        name=skill["name"]
+                    ).exists() \
                         else Skill.objects.get(name=skill["name"]),
 
 
