3841a25b2154954ad4a306abbaca7da37f4de305
soobig666 <darkvader.ukr.net@gmail.com>
2023-01-23 11:41:49 +0200

    create Race, Skill, Guild - models in models.py

--- db/models.py
+++ db/models.py
@@ -1,1 +1,16 @@
 from django.db import models
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True)
+
