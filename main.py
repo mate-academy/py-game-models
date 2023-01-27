221bc542e24d8cc39df1c21abae578e7f047cf33
Tania Kharchuk <tkharchuk14@gmail.com>
2023-01-27 22:20:11 +0200

    solution

--- main.py
+++ main.py
@@ -1,10 +1,54 @@
+import json
+
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json") as file:
+        data = json.load(file)
+    print(data)
+    for player_nickname, player_info in data.items():
+
+        if not Race.objects.filter(name=player_info["race"]["name"]).exists():
+            race = Race.objects.create(
+                name=player_info["race"]["name"],
+                description=player_info["race"]["description"]
+            )
+        else:
+            race = Race.objects.get(name=player_info["race"]["name"])
+
+        for skill in player_info["race"]["skills"]:
+            if not Skill.objects.filter(name=skill["name"]).exists():
+                skill = Skill.objects.create(
+                    name=skill["name"],
+                    bonus=skill["bonus"],
+                    race=race
+                )
+            else:
+                skill = Skill.objects.get(name=skill["name"])
+
+        if (player_info["guild"] and not Guild.objects.filter(
+                name=player_info["guild"]["name"]
+        ).exists()):
+            guild = Guild.objects.create(
+                name=player_info["guild"]["name"],
+                description=player_info["guild"]["description"]
+            )
+        elif player_info["guild"]:
+            guild = Guild.objects.get(name=player_info["guild"]["name"])
+        else:
+            guild = None
+
+        if not Player.objects.filter(nickname=player_nickname).exists():
+            Player.objects.create(
+                nickname=player_nickname,
+                email=player_info["email"],
+                bio=player_info["bio"],
+                race=race,
+                guild=guild
+            )
 
 
 if __name__ == "__main__":
