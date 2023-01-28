e8ddc58f40e054ec73ce387a20f9398ce9aabeb4
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-01-28 11:17:54 +0200

    Solution 2

--- main.py
+++ main.py
@@ -10,39 +10,44 @@ def main() -> None:
         players_dict = load(file)
 
     for player_name, info in players_dict.items():
+
+        race_info = info["race"]
+
         if not Race.objects.filter(
-                name=info["race"]["name"]
+                name=race_info["name"]
         ).exists():
             Race.objects.create(
-                name=info["race"]["name"],
-                description=info["race"]["description"],
+                name=race_info["name"],
+                description=race_info["description"],
             )
 
-        if info["guild"] and \
-                not Guild.objects.filter(name=info["guild"]["name"]).exists():
-            Guild.objects.create(
-                name=info["guild"]["name"],
-                description=info["guild"]["description"]
-            )
-
-        for item in info["race"]["skills"]:
+        for item in race_info["skills"]:
             if not Skill.objects.filter(name=item["name"]).exists():
                 Skill.objects.create(
                     name=item["name"],
                     bonus=item["bonus"],
-                    race=Race.objects.get(name=info["race"]["name"])
+                    race=Race.objects.get(name=race_info["name"])
                 )
 
+        guild_info = info["guild"]
+
+        if guild_info and \
+                not Guild.objects.filter(name=guild_info["name"]).exists():
+            Guild.objects.create(
+                name=guild_info["name"],
+                description=guild_info["description"]
+            )
+
         guild_type = (
-            Guild.objects.get(name=info["guild"]["name"])
-            if info["guild"] else None
+            Guild.objects.get(name=guild_info["name"])
+            if guild_info else None
         )
 
         Player.objects.create(
             nickname=player_name,
             email=info["email"],
             bio=info["bio"],
-            race=Race.objects.get(name=info["race"]["name"]),
+            race=Race.objects.get(name=race_info["name"]),
             guild=guild_type
         )
 
