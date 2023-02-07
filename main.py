70c017d769f9f527d66439635357c315a9a580c1
Artem Motrunich <motrunich2013@gmail.com>
2023-02-07 11:48:52 +0100

    Solution v_2

--- main.py
+++ main.py
@@ -7,43 +7,37 @@ def main() -> None:
     # Open and load JSON file to "players" variable
     with open("players.json") as players_file:
         players = json.load(players_file)
-        for player in players:
-            race = players[player]["race"]
-            skills = players[player]["race"]["skills"]
-            guild = players[player]["guild"]
+        for nickname, info in players.items():
+            race = info["race"]
+            skills = info["race"]["skills"]
+            guild = info["guild"]
             # Create Race objects if records not exist yet
-            if not Race.objects.filter(name=race["name"]).exists():
-                Race.objects.create(
-                    name=race["name"],
-                    description=race["description"]
-                )
+            obj, created = Race.objects.get_or_create(
+                name=race["name"],
+                description=race["description"]
+            )
             # Create Guild objects if records not exist yet
             if guild:
-                if not Guild.objects.filter(name=guild["name"]).exists():
-                    Guild.objects.create(
-                        name=guild["name"],
-                        description=guild["description"]
-                    )
-            # Create Skill objects if records not exist yet
+                obj, created = Guild.objects.get_or_create(
+                    name=guild["name"],
+                    description=guild["description"]
+                )
+            # # Create Skill objects if records not exist yet
             if skills:
                 for skill in skills:
-                    if not Skill.objects.filter(name=skill["name"]).exists():
-                        Skill.objects.create(
-                            name=skill["name"],
-                            bonus=skill["bonus"],
-                            race=Race.objects.get(name=race["name"])
-                        )
-            # Create Player objects if records not exist yet
-            if not Player.objects.filter(nickname=player).exists():
-                Player.objects.create(
-                    nickname=player,
-                    email=players[player]["email"],
-                    bio=players[player]["bio"],
-                    race=Race.objects.get(name=race["name"]),
-                    guild=Guild.objects.get(
-                        name=guild["name"]
-                    ) if guild else None
-                )
+                    obj, created = Skill.objects.get_or_create(
+                        name=skill["name"],
+                        bonus=skill["bonus"],
+                        race=Race.objects.get(name=race["name"])
+                    )
+            # # Create Player objects if records not exist yet
+            obj, created = Player.objects.get_or_create(
+                nickname=nickname,
+                email=info["email"],
+                bio=info["bio"],
+                race=Race.objects.get(name=race["name"]),
+                guild=Guild.objects.get(name=guild["name"]) if guild else None
+            )
 
 
 if __name__ == "__main__":
