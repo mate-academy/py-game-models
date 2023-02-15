ed8f12a967028a969f1fedfc1ab15e3a4fae408b
Serhii Terletckiy <serhii.terletskiy.ua@gmail.com>
2023-02-15 18:40:30 +0200

    Solution

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import init_django_orm  # noqa: F401
-
-from db.models import Race, Skill, Player, Guild
+import json
+from db.models import Race, Skill, Player, Guild  # noqa: F401
+from app.createBaseClasses import CreateBase
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r") as json_file:
+        loaded_json: dict = json.load(json_file)
+        for player in loaded_json.keys():
+            CreateBase.create_player(
+                player,
+                loaded_json[player]
+            )
 
 
 if __name__ == "__main__":
