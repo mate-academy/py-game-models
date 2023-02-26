84e3884d9e90e830c635b887318e64fb71329265
Mazur Mariya <mazurinka29@gmail.com>
2023-02-26 21:03:55 +0200

    'Solution'

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from player_moduls.race_func import race_func
 from player_moduls.skill_func import skill_func
 from player_moduls.guild_func import guild_func
 from player_moduls.player_func import player_func
+from db.models import Player, Skill, Guild, Race
 
 
 def main() -> None:
