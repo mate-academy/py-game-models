ed7aa593d895c2eec8ff6b138feb73cc67d9bef4
Mazur Mariya <mazurinka29@gmail.com>
2023-02-27 14:35:19 +0200

    'Solution'

--- main.py
+++ main.py
@@ -1,10 +1,52 @@
+from typing import Any
 import init_django_orm  # noqa: F401
 import json
-from player_moduls.race_func import race_func
-from player_moduls.skill_func import skill_func
-from player_moduls.guild_func import guild_func
-from player_moduls.player_func import player_func
-from db.models import Player, Skill, Guild, Race
+from db.models import Race, Skill, Player, Guild
+
+
+def race_func(player: dict) -> Any:
+    if not Race.objects.filter(name=player["race"]["name"]).exists():
+        race_inst, created = Race.objects.get_or_create(
+            name=player["race"]["name"],
+            defaults={"description": player["race"]["description"]}
+        )
+        return race_inst
+
+
+def skill_func(player: dict) -> None:
+    for skill in player["race"]["skills"]:
+        if not Skill.objects.filter(name=skill["name"]).exists():
+            Skill.objects.get_or_create(
+                name=skill["name"],
+                defaults={"bonus": skill["bonus"], "race": race_func(player)}
+            )
+
+
+def guild_func(player: dict) -> Any:
+    guild = player.get("guild")
+    if guild is not None:
+        if not Guild.objects.filter(name=player["guild"]["name"]).exists():
+            guild_inst, created = Guild.objects.get_or_create(
+                name=player["guild"]["name"],
+                defaults={"description": player["guild"]["description"]}
+            )
+
+        else:
+            guild_inst = None
+
+        return guild_inst
+
+
+def player_func(players: dict) -> None:
+    for player_name, player in players.items():
+        if not Player.objects.filter(nickname=player_name).exists():
+            Player.objects.create(
+                nickname=player_name,
+                email=player["email"],
+                bio=player["bio"],
+                race=race_func(player),
+                guild=guild_func(player)
+            )
 
 
 def main() -> None:
@@ -12,11 +54,11 @@ def main() -> None:
     with open("players.json", "r") as file:
         players = json.load(file)
 
-        for player_name, player in players.items():
-            player_func(players)
-            race_func(player)
-            skill_func(player)
-            guild_func(player)
+    for player_name, player in players.items():
+        race_func(player)
+        skill_func(player)
+        guild_func(player)
+        player_func(players)
 
 
 if __name__ == "__main__":
