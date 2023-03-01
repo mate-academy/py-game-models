aedf98c1aaa449baf00846fedcda3e1d7ff78bc8
Vitalii Khmura <vitalikkhmura13@gmail.comw>
2023-03-01 21:31:47 +0200

    Solution 2

--- main.py
+++ main.py
@@ -15,46 +15,35 @@ def read_json() -> dict:
     return players
 
 
-def create_race(race_info: Dict[str, Any]) -> Race:
-    race = Race(
-        name=race_info.get("name"),
-        description=race_info.get("description")
-    )
-    race.save()
-
-    for skill in race_info.get("skills"):
-        Skill.objects.create(
-            name=skill.get("name"),
-            bonus=skill.get("bonus"),
-            race=race
-        )
-    return race
-
-
-def create_guild(guild_info: Dict[str, Any]) -> Guild:
-    guild = Guild(
-        name=guild_info.get("name"),
-        description=guild_info.get("description")
-    )
-    guild.save()
-    return guild
-
-
-def get_race(data: Dict[str, Any]) -> Race:
+def get_or_create_race(data: Dict[str, Any]) -> Race:
     try:
         race = Race.objects.get(name=data.get("name"))
     except ObjectDoesNotExist:
-        race = create_race(data)
+        race = Race(
+            name=data.get("name"),
+            description=data.get("description")
+        )
+        race.save()
+        for skill in data.get("skills"):
+            Skill.objects.create(
+                name=skill.get("name"),
+                bonus=skill.get("bonus"),
+                race=race
+            )
     return race
 
 
-def get_guild(data: Dict[str, Any]) -> Guild:
+def get_or_create_guild(data: Dict[str, Any]) -> Guild:
     guild = None
     if data:
         try:
             guild = Guild.objects.get(name=data.get("name"))
         except ObjectDoesNotExist:
-            guild = create_guild(data)
+            guild = Guild(
+                name=data.get("name"),
+                description=data.get("description")
+            )
+            guild.save()
     return guild
 
 
@@ -63,8 +52,8 @@ def main() -> None:
     for player, info in players.items():
         race_info = info.get("race")
         guild_info = info.get("guild")
-        race = get_race(race_info)
-        guild = get_guild(guild_info)
+        race = get_or_create_race(race_info)
+        guild = get_or_create_guild(guild_info)
         Player.objects.create(
             nickname=player,
             email=info.get("email"),
