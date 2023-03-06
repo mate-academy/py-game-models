e4718ff37428aab1935151f9a2e16e3ec3fa2344
maccarets <maccarets@gmail.com>
2023-03-06 14:42:45 +0200

    models is done

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
-
+import json
 
 def main() -> None:
-    pass
+    with open("players.json") as file:
+        data = json.load(file)
+        for key in data:
+            Player.objects.create(nickname=key, bio=data[key]["bio"])
 
 
 if __name__ == "__main__":
