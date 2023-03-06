4d414f84e1d077cbc6f95afe376c62bda8c84bf0
Taras Savchyn <savik1992@gmail.com>
2023-03-06 19:44:27 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,39 @@
+from __future__ import annotations
+
 from django.db import models
+
+races = (
+    ("Elf", "information"),
+    ("Dwarf", "information"),
+    ("Human", "information"),
+    ("Ork", "information"),
+)
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=255, choices=races, unique=True)
+    description = models.TextField(blank=True)
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    guild = models.ForeignKey(
+        Guild, on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
+    created_at = models.DateField(auto_now=True)
--- main.py
+++ main.py
@@ -1,10 +1,68 @@
 import init_django_orm  # noqa: F401
+import json
 
 from db.models import Race, Skill, Player, Guild
+from django.db.utils import IntegrityError
 
 
 def main() -> None:
-    pass
+    with open("./players.json") as file:
+        context = json.load(file)
+    for player, information in context.items():
+
+        # створюємо гільдії
+        guild = None
+        if information["guild"]:
+            guild = Guild(
+                name=information["guild"]["name"],
+                description=information["guild"]["description"],
+            )
+            try:
+                guild.save()
+            except IntegrityError:
+                pass
+
+        # створюємо раси
+        race = Race(
+            name=information["race"]["name"],
+            description=information["race"]["description"],
+        )
+        try:
+            race.save()
+        except IntegrityError:
+            pass
+
+        # створюємо скіли
+        race = Race.objects.get(name=race.name)
+
+        if information["race"]["skills"]:
+            for skills in information["race"]["skills"]:
+                skill = Skill(
+                    name=skills["name"],
+                    bonus=skills["bonus"],
+                    race=race
+                )
+                try:
+                    skill.save()
+                except IntegrityError:
+                    pass
+
+        # створюємо гравця
+        race = Race.objects.get(name=race.name)
+        if guild is not None:
+            guild = Guild.objects.get(name=guild.name)
+        person = Player(
+            nickname=player,
+            email=information["email"],
+            bio=information["bio"],
+            race=race,
+            guild=guild,
+        )
+
+        try:
+            person.save()
+        except IntegrityError:
+            pass
 
 
 if __name__ == "__main__":
