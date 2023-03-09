f586aaab284b47f0f14f87c08a4743686bff9d47
VladyslavCherkashyn <Sparrowq2@gmail.com>
2023-03-09 21:05:57 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,30 @@
 from django.db import models
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(max_length=255, help_text="skill bonus type")
+    race = models.ForeignKey(Race, on_delete=models.SET_NULL, null=True)
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True)
+
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField(
+        max_length=255,
+        blank=True,
+        help_text="short description provided by a user about himself/herself."
+    )
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
