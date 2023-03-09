18cc060e4e9735603de20112156b188fbda80cbd
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-03-09 16:23:15 +0200

    Created entities in DB

--- db/models.py
+++ db/models.py
@@ -1,1 +1,41 @@
 from django.db import models
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(max_length=255)
+    race = models.ForeignKey(
+        Race,
+        on_delete=models.SET_DEFAULT,
+        default="No skills"
+    )
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True)
+
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField(
+        "description provided by a user about himself/herself",
+        max_length=255
+    )
+    race = models.ForeignKey(
+        Race,
+        on_delete=models.SET_DEFAULT,
+        default="unknown"
+    )
+    guild = models.ForeignKey(
+        Guild,
+        on_delete=models.SET_DEFAULT,
+        default="free condottiere"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
--- main.py
+++ main.py
@@ -1,10 +1,31 @@
+import json
+from typing import List
+
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
+with open("players.json") as data:
+    DATA = json.load(data)
+
 
 def main() -> None:
-    pass
+    for user_name, user_data in DATA.items():
+        race, _ = Race.objects.get_or_create(
+            name=user_data.get("race").get("name"),
+            description=user_data.get("race").get("description")
+        )
+        for skill in user_data.get("race").get("skills"):
+            Skill.objects.get_or_create(
+                skill.get("name")
+            )
+
+        Player(
+            nickname=user_name,
+            email=user_data.get("email"),
+            bio=user_data.get("bio"),
+            race=race
+        )
 
 
 if __name__ == "__main__":
