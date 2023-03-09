ce62a4b953b541582e2012cb17159299db1ab29e
Yulia <Mala>
2023-03-09 20:01:08 +0200

    some vars added

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def main() -> None:
             Race.objects.create(
                 name=info["race"]["name"],
                 description=info["race"]["description"])
+        player_race = Race.objects.get(name=info["race"]["name"])
         if (info["guild"] is not None and not
                 Guild.objects.filter(name=info["guild"]["name"]).exists()):
             Guild.objects.create(
@@ -23,14 +24,14 @@ def main() -> None:
                 Skill.objects.create(
                     name=skill["name"],
                     bonus=skill["bonus"],
-                    race=Race.objects.get(name=info["race"]["name"]))
+                    race=player_race)
         player_guild = (None if info["guild"] is None
                         else Guild.objects.get(name=info["guild"]["name"]))
         Player.objects.create(
             nickname=name,
             email=info["email"],
             bio=info["bio"],
-            race=Race.objects.get(name=info["race"]["name"]),
+            race=player_race,
             guild=player_guild
         )
 
