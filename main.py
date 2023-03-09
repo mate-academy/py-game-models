2044d601a531d6c86f32bdb8f7c51405f559dfd5
Vladyslav Demydenko <bublikyo3@gmail.com>
2023-03-09 11:54:44 +0100

    refactored with get_or_create Solution

--- main.py
+++ main.py
@@ -7,48 +7,40 @@ import json
 def main() -> None:
     with open("players.json") as players:
         players_data = json.load(players)
+
     for name, data in players_data.items():
         race = data["race"]
         guild = data["guild"]
         skills = race["skills"]
 
-        exists_race = Race.objects.filter(name=race["name"]).exists()
         race = (
-            Race.objects.create(
+            Race.objects.get_or_create(
                 name=race["name"],
-                description=race["description"]
-            )
-            if not exists_race else Race.objects.get(name=race["name"])
-        )
+                description=race["description"])
+        )[0]
 
         if guild:
-            exists_guild = Guild.objects.filter(name=guild["name"]).exists()
             guild = (
-                Guild.objects.create(
+                Guild.objects.get_or_create(
                     name=guild["name"],
                     description=guild["description"]
                 )
-                if not exists_guild else Guild.objects.get(name=guild["name"])
-            )
+            )[0]
 
         for skill in skills:
-            exists_skill = Skill.objects.filter(name=skill["name"]).exists()
-            if not exists_skill:
-                Skill.objects.create(
-                    name=skill["name"],
-                    bonus=skill["bonus"],
-                    race=race
-                )
-
-        exists_player = Player.objects.filter(nickname=name).exists()
-        if not exists_player:
-            Player.objects.create(
-                nickname=name,
-                email=data["email"],
-                bio=data["bio"],
-                race=race,
-                guild=guild
+            Skill.objects.get_or_create(
+                name=skill["name"],
+                bonus=skill["bonus"],
+                race=race
             )
+
+        Player.objects.get_or_create(
+            nickname=name,
+            email=data["email"],
+            bio=data["bio"],
+            race=race,
+            guild=guild
+        )
 
 
 if __name__ == "__main__":
