e0a772550ef014a56a72c5d71387bc44fafdebb1
Maksym Nikityuk <maksym71417@gmail.com>
2023-03-09 20:45:07 +0200

    Fixed Solution

--- main.py
+++ main.py
@@ -10,29 +10,27 @@ def main() -> None:
         players = json.load(file)
 
     for name, player_info in players.items():
-        race = player_info["race"]["name"]
+        race_name = player_info["race"]["name"]
         race_descript = player_info["race"]["description"]
         race_skills = player_info["race"]["skills"]
 
-        if Race.objects.filter(name=race).exists():
-            Race.objects.get(name=race)
+        if Race.objects.filter(name=race_name).exists():
+            race = Race.objects.get(name=race_name)
         else:
-            Race.objects.create(name=race,
-                                description=race_descript)
+            race = Race.objects.create(name=race_name,
+                                       description=race_descript)
 
         if race_skills:
             for skill in race_skills:
                 skill_name = skill.get("name")
                 skill_bonus = skill.get("bonus")
-                skill_race = Race.objects.get(name=race)
 
-                if Skill.objects.filter(name=skill_name).exists():
-                    Skill.objects.get(name=skill_name)
-                else:
+                if not Skill.objects.filter(name=skill_name).exists():
                     Skill.objects.create(name=skill_name,
                                          bonus=skill_bonus,
-                                         race=skill_race)
+                                         race=race)
 
+        guild = None
         if player_info["guild"]:
             guild_name = player_info["guild"]["name"]
             guild_description = player_info["guild"]["description"]
@@ -42,10 +40,6 @@ def main() -> None:
             else:
                 guild = Guild.objects.create(name=guild_name,
                                              description=guild_description)
-        else:
-            guild = None
-
-        race = Race.objects.get(name=player_info["race"]["name"])
 
         Player.objects.create(nickname=name,
                               email=player_info["email"],
