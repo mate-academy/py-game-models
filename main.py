2488e434337f9e62b98b610b58b316d07caeec44
Viktoria Rybenchuk <rybenchukv@gmail.com>
2023-03-10 13:31:00 +0100

    Solution

--- main.py
+++ main.py
@@ -9,68 +9,44 @@ with open("players.json", "r") as sourse:
 
 
 def main() -> None:
-    global race_id
     for nickname, info in data.items():
-        if info["race"]:
-            name_race = (
-                info["race"]["name"] if
-                info["race"]["name"] else None
-            )
-            race_description = (
-                info["race"]["description"] if
-                info["race"]["description"] else None
-            )
+        name_race = info["race"]["name"]
+        race_description = info["race"]["description"]
 
-            if not Race.objects.filter(name=name_race).exists():
-                race = Race(
-                    name=name_race,
-                    description=race_description
-                )
-                race.save()
-                race_id = Race.objects.get(name=name_race).id
-        else:
-            race_id = None
-
+        if not Race.objects.filter(name=name_race).exists():
+            race = Race.objects.create(
+                name=name_race,
+                description=race_description
+            )
         if info["guild"]:
-            guild_name = (
-                info["guild"]["name"] if
-                info["guild"]["name"] else None)
-            guild_description = (
-                info["guild"]["description"] if
-                info["guild"]["description"] else None)
-
+            guild_name = info["guild"]["name"]
+            guild_description = info["guild"]["description"]
             if not Guild.objects.filter(name=guild_name).exists():
-                guild_object = Guild(
+                guild = Guild.objects.create(
                     name=guild_name,
                     description=guild_description
                 )
-                guild_object.save()
-
-            guild_id = Guild.objects.get(name=guild_name).id
         else:
-            guild_id = None
+            guild = None
 
-        if info["race"]["skills"]:
-            for skill in info["race"]["skills"]:
-                name_skill = skill["name"] if skill["name"] else None
-                bonus = skill["bonus"] if skill["bonus"] else None
-                if not Skill.objects.filter(name=name_skill).exists():
-                    skill_object = Skill(
-                        name=name_skill,
-                        bonus=bonus,
-                        race_id=race_id)
-                    skill_object.save()
+        for skill in info["race"]["skills"]:
+            name_skill = skill["name"]
+            bonus = skill["bonus"]
+            if not Skill.objects.filter(name=name_skill).exists():
+                Skill.objects.create(
+                    name=name_skill,
+                    bonus=bonus,
+                    race=race
+                )
 
-        email = info["email"] if info["email"] else None
-        biography = info["bio"] if info["bio"] else None
-
-        Player.objects.create(
-            nickname=nickname,
-            email=email,
-            bio=biography,
-            race_id=race_id,
-            guild_id=guild_id
-        )
+        if not Player.objects.filter(nickname=nickname).exists():
+            Player.objects.create(
+                nickname=nickname,
+                email=info["email"],
+                bio=info["bio"],
+                race=race,
+                guild=guild
+            )
 
 
 if __name__ == "__main__":
