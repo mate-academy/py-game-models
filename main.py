a038fade80d4017f3a236417fc3d42d8c4823499
Anatoliy Pilipey <pilipeytolya@gmail.com>
2023-03-12 22:49:09 +0200

    Solution

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import init_django_orm  # noqa: F401
 import json
 
-
 from db.models import Race, Skill, Player, Guild
 
 
@@ -10,32 +9,40 @@ def main() -> None:
         players_info = json.load(players_file)
     for player_name, player_info in players_info.items():
         race_name = player_info["race"]["name"]
-        if not Race.objects.filter(name=race_name):
+        try:
+            Race.objects.get(name=race_name)
+        except Race.DoesNotExist:
             Race.objects.create(
                 name=race_name,
                 description=player_info["race"]["description"]
             )
             for skill in player_info["race"]["skills"]:
-                if not Skill.objects.filter(name=skill["name"]):
+                try:
+                    Skill.objects.get(name=skill["name"])
+                except Skill.DoesNotExist:
                     Skill.objects.create(
                         name=skill["name"],
                         bonus=skill["bonus"],
                         race=Race.objects.get(name=race_name)
                     )
+        if isinstance(player_info["guild"], dict):
+            guild_name = player_info["guild"]["name"]
+            try:
+                Guild.objects.get(name=guild_name)
+                if guild_name is None:
+                    raise Guild.DoesNotExist
+            except Guild.DoesNotExist:
+                Guild.objects.create(
+                    name=player_info["guild"]["name"],
+                    description=player_info["guild"]["description"]
+                )
 
-        guild_name = player_info["guild"]["name"]\
-            if type(player_info["guild"]) == dict else None
-        if not Guild.objects.filter(
-                name=guild_name
-        ) and guild_name is not None:
-            Guild.objects.create(
-                name=player_info["guild"]["name"],
-                description=player_info["guild"]["description"]
-            )
-        if not Player.objects.filter(nickname=player_name):
+        try:
+            Player.objects.get(nickname=player_name)
+        except Player.DoesNotExist:
             player_guild = Guild.objects.get(
                 name=player_info["guild"]["name"]
-            ) if type(player_info["guild"]) == dict else None
+            ) if isinstance(player_info["guild"], dict) else None
             Player.objects.create(
                 nickname=player_name,
                 email=player_info["email"],
