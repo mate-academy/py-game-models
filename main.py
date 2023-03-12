a6bedb9d171bef8fbd95b81f9d909db0e53064b5
Yulia <Mala>
2023-03-12 16:39:44 +0200

    get_or_create instead of if -> create

--- main.py
+++ main.py
@@ -9,33 +9,26 @@ def main() -> None:
     with open("players.json", "r") as file:
         players = json.load(file)
     for name, info in players.items():
-        if not Race.objects.filter(name=info["race"]["name"]).exists():
-            Race.objects.create(
-                name=info["race"]["name"],
-                description=info["race"]["description"])
-        player_race = Race.objects.get(name=info["race"]["name"])
-        if (info["guild"] is not None and not
-                Guild.objects.filter(name=info["guild"]["name"]).exists()):
-            Guild.objects.create(
+        player_race, _ = Race.objects.get_or_create(
+            name=info["race"]["name"],
+            description=info["race"]["description"])
+        if info["guild"] is not None:
+            player_guild, _ = Guild.objects.get_or_create(
                 name=info["guild"]["name"],
-                description=info["guild"]["description"]
-            )
+                description=info["guild"]["description"])
+        else:
+            player_guild = None
         for skill in info["race"]["skills"]:
-            if not Skill.objects.filter(name=skill["name"]).exists():
-                Skill.objects.create(
-                    name=skill["name"],
-                    bonus=skill["bonus"],
-                    race=player_race
-                )
-        player_guild = (None if info["guild"] is None
-                        else Guild.objects.get(name=info["guild"]["name"]))
+            Skill.objects.get_or_create(
+                name=skill["name"],
+                bonus=skill["bonus"],
+                race=player_race)
         Player.objects.create(
             nickname=name,
             email=info["email"],
             bio=info["bio"],
             race=player_race,
-            guild=player_guild
-        )
+            guild=player_guild)
 
 
 if __name__ == "__main__":
