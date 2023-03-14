2246f3afe24aa53a14ea6cdafecc8be603ab5fef
Serhii Skivka <skivkas.v@gmail.com>
2023-03-14 22:00:20 +0200

    Solution

--- main.py
+++ main.py
@@ -10,32 +10,35 @@ def main() -> None:
 
     for player_nickname, player_info in player_data.items():
 
-        race, created = Race.objects.get_or_create(
-            name=player_info["race"]["name"],
-            description=player_info["race"]["description"]
+        race = player_info.get("race")
+        new_race, created = Race.objects.get_or_create(
+            name=race.get("name"),
+            description=race.get("description")
         )
 
-        for skill in player_info["race"]["skills"]:
+        skills = race.get("skills")
+        for skill in skills:
             Skill.objects.get_or_create(
-                race=race,
-                name=skill["name"],
-                bonus=skill["bonus"]
+                race=new_race,
+                name=skill.get("name"),
+                bonus=skill.get("bonus")
             )
 
-        if player_info["guild"] is not None:
-            guild, created = Guild.objects.get_or_create(
-                name=player_info["guild"]["name"],
-                description=player_info["guild"]["description"]
+        guild = player_info.get("guild")
+        if guild is not None:
+            new_guild, created = Guild.objects.get_or_create(
+                name=guild.get("name"),
+                description=guild.get("description")
             )
         else:
-            guild = None
+            new_guild = None
 
         Player.objects.get_or_create(
             nickname=player_nickname,
-            email=player_info["email"],
-            bio=player_info["bio"],
-            race=race,
-            guild=guild
+            email=player_info.get("email"),
+            bio=player_info.get("bio"),
+            race=new_race,
+            guild=new_guild
         )
 
 
