e44e74d24adc734a939797c4f926a52800f3385d
6aPa6aH <6apa6ah666@gmail.com>
2023-03-16 14:47:14 +0200

    fix_code

--- main.py
+++ main.py
@@ -10,50 +10,32 @@ def main() -> None:
 
     for player in json_data:
         player_info = json_data[player]
-        if Race.objects.filter(
-                name=player_info["race"]["name"]
-        ):
-            pass
-        else:
-            Race.objects.create(
-                name=player_info["race"]["name"],
-                description=player_info["race"]["description"]
-            )
+        Race.objects.get_or_create(
+            name=player_info["race"]["name"],
+            description=player_info["race"]["description"]
+        )
         if player_info["guild"] is not None:
-            if Guild.objects.filter(
-                    name=player_info["guild"]["name"]
-            ):
-                pass
-            else:
-                Guild.objects.create(
-                    name=player_info["guild"]["name"],
-                    description=player_info["guild"]["description"]
-                )
-        guild_id = Guild.objects.filter(
-            name=player_info["guild"]["name"]
-        ).values("id") if player_info["guild"] else None
-        race_id = Race.objects.filter(
-            name=player_info["race"]["name"]
-        ).values("id")
-        if Player.objects.filter(nickname=player):
-            pass
-        else:
-            Player.objects.create(
-                nickname=player,
-                email=player_info["email"],
-                bio=player_info["bio"],
-                race_id=race_id,
-                guild_id=guild_id
+            Guild.objects.get_or_create(
+                name=player_info["guild"]["name"],
+                description=player_info["guild"]["description"]
             )
+        guild_name = player_info["guild"]
+        guild_id = Guild.objects.get(
+            name=guild_name["name"]).id if guild_name else None
+        race_id = Race.objects.get(name=player_info["race"]["name"]).id
+        Player.objects.get_or_create(
+            nickname=player,
+            email=player_info["email"],
+            bio=player_info["bio"],
+            race_id=race_id,
+            guild_id=guild_id
+        )
         for skill in player_info["race"]["skills"]:
-            if Skill.objects.filter(name=skill["name"]):
-                pass
-            else:
-                Skill.objects.create(
-                    name=skill["name"],
-                    bonus=skill["bonus"],
-                    race_id=race_id
-                )
+            Skill.objects.get_or_create(
+                name=skill["name"],
+                bonus=skill["bonus"],
+                race_id=race_id
+            )
 
 
 if __name__ == "__main__":
