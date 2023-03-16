c2ddc2469297f7788b1f4c5fb9b2c413d086323f
Zenon <zenion@ukr.net>
2023-03-16 18:42:19 +0200

    Fix

--- main.py
+++ main.py
@@ -12,33 +12,23 @@ def main() -> None:
         skills = data.get("race").get("skills")
         guild = data.get("guild")
 
-        if Race.objects.filter(name=race.get("name")).exists():
-            race = Race.objects.get(name=race.get("name"))
-        else:
-            race = Race(
-                name=race.get("name"),
-                description=race.get("description")
-            )
-            race.save()
+        race, _ = Race.objects.get_or_create(
+            name=race.get("name"),
+            description=race.get("description")
+        )
 
         for skill in skills:
-            if not Skill.objects.filter(name=skill.get("name")).exists():
-                skill = Skill(
-                    name=skill.get("name"),
-                    bonus=skill.get("bonus"),
-                    race=race
-                )
-                skill.save()
+            Skill.objects.get_or_create(
+                name=skill.get("name"),
+                bonus=skill.get("bonus"),
+                race=race
+            )
 
         if guild:
-            if Guild.objects.filter(name=guild.get("name")).exists():
-                guild = Guild.objects.get(name=guild.get("name"))
-            else:
-                guild = Guild(
-                    name=guild.get("name"),
-                    description=guild.get("description")
-                )
-                guild.save()
+            guild, _ = Guild.objects.get_or_create(
+                name=guild.get("name"),
+                description=guild.get("description")
+            )
 
         player = Player(
             nickname=player,
