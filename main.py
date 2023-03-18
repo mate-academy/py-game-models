99c95578f6e29b0ffcb9ad88ec008a2ef3300afb
Vitalii Shulepa <vitaliy.shulep@gmail.com>
2023-03-18 12:06:19 +0200

    Fixed using get method

--- main.py
+++ main.py
@@ -15,34 +15,40 @@ def main() -> None:
 
     for player_name, player_data in data.items():
         race_name = player_data["race"]["name"]
-        guild_name = player_data["guild"]["name"] if player_data[
+        guild_name = player_data.get("guild").get("name") if player_data.get(
             "guild"
-        ] else None
+        ) else None
 
         race, _ = Race.objects.get_or_create(
             name=race_name,
-            defaults={"description": player_data["race"]["description"]}
+            defaults={
+                "description": player_data.get("race").get("description")
+            }
         )
         races[race_name] = race
 
         if guild_name:
             guild, _ = Guild.objects.get_or_create(
                 name=guild_name,
-                defaults={"description": player_data["guild"]["description"]}
+                defaults={
+                    "description": player_data.get("guild").get(
+                        "description"
+                    )
+                }
             )
             guilds[guild_name] = guild
 
-        for skill_data in player_data["race"]["skills"]:
-            skill_name = skill_data["name"]
+        for skill_data in player_data.get("race").get("skills"):
+            skill_name = skill_data.get("name")
             if skill_name not in skills:
                 skill = Skill.objects.create(
-                    name=skill_name, bonus=skill_data["bonus"], race=race
+                    name=skill_name, bonus=skill_data.get("bonus"), race=race
                 )
                 skills[skill_name] = skill
 
         Player.objects.create(
-            nickname=player_name, email=player_data["email"],
-            bio=player_data["bio"], race=race,
+            nickname=player_name, email=player_data.get("email"),
+            bio=player_data.get("bio"), race=race,
             guild=guilds.get(guild_name))
 
 
