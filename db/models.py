281aa95dda305b25dd3bf61a7bb60aa0757a615f
Zenon <zenion@ukr.net>
2023-03-19 18:52:22 +0200

    Fix

--- db/models.py
+++ db/models.py
@@ -9,9 +9,7 @@ class Race(models.Model):
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race,
-                             on_delete=models.CASCADE,
-                             related_name="skill_set")
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
 
 
 class Guild(models.Model):
