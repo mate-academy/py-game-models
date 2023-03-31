42873d44841217e20dd656e772c7a45c2b7a0b27
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-03-31 09:24:39 +0300

    Use get_or_create()

--- main.py
+++ main.py
@@ -18,13 +18,10 @@ def main() -> None:
         race_name = race_data.get("name")
         race_description = race_data.get("description")
 
-        if not Race.objects.filter(name=race_name).exists():
-            race = Race.objects.create(
-                name=race_name,
-                description=race_description
-            )
-        else:
-            race = Race.objects.get(name=race_name)
+        race, created = Race.objects.get_or_create(
+            name=race_name,
+            description=race_description
+        )
 
         guild_data = player_data.get("guild")
         guild = None
@@ -33,28 +30,23 @@ def main() -> None:
             guild_name = guild_data.get("name")
             guild_description = guild_data.get("description")
 
-            if not Guild.objects.filter(name=guild_name).exists():
-                guild = Guild.objects.create(
-                    name=guild_name,
-                    description=guild_description
-                )
-            else:
-                guild = Guild.objects.get(name=guild_name)
+            guild, created = Guild.objects.get_or_create(
+                name=guild_name,
+                description=guild_description
+            )
 
         race_skills = race_data.get("skills")
 
         for skill in race_skills:
             skill_name = skill.get("name")
             skill_bonus = skill.get("bonus")
+            skill_race = Race.objects.get(name=race_name)
 
-            if not Skill.objects.filter(name=skill_name).exists():
-                skill_race = Race.objects.get(name=race_name)
-
-                Skill.objects.create(
-                    name=skill_name,
-                    bonus=skill_bonus,
-                    race=skill_race
-                )
+            Skill.objects.get_or_create(
+                name=skill_name,
+                bonus=skill_bonus,
+                race=skill_race
+            )
 
         Player.objects.create(
             nickname=nickname,
