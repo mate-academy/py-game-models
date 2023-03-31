0e86bfdf3e38248f009adbdcfdc42c4f55006909
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-03-31 16:03:56 +0300

    Separate main() into a few functions

--- main.py
+++ main.py
@@ -5,56 +5,83 @@ import init_django_orm  # noqa: F401
 from db.models import Race, Skill, Player, Guild
 
 
+def get_or_create_race(player: tuple[str, dict]) -> Race:
+    (player_name, player_data) = player
+
+    race_data = player_data.get("race")
+    race_name = race_data.get("name")
+    race_description = race_data.get("description")
+
+    race, created = Race.objects.get_or_create(
+        name=race_name,
+        description=race_description
+    )
+
+    return race
+
+
+def get_or_create_guild(player: tuple[str, dict]) -> Guild:
+    (player_name, player_data) = player
+
+    guild_data = player_data.get("guild")
+    guild = None
+
+    if guild_data:
+        guild_name = guild_data.get("name")
+        guild_description = guild_data.get("description")
+
+        guild, created = Guild.objects.get_or_create(
+            name=guild_name,
+            description=guild_description
+        )
+
+    return guild
+
+
+def create_race_skills(player: tuple[str, dict]) -> None:
+    (player_name, player_data) = player
+
+    race_data = player_data.get("race")
+    race_name = race_data.get("name")
+    race_skills = race_data.get("skills")
+
+    for skill in race_skills:
+        skill_name = skill.get("name")
+        skill_bonus = skill.get("bonus")
+        skill_race = Race.objects.get(name=race_name)
+
+        Skill.objects.get_or_create(
+            name=skill_name,
+            bonus=skill_bonus,
+            race=skill_race
+        )
+
+
+def create_player(player: tuple[str, dict], race: Race, guild: Guild) -> None:
+    (player_name, player_data) = player
+
+    nickname = player_name
+    email = player_data.get("email")
+    bio = player_data.get("bio")
+
+    Player.objects.create(
+        nickname=nickname,
+        email=email,
+        bio=bio,
+        race=race,
+        guild=guild
+    )
+
+
 def main() -> None:
     with open("players.json") as source_file:
         players = json.load(source_file)
 
-    for player_name, player_data in players.items():
-        nickname = player_name
-        email = player_data.get("email")
-        bio = player_data.get("bio")
-
-        race_data = player_data.get("race")
-        race_name = race_data.get("name")
-        race_description = race_data.get("description")
-
-        race, created = Race.objects.get_or_create(
-            name=race_name,
-            description=race_description
-        )
-
-        guild_data = player_data.get("guild")
-        guild = None
-
-        if guild_data:
-            guild_name = guild_data.get("name")
-            guild_description = guild_data.get("description")
-
-            guild, created = Guild.objects.get_or_create(
-                name=guild_name,
-                description=guild_description
-            )
-
-        race_skills = race_data.get("skills")
-
-        for skill in race_skills:
-            skill_name = skill.get("name")
-            skill_bonus = skill.get("bonus")
-            skill_race = Race.objects.get(name=race_name)
-
-            Skill.objects.get_or_create(
-                name=skill_name,
-                bonus=skill_bonus,
-                race=skill_race
-            )
-
-        Player.objects.create(
-            nickname=nickname,
-            email=email,
-            bio=bio,
-            race=race,
-            guild=guild
-        )
+    for player in players.items():
+        race = get_or_create_race(player)
+        guild = get_or_create_guild(player)
+        create_race_skills(player)
+        create_player(player, race, guild)
 
 
 if __name__ == "__main__":
