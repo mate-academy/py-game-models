d5247094b529089a3ab9896897bc15289799e4bf
Mkrtych Khachatrian <mukuchbrat@gmail.com>
2023-04-02 15:15:10 +0300

    fixed get(), added get_or_create()

--- main.py
+++ main.py
@@ -14,45 +14,45 @@ def main() -> None:
 
         # Creating a race
 
-        race_name = player.get("race").get("name")
-        race_description = player.get("race").get("description")
-        if not Race.objects.filter(
-                name=race_name).exists() and race_description:
-            race_obj = Race.objects.create(
+        race_name = player.get("race", {}).get("name")
+        race_description = player.get("race", {}).get("description")
+
+        if race_description:
+            race_obj, race_created = Race.objects.get_or_create(
                 name=race_name,
                 description=race_description
             )
-        elif not Race.objects.filter(
-                name=race_name).exists() and race_description is None:
-            race_obj = Race.objects.create(name=race_name)
+        elif race_description is None:
+            race_obj, race_created = Race.objects.get_or_create(name=race_name)
 
         # Creating skills
 
-        for skill in player.get("race").get("skills"):
+        for skill in player.get("race", {}).get("skills"):
             skill_name = skill.get("name")
             skill_bonus = skill.get("bonus")
-            if not Skill.objects.filter(name=skill_name).exists():
-                skill = Skill.objects.create(
-                    name=skill_name,
-                    bonus=skill_bonus,
-                    race=race_obj
-                )
+
+            skill, skill_created = Skill.objects.get_or_create(
+                name=skill_name,
+                bonus=skill_bonus,
+                race=race_obj
+            )
 
         # Creating a guild
 
         if player.get("guild") is not None:
-            guild_name = player.get("guild").get("name")
+            guild_name = player.get("guild", {}).get("name")
             guild_description = player.get("guild", {}).get("description")
-            if not Guild.objects.filter(name=guild_name).exists():
-                guild_obj = Guild.objects.create(
-                    name=guild_name,
-                    description=guild_description
-                )
+
+            guild_obj, guild_created = Guild.objects.get_or_create(
+                name=guild_name,
+                description=guild_description
+            )
 
         # Creating a player
 
         player_email = player.get("email")
         player_bio = player.get("bio")
+
         player_obj = Player.objects.create(
             nickname=player_nickname,
             email=player_email,
