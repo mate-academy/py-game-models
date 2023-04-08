5c81df2868bcec8798576afcf64c4c255718da4f
Yurii Kuchmenko <kuazakotl1@gmail.com>
2023-04-08 20:53:53 +0300

    Refactor

--- main.py
+++ main.py
@@ -14,38 +14,36 @@ def main() -> None:
         guild = data["guild"]
 
         if not Race.objects.filter(name=race["name"]).exists():
-            player_race = Race.objects.create(
+            race = Race.objects.create(
                 name=race["name"],
                 description=race["description"],
             )
         else:
-            player_race = Race.objects.get(name=race["name"])
+            race = Race.objects.get(name=race["name"])
 
         for skill in skills:
             if not Skill.objects.filter(name=skill["name"]).exists():
                 Skill.objects.create(
                     name=skill["name"],
                     bonus=skill["bonus"],
-                    race=player_race,
+                    race=race,
                 )
 
         if guild:
             if not Guild.objects.filter(name=guild["name"]).exists():
-                player_guild = Guild.objects.create(
+                guild = Guild.objects.create(
                     name=guild["name"],
                     description=guild.get("description"),
                 )
             else:
-                player_guild = Guild.objects.get(name=guild["name"])
-        else:
-            player_guild = None
+                guild = Guild.objects.get(name=guild["name"])
 
         Player.objects.create(
             nickname=player,
             email=data["email"],
             bio=data["bio"],
-            race=player_race,
-            guild=player_guild,
+            race=race,
+            guild=guild,
         )
 
 
