a852c02135fe0a1cdaa897033859d53a66efe300
V Borovets <vborovets0@gmail.com>
2023-05-07 09:33:08 +0300

    create race

--- main.py
+++ main.py
@@ -8,21 +8,26 @@ def main() -> None:
     with open("players.json") as file:
         players_data = json.load(file)
         for nickname, value in players_data.items():
+
+            # create race
+            for race in value["race"]:
+                race_name = race["name"]
+                race_description = race["description"]
+                Race.objects.create(name=race_name, description=race_description)
+                race_id = Race.objects.get()
+                for skill in race["skills"]:
+                    skill_name = skill["name"]
+                    skill_bonus = skill["bonus"]
+
             email = value["email"]
             bio = value["bio"]
             if value["guild"]:
                 guild_name = value["guild"]["name"]
                 guild_description = value["guild"]["description"]
 
-            print(nickname,email, bio, guild_name, guild_description)
+            print(nickname, email, bio, guild_name, guild_description)
             print()
-            for race in value["race"]:
-                race_name = race["name"]
-                race_description = race["description"]
 
-                for skill in race["skills"]:
-                    skill_name = skill["name"]
-                    skill_bonus = skill["bonus"]
 
 
 
