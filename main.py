dafe0e80b7f1683d5680adf424c6592beb8e7a19
Volodymyr Borovets <vborovets0@gmail.com>
2023-05-07 16:42:07 +0300

    replace exists to get_or_create

--- main.py
+++ main.py
@@ -10,50 +10,44 @@ def main() -> None:
         for nickname, player_info in players_data.items():
 
             # create race
-            for _ in player_info["race"]:
-                race_name = player_info["race"]["name"]
-                race_description = player_info["race"]["description"]
-                if not Race.objects.filter(name=race_name).exists():
-                    Race.objects.create(
-                        name=race_name,
-                        description=race_description
-                    )
-                race_id = Race.objects.get(name=race_name).id
+            race_name = player_info["race"]["name"]
+            race_description = player_info["race"]["description"]
+            race, _ = Race.objects.get_or_create(
+                name=race_name,
+                description=race_description
+            )
+            # race_id = Race.objects.get(name=race_name).id
 
-                # create skills
-                for skill in player_info["race"]["skills"]:
-                    skill_name = skill["name"]
-                    skill_bonus = skill["bonus"]
-                    if not Skill.objects.filter(name=skill_name).exists():
-                        Skill.objects.create(
-                            name=skill_name,
-                            bonus=skill_bonus,
-                            race_id=race_id
-                        )
+            # create skills
+            for skill in player_info["race"]["skills"]:
+                skill_name = skill["name"]
+                skill_bonus = skill["bonus"]
+                Skill.objects.get_or_create(
+                    name=skill_name,
+                    bonus=skill_bonus,
+                    race=race
+                )
 
             # create guild
-            if player_info["guild"]:
+            if player_info.get("guild"):
                 guild_name = player_info["guild"]["name"]
                 guild_description = player_info["guild"]["description"]
-                if not Guild.objects.filter(name=guild_name).exists():
-                    Guild.objects.create(
-                        name=guild_name,
-                        description=guild_description
-                    )
-                guild_id = Guild.objects.get(name=guild_name).id
+                guild, _ = Guild.objects.get_or_create(
+                    name=guild_name,
+                    description=guild_description
+                )
             else:
-                guild_id = None
+                guild = None
 
             # create player
             email = player_info["email"]
             bio = player_info["bio"]
-            if not Player.objects.filter(nickname=nickname).exists():
-                Player.objects.create(
-                    nickname=nickname,
-                    email=email, bio=bio,
-                    race_id=race_id,
-                    guild_id=guild_id
-                )
+            Player.objects.get_or_create(
+                nickname=nickname,
+                email=email, bio=bio,
+                race=race,
+                guild=guild
+            )
 
 
 if __name__ == "__main__":
