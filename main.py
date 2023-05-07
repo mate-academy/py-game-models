d4123cb6ec8a406e524a822cd755c68303aee470
V Borovets <vborovets0@gmail.com>
2023-05-07 08:33:35 +0300

    get data from json

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
+import json
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json") as file:
+        players_data = json.load(file)
+        for nickname, value in players_data.items():
+            email = value["email"]
+            bio = value["bio"]
+            if value["guild"]:
+                guild_name = value["guild"]["name"]
+                guild_description = value["guild"]["description"]
+
+            print(nickname,email, bio, guild_name, guild_description)
+            print()
+            for race in value["race"]:
+                race_name = race["name"]
+                race_description = race["description"]
+
+                for skill in race["skills"]:
+                    skill_name = skill["name"]
+                    skill_bonus = skill["bonus"]
+
 
 
 if __name__ == "__main__":
