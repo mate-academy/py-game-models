97b7e9024006c53c84dd985d2665e82e379e09eb
Roman Kotok <kotokroman@gmail.com>
2023-05-19 18:38:06 +0300

    sol

--- main.py
+++ main.py
@@ -10,40 +10,38 @@ def main() -> None:
 
         loaded_players = json.load(players)
 
-        players.close()
+    for player_name, player_info in loaded_players.items():
 
-        for player_name, player_info in loaded_players.items():
+        guild_info = player_info.get("guild")
+        race_info = player_info.get("race")
+        skills = race_info.get("skills")
 
-            guild_info = player_info.get("guild")
-            race_info = player_info.get("race")
-            skills = race_info.get("skills")
-
-            if guild_info:
-                guild, _ = Guild.objects.get_or_create(
-                    name=guild_info.get("name"),
-                    description=guild_info.get("description")
-                )
-            else:
-                guild = None
-
-            race, _ = Race.objects.get_or_create(
-                name=race_info.get("name"),
-                description=race_info.get("description"))
-
-            for skill in skills:
-                Skill.objects.get_or_create(
-                    name=skill.get("name"),
-                    bonus=skill.get("bonus"),
-                    race=race
-                )
-
-            Player.objects.get_or_create(
-                nickname=player_name,
-                email=player_info.get("email"),
-                bio=player_info.get("bio"),
-                race=race,
-                guild=guild
+        if guild_info:
+            guild, _ = Guild.objects.get_or_create(
+                name=guild_info.get("name"),
+                description=guild_info.get("description")
             )
+        else:
+            guild = None
+
+        race, _ = Race.objects.get_or_create(
+            name=race_info.get("name"),
+            description=race_info.get("description"))
+
+        for skill in skills:
+            Skill.objects.get_or_create(
+                name=skill.get("name"),
+                bonus=skill.get("bonus"),
+                race=race
+            )
+
+        Player.objects.get_or_create(
+            nickname=player_name,
+            email=player_info.get("email"),
+            bio=player_info.get("bio"),
+            race=race,
+            guild=guild
+        )
 
 
 if __name__ == "__main__":
