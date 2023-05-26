16257f26f2e8dc222127d193c0d7caa826078a17
Viktor Rura <ruravictor@gmail.com>
2023-05-26 21:53:59 +0100

    Update code with get() and get_or_create() methods

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main() -> None:
 
     for player_name, player_data in data.items():
         guild_name = (
-            player_data["guild"]["name"]
+            player_data["guild"].get("name")
             if player_data["guild"]
             else None
         )
@@ -19,54 +19,46 @@ def main() -> None:
         else:
             guild_description = None
             guild = None
-        if guild_name and not Guild.objects.filter(
-                name=guild_name
-        ).exists():
-            guild = Guild.objects.create(
+
+        if guild_name:
+            guild, created = Guild.objects.get_or_create(
                 name=guild_name,
                 description=guild_description
             )
 
         race_name = (
-            player_data["race"]["name"]
+            player_data["race"].get("name")
             if player_data["race"]
             else None
         )
         race_description = player_data["race"].get("description")
 
-        if race_name and not Race.objects.filter(
-                name=race_name
-        ).exists():
-            race = Race.objects.create(
-                name=race_name,
-                description=race_description
-            )
+        race, created = Race.objects.get_or_create(
+            name=race_name,
+            description=race_description
+        )
 
-        for skill_data in player_data["race"]["skills"]:
+        for skill_data in player_data["race"].get("skills"):
             skill_name = skill_data["name"] if skill_data else None
             skill_bonus = skill_data.get("bonus")
-            if skill_name and not Skill.objects.filter(
-                    name=skill_name
-            ).exists():
-                Skill.objects.create(
-                    name=skill_name,
-                    bonus=skill_bonus,
-                    race=race
-                )
+
+            Skill.objects.get_or_create(
+                name=skill_name,
+                bonus=skill_bonus,
+                race=race
+            )
 
         player_nickname = player_name
-        player_email = player_data["email"]
-        player_bio = player_data["bio"]
-        if not Player.objects.filter(
-                nickname=player_nickname
-        ).exists():
-            Player.objects.create(
-                nickname=player_nickname,
-                email=player_email,
-                bio=player_bio,
-                race=race,
-                guild=guild
-            )
+        player_email = player_data.get("email")
+        player_bio = player_data.get("bio")
+
+        Player.objects.get_or_create(
+            nickname=player_nickname,
+            email=player_email,
+            bio=player_bio,
+            race=race,
+            guild=guild
+        )
 
 
 if __name__ == "__main__":
