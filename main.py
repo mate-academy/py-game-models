b0546abd66b1f49a78e13fd1f0da9c19935d88c1
Maks Blazhko <makssimm1@gmail.com>
2023-05-27 14:49:09 +0200

    use get_or_create

--- main.py
+++ main.py
@@ -9,27 +9,22 @@ def main() -> None:
         players_dict = json.load(players_file).items()
 
     for player, player_attr in players_dict:
-        if not Race.objects.filter(name=player_attr["race"]["name"]).exists():
-            Race.objects.create(
-                name=player_attr["race"]["name"],
-                description=player_attr["race"]["description"],
-            )
+        Race.objects.get_or_create(
+            name=player_attr["race"]["name"],
+            description=player_attr["race"]["description"],
+        )
 
         for skill in player_attr["race"]["skills"]:
-            if not Skill.objects.filter(name=skill["name"]).exists():
-                race = Race.objects.get(name=player_attr["race"]["name"])
-                Skill.objects.create(
-                    name=skill["name"], bonus=skill["bonus"], race=race
-                )
+            race = Race.objects.get(name=player_attr["race"]["name"])
+            Skill.objects.get_or_create(
+                name=skill["name"], bonus=skill["bonus"], race=race
+            )
 
         if player_attr.get("guild") is not None:
-            if not Guild.objects.filter(
-                name=player_attr["guild"]["name"]
-            ).exists():
-                Guild.objects.create(
-                    name=player_attr["guild"]["name"],
-                    description=player_attr["guild"]["description"],
-                )
+            Guild.objects.get_or_create(
+                name=player_attr.get("guild")["name"],
+                description=player_attr.get("guild")["description"],
+            )
 
         Player.objects.create(
             nickname=player,
