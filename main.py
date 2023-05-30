9a70800435a25b31e362e43c6f7957a4e9ca633f
Ivan <ivan.korshunov.1997@gmail.com>
2023-05-30 22:57:06 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+import json
+
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json") as data:
+        users_data = json.load(data)
+        for nickname, other_data in users_data.items():
+            Player.objects.create(
+                nickname=nickname,
+                email=other_data["email"],
+                bio=other_data["bio"]
+            )
 
 
 if __name__ == "__main__":
