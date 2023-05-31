a3bede565d94480084a96c0a66f5271e59b398d3
Ivan <ivan.korshunov.1997@gmail.com>
2023-05-31 18:11:48 +0300

    solution

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import json
 
 import init_django_orm  # noqa: F401
 
-from db.create_guild_and_race import create_race_and_guild
-from db.create_skills import create_skills
-from db.create_players import create_players
 from db.models import Player, Race, Guild, Skill  # noqa: F401
 
 
@@ -12,13 +9,33 @@ def main() -> None:
     with open("players.json") as data:
         users_data = json.load(data)
 
-        races, guilds = create_race_and_guild(users_data)
-        Race.objects.bulk_create(races)
-        Guild.objects.bulk_create(guilds)
+        for nickname, other_data in users_data.items():
+            race_data = other_data["race"]
+            race, _ = Race.objects.get_or_create(
+                name=race_data["name"],
+                description=race_data["description"]
+            )
+            guild_data = other_data["guild"]
+            skills_list = race_data["skills"]
+            for skill in skills_list:
+                Skill.objects.get_or_create(
+                    name=skill["name"],
+                    bonus=skill["bonus"],
+                    race=race
+                )
+            if guild_data:
+                guild_data, _ = Guild.objects.get_or_create(
+                    name=guild_data["name"],
+                    description=guild_data["description"]
+                )
 
-        skills = create_skills(users_data)
-        Skill.objects.bulk_create(skills)
-        create_players(users_data)
+            Player.objects.create(
+                nickname=nickname,
+                email=other_data["email"],
+                bio=other_data["bio"],
+                race=race,
+                guild=guild_data,
+            )
 
 
 if __name__ == "__main__":
