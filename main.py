be08e3edd006ea82b8a0072514d3f0ede184a9ee
Yaroslav Rybalka <markus9869@gmail.com>
2023-06-12 12:24:45 +0300

    Solution

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import datetime
-
 import init_django_orm  # noqa: F401
 import json
 
@@ -7,45 +5,42 @@ from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    with open("players.json", "r") as file:
-        data = json.load(file)
-        for player_name, player_data in data.items():
-            player = Player(
-                nickname=player_name,
-                email=player_data['email'],
-                bio=player_data['bio'],
-                created_at=datetime.datetime.now()
+    with open("players.json") as file:
+        players_data = json.load(file)
+
+    for player_name, player_data in players_data.items():
+        guild_data = player_data.get("guild")
+        guild_name = guild_data["name"] if guild_data else None
+
+        race_name = player_data["race"]["name"]
+
+        if guild_name is not None:
+            guild, _ = Guild.objects.get_or_create(
+                name=guild_name,
+                description=guild_data["description"] if guild_data else None,
             )
-            player.save()
+        else:
+            guild = None
 
-            race_data = player_data['race']
-            race = Race(
-                name=race_data['name'],
-                description=race_data['description']
+        race, _ = Race.objects.get_or_create(
+            name=race_name, description=player_data["race"]["description"]
+        )
+
+        for skill_data in player_data["race"]["skills"]:
+            skill_name = skill_data["name"]
+            skill_bonus = skill_data["bonus"]
+
+            skill, _ = Skill.objects.get_or_create(
+                name=skill_name, bonus=skill_bonus, race=race
             )
-            race.save()
 
-            player.race = race
-
-            skills_data = race_data['skills']
-            for skill_data in skills_data:
-                skill = Skill(
-                    name=skill_data['name'],
-                    bonus=skill_data['bonus'],
-                    race=race
-                )
-                skill.save()
-
-            guild_data = player_data['guild']
-            guild = Guild(
-                name=guild_data['name'],
-                description=guild_data['description']
-            )
-            guild.save()
-
-            player.guild = guild
-
-            player.save()
+        player, _ = Player.objects.get_or_create(
+            nickname=player_name,
+            email=player_data["email"],
+            bio=player_data["bio"],
+            race=race,
+            guild=guild,
+        )
 
 
 if __name__ == "__main__":
