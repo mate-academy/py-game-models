9c5f36a623ff35eb54872411d5e190582724b895
Andrii Kuzmenkov <kuzmenkov.andr@gmail.com>
2023-06-19 22:22:38 +0300

    Refactored

--- main.py
+++ main.py
@@ -9,29 +9,26 @@ def main() -> None:
         data = json.load(file)
 
     for nickname, player_data in data.items():
-        guild = player_data["guild"]
-        if guild and not Guild.objects.filter(name=guild["name"]).exists():
-            Guild.objects.create(
-                name=guild["name"],
-                description=guild["description"]
+        guild_dict = player_data["guild"]
+        guild = None
+        if guild_dict:
+            guild, is_guild_created = Guild.objects.get_or_create(
+                name=guild_dict["name"],
+                description=guild_dict["description"]
             )
-        guild = Guild.objects.get(name=guild["name"]) if guild else None
 
         race_dict = player_data["race"]
-        if not Race.objects.filter(name=race_dict["name"]).exists():
-            Race.objects.create(
-                name=race_dict["name"],
-                description=race_dict["description"]
-            )
-        race = Race.objects.get(name=race_dict["name"])
+        race, is_race_created = Race.objects.get_or_create(
+            name=race_dict["name"],
+            description=race_dict["description"]
+        )
 
         for skill in race_dict["skills"]:
-            if not Skill.objects.filter(name=skill["name"]).exists():
-                Skill.objects.create(
-                    name=skill["name"],
-                    bonus=skill["bonus"],
-                    race=race
-                )
+            Skill.objects.get_or_create(
+                name=skill["name"],
+                bonus=skill["bonus"],
+                race=race
+            )
 
         Player.objects.create(
             nickname=nickname,
