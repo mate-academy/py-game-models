e0471cc2f971be3d79df2bf1fe6149398d064af6
maccarets <maccarets@gmail.com>
2023-06-28 17:58:57 +0300

    done

--- db/models.py
+++ db/models.py
@@ -22,4 +22,6 @@ class Player(models.Model):
     email = models.EmailField(max_length=255, unique=True)
     bio = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
-    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
+    guild = models.ForeignKey(Guild,
+                              on_delete=models.SET_NULL,
+                              null=True)
--- main.py
+++ main.py
@@ -8,7 +8,8 @@ def main() -> None:
     with open("players.json") as file:
         data = json.load(file)
         for key in data:
-            if not Race.objects.filter(name=data[key]["race"]["name"]).exists():
+            if not Race.objects.filter(
+                    name=data[key]["race"]["name"]).exists():
                 race = Race(name=data[key]["race"]["name"],
                             description=data[key]["race"]["description"])
                 race.save()
@@ -24,15 +25,18 @@ def main() -> None:
             if data[key]["guild"] is None:
                 guild = None
             else:
-                if not Guild.objects.filter(name=data[key]["guild"]["name"]).exists():
-                    guild = Guild(name=data[key]["guild"]["name"],
-                                  description=data[key]["guild"]["description"])
+                if not Guild.objects.filter(
+                        name=data[key]["guild"]["name"]).exists():
+                    guild = Guild(
+                        name=data[key]["guild"]["name"],
+                        description=data[key]["guild"]["description"])
                     guild.save()
                 else:
                     guild = Guild.objects.get(name=data[key]["guild"]["name"])
 
             if not Player.objects.filter(nickname=key).exists():
-                player = Player(nickname=key, email=data[key]["email"],
+                player = Player(nickname=key,
+                                email=data[key]["email"],
                                 bio=data[key]["bio"], race=race, guild=guild)
                 player.save()
 
