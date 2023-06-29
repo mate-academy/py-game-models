f45dbb3682207107f09f54d29b171b5054ac0690
DmytroTsehelnyk <dmytrotsehelnyk@yahoo.com>
2023-06-29 16:07:59 +0300

    Solution

--- main.py
+++ main.py
@@ -1,10 +1,77 @@
+import json
+from typing import List
+
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
+def receive_player_race(race: dict[str]) -> None:
+    if not Race.objects.filter(
+        name=race["name"]
+    ).exists():
+        Race.objects.create(
+            name=race["name"],
+            description=race["description"]
+        )
+
+
+def receive_player_guild(guild: dict[str]) -> None:
+    if guild and not Guild.objects.filter(
+        name=guild["name"]
+    ).exists():
+        Guild.objects.create(
+            name=guild["name"],
+            description=guild["description"]
+        )
+
+
+def receive_race_skills(skills: List[dict], race: str) -> None:
+    for skill in skills:
+        if not Skill.objects.filter(
+            name=skill["name"]
+        ).exists():
+            Skill.objects.create(
+                name=skill["name"],
+                bonus=skill["bonus"],
+                race_id=get_race_id(race)
+            )
+
+
+def get_guild_id(guild: dict[str]) -> int | None:
+    if guild:
+        return Guild.objects.get(name=guild["name"]).id
+
+
+def get_race_id(race_name: str) -> int:
+    return Race.objects.get(name=race_name).id
+
+
 def main() -> None:
-    pass
+    with open("players.json") as file:
+        players_data = json.load(file)
+
+    for player_name, player_data in players_data.items():
+        if Player.objects.filter(
+            nickname=player_name
+        ).exists():
+            continue
+
+        receive_player_race(player_data["race"])
+        receive_player_guild(player_data["guild"])
+
+        receive_race_skills(
+            player_data["race"]["skills"],
+            player_data["race"]["name"]
+        )
+
+        Player.objects.create(
+            nickname=player_name,
+            email=player_data["email"],
+            bio=player_data["bio"],
+            race_id=get_race_id(player_data["race"]["name"]),
+            guild_id=get_guild_id(player_data["guild"])
+        )
 
 
 if __name__ == "__main__":
