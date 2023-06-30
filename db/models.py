0ad1a94d0d63a500aa59e90c1b07d55caf6af483
Diana Shyrokikh <diashiroks@gmail.com>
2023-06-30 12:27:30 +0300

    Added choices in Race.name

--- db/models.py
+++ db/models.py
@@ -2,9 +2,17 @@ from django.db import models
 
 
 class Race(models.Model):
+    RACES = [
+        ("elf", "Elf"),
+        ("dwarf", "Dwarf"),
+        ("human", "Human"),
+        ("ork", "Ork"),
+    ]
+
     name = models.CharField(
         max_length=255,
-        unique=True
+        unique=True,
+        choices=RACES
     )
     description = models.TextField(blank=True)
 
