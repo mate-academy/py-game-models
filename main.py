69ef6d7a59d349d31b1c3dd4aefb1dc254c670c8
Tarasidze <dtar84@ukr.net>
2023-07-01 21:48:29 +0300

    Solution 1.01

--- main.py
+++ main.py
@@ -7,35 +7,41 @@ FILE_NAME = "players.json"
 
 
 def main() -> None:
+
     with open(FILE_NAME, "r") as file:
         players = json.load(file)
 
-    guilds = set()
-    races = set()
-    skills = set()
-    players_list = []
-
     for nickname_, value in players.items():
-        if not Player.objects.filter(nickname=nickname_).exists():
-            Player.objects.create(nickname=nickname_,
-                                  email=value["email"],
-                                  bio=value["bio"],
-                                  )
 
         if not Race.objects.filter(name=value["race"]["name"]).exists():
             Race.objects.create(name=value["race"]["name"],
                                 description=value["race"]["description"])
 
+        player_race = Race.objects.get(name=value["race"]["name"])
+
+        if value["guild"]:
+            if not Guild.objects.filter(name=value["guild"]["name"]).exists():
+                Guild.objects.create(name=value["guild"]["name"],
+                                     description=value["guild"]["description"])
+
+            player_guild = Guild.objects.get(name=value["guild"]["name"])
+
         for skill in value["race"]["skills"]:
             if not Skill.objects.filter(name=skill["name"]).exists():
                 Skill.objects.create(name=skill["name"],
                                      bonus=skill["bonus"],
+                                     race=player_race
                                      )
 
-
-
-
-
+        if not Player.objects.filter(nickname=nickname_).exists():
+            Player.objects.create(nickname=nickname_,
+                                  email=value["email"],
+                                  bio=value["bio"],
+                                  race=player_race,
+                                  guild=player_guild
+                                  )
+        player_race = None
+        player_guild = None
 
 
 if __name__ == "__main__":
