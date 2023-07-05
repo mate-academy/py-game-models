61e8e4153420cf77635f5b546c59c3a647a91c89
maccarets <maccarets@gmail.com>
2023-07-05 15:10:05 +0300

    fixed

--- main.py
+++ main.py
@@ -5,28 +5,30 @@ import json
 
 
 def main() -> None:
-    with open("players.json") as file:
-        data = json.load(file)
-    for key in data:
+    with open("players.json") as players_data_file:
+        people = json.load(players_data_file)
+    for person in people:
         race, created = Race.objects.get_or_create(
-            name=data[key]["race"]["name"],
-            description=data[key]["race"]["description"])
+            name=people[person]["race"]["name"],
+            description=people[person]["race"]["description"]
+        )
 
-        for skill_name in data[key]["race"]["skills"]:
+        for skill_name in people[person]["race"]["skills"]:
             if not Skill.objects.filter(name=skill_name["name"]).exists():
                 Skill.objects.create(name=skill_name["name"],
                                      bonus=skill_name["bonus"], race=race)
-        if data[key]["guild"] is None:
+        if people[person]["guild"] is None:
             guild = None
         else:
             guild, created = Guild.objects.get_or_create(
-                name=data[key]["guild"]["name"],
-                description=data[key]["guild"]["description"])
+                name=people[person]["guild"]["name"],
+                description=people[person]["guild"]["description"]
+            )
 
-        if not Player.objects.filter(nickname=key).exists():
-            Player.objects.create(nickname=key,
-                                  email=data[key]["email"],
-                                  bio=data[key]["bio"],
+        if not Player.objects.filter(nickname=person).exists():
+            Player.objects.create(nickname=person,
+                                  email=people[person]["email"],
+                                  bio=people[person]["bio"],
                                   race=race,
                                   guild=guild)
 
