bb11c2134f04947f6239bd28680470b349bdd1c6
Dukh Vlad <84961728+RedCatHot@users.noreply.github.com>
2023-07-05 13:41:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -59,13 +59,9 @@ def main() -> None:
     players = load_players_from_file()
 
     for player, player_info in players.items():
-        race_info = player_info.get("race")
-        skills_info = race_info.get("skills")
-        guild_info = player_info.get("guild")
-
-        race = create_race(race_info)
-        create_skills(skills_info, race)
-        guild = create_guild(guild_info)
+        race = create_race(player_info.get("race"))
+        create_skills(player_info.get("race").get("skills"), race)
+        guild = create_guild(player_info.get("guild"))
 
         create_player(player, player_info, race, guild)
 
