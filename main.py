b184f478d22ad086dd4016f10eb66d664cd00a60
Vlad <vladislav.zhyla@gmail.com>
2023-07-05 19:24:40 +0300

    Funcs optimized

--- main.py
+++ main.py
@@ -5,20 +5,18 @@ import init_django_orm  # noqa: F401
 from db.models import Race, Skill, Player, Guild
 
 
-def create_race(race_info: dict) -> None:
-    if not Race.objects.filter(
-            name=race_info["name"]
-    ).exists():
+def create_race(race_info: dict) -> int:
+    if not Race.objects.filter(name=race_info["name"]).exists():
         Race.objects.create(
             name=race_info["name"],
             description=race_info["description"],
         )
 
+    return Race.objects.get(name=race_info["name"]).id
+
 
 def create_skill(skill_info: dict, race_id: int) -> None:
-    if not Skill.objects.filter(
-            name=skill_info["name"]
-    ).exists():
+    if not Skill.objects.filter(name=skill_info["name"]).exists():
         Skill.objects.create(
             name=skill_info["name"],
             bonus=skill_info["bonus"],
@@ -26,31 +24,30 @@ def create_skill(skill_info: dict, race_id: int) -> None:
         )
 
 
-def create_guild(guild: dict) -> None:
+def create_guild(guild: [dict, None]) -> None:
+    if not guild:
+        return None
     if not Guild.objects.filter(name=guild["name"]).exists():
         Guild.objects.create(
             name=guild["name"],
             description=guild["description"]
         )
 
+    return Guild.objects.get(name=guild["name"]).id
+
 
 def create_player(nickname: str, info: dict) -> None:
     race = info["race"]
-    create_race(race)
-    race_id = Race.objects.get(name=race["name"]).id
+    race_id = create_race(race)
     for skill in race["skills"]:
         create_skill(skill, race_id)
     guild = info["guild"]
-    if guild:
-        create_guild(guild)
     Player.objects.create(
         nickname=nickname,
         email=info["email"],
         bio=info["bio"],
         race_id=race_id,
-        guild_id=Guild.objects.get(
-            name=guild["name"]
-        ).id if guild else None
+        guild_id=create_guild(guild)
     )
 
 
