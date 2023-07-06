228025f3aa01bd7cd0431a2dba745261eaff7b60
Erik Agayan <erik.agayan1@gmail.com>
2023-07-06 11:07:18 +0300

    Solution #5

--- main.py
+++ main.py
@@ -5,21 +5,17 @@ import init_django_orm  # noqa: F401
 from db.models import Race, Skill, Player, Guild
 
 
-def main() -> None:
-    with open("players.json") as config_players:
-        players = json.load(config_players)
-
+def create_races(players: dict) -> None:
     for player_name, player_info in players.items():
-        race = player_info["race"]
-        guild = player_info["guild"]
+        race_info = player_info["race"]
 
         race, is_race_created = Race.objects.get_or_create(
-            name=race["name"],
-            description=race["description"]
+            name=race_info["name"],
+            description=race_info["description"]
         )
 
         if is_race_created:
-            for skill in player_info["race"]["skills"]:
+            for skill in race_info["skills"]:
                 skill_name = skill["name"]
                 skill_bonus = skill["bonus"]
 
@@ -29,13 +25,18 @@ def main() -> None:
                     race=race
                 )
 
-        if guild:
+
+def create_players(players: dict) -> None:
+    for player_name, player_info in players.items():
+        race = Race.objects.get(name=player_info["race"]["name"])
+        guild_info = player_info["guild"]
+        guild = None
+
+        if guild_info:
             guild, is_guild_created = Guild.objects.get_or_create(
-                name=guild["name"],
-                description=guild["description"]
+                name=guild_info["name"],
+                description=guild_info["description"]
             )
-        else:
-            guild = None
 
         Player.objects.create(
             nickname=player_name,
@@ -44,6 +45,14 @@ def main() -> None:
             race=race,
             guild=guild,
         )
+
+
+def main() -> None:
+    with open("players.json") as config_players:
+        players: dict = json.load(config_players)
+
+    create_races(players)
+    create_players(players)
 
 
 if __name__ == "__main__":
