7e7e0f93897e2cf3119898444d163659c631b75b
Vladyslav Kaminskyi <119516716+VladyslavKaminskyi@users.noreply.github.com>
2023-07-08 14:42:38 +0200

    Solution

--- main.py
+++ main.py
@@ -10,24 +10,27 @@ def main() -> None:
         players_data = json.load(file)
     for player_name, player_data in players_data.items():
         race_data = player_data.get("race")
-        race = Race.objects.get_or_create(name=race_data.get("name"),
-                                          description=race_data.get(
-                                              "description"))
+        race = Race.objects.get_or_create(
+            name=race_data.get("name"),
+            description=race_data.get("description")
+        )
         for skill in race_data.get("skills"):
-            Skill.objects.get_or_create(name=skill.get("name"),
-                                        bonus=skill.get("bonus"),
-                                        race=race[0])
+            Skill.objects.get_or_create(
+                name=skill.get("name"), bonus=skill.get("bonus"), race=race[0]
+            )
         guild = player_data.get("guild")
         if guild:
-            guild = Guild.objects.get_or_create(name=guild.get("name"),
-                                                description=guild.get(
-                                                    "description"))
+            guild = Guild.objects.get_or_create(
+                name=guild.get("name"), description=guild.get("description")
+            )
             guild = guild[0]
-        Player.objects.create(nickname=player_name,
-                              email=player_data.get("email"),
-                              bio=player_data.get("bio"),
-                              race=race[0],
-                              guild=guild)
+        Player.objects.create(
+            nickname=player_name,
+            email=player_data.get("email"),
+            bio=player_data.get("bio"),
+            race=race[0],
+            guild=guild,
+        )
 
 
 if __name__ == "__main__":
