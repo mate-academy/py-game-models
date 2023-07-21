5df4ab279bbf2d3cdbd836ae0eb54638e75b72ff
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-07-22 01:04:25 +0300

    Solutions_1.1

--- db/models.py
+++ db/models.py
@@ -5,6 +5,9 @@ class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -14,10 +17,16 @@ class Skill(models.Model):
         on_delete=models.CASCADE
     )
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Guild(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True)
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Player(models.Model):
@@ -30,3 +39,6 @@ class Player(models.Model):
     )
     guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"My name is {self.name}, my email is {self.email}"
