816c859e04fb22395e3bfe33477ea3f8eb9e2ecc
mark.shapka <mark.shapka1@gmail.com>
2023-08-12 22:00:00 +0300

    added else condition and addition variable

--- main.py
+++ main.py
@@ -16,24 +16,28 @@ def main() -> None:
         )
 
         guild = data.get("guild")
+        guild_from_db = None
         if guild is not None:
             guild_name = guild.get("name")
             if not Guild.objects.filter(name=guild_name).exists():
-                Guild.objects.create(
+                guild_from_db = Guild.objects.create(
                     name=guild_name,
                     description=guild.get("description")
                 )
-            guild_from_db = Guild.objects.get(name=guild_name)
-            new_player.guild = guild_from_db
+            else:
+                guild_from_db = Guild.objects.get(name=guild_name)
+        new_player.guild = guild_from_db
 
         race_ = data.get("race")
         race_name = race_.get("name")
+        race_from_db = None
         if not Race.objects.filter(name=race_name).exists():
             Race.objects.create(
                 name=race_name,
                 description=race_.get("description")
             )
-        race_from_db = Race.objects.get(name=race_name)
+        else:
+            race_from_db = Race.objects.get(name=race_name)
 
         skills = race_.get("skills")
         if skills is not None:
@@ -48,8 +52,6 @@ def main() -> None:
         new_player.race = race_from_db
         if not Player.objects.filter(nickname=new_player.nickname).exists():
             new_player.save()
-        print(new_player)
-        print("---------------")
 
 
 if __name__ == "__main__":
