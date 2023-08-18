40f9d6a2293a6d274f21e32c83fb2479bed3848a
Veinmax <aturjust88@gmail.com>
2023-08-18 21:04:25 +0300

    Solution3

--- main.py
+++ main.py
@@ -10,28 +10,25 @@ def main() -> None:
     for player, player_data in data.items():
         race, created = Race.objects.get_or_create(
             name=player_data["race"]["name"],
-            description=player_data["race"]["description"]
+            description=player_data["race"]["description"],
         )
 
         for skill in player_data["race"]["skills"]:
             Skill.objects.get_or_create(
-                name=skill["name"],
-                bonus=skill["bonus"],
-                race=race
+                name=skill["name"], bonus=skill["bonus"], race=race
             )
 
         guild = player_data["guild"]
         if guild:
             guild, created = Guild.objects.get_or_create(
-                name=guild["name"],
-                description=guild["description"]
+                name=guild["name"], description=guild["description"]
             )
         Player.objects.create(
             nickname=player,
             email=player_data["email"],
             bio=player_data["bio"],
             race=race,
-            guild=guild
+            guild=guild,
         )
 
 
