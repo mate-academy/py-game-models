edd3a9a9f93a25c9ecbec5b0c388c58d9705fcba
Illia Zhmurskiy <thebrawler6@gmail.com>
2023-08-28 13:43:37 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -18,7 +18,7 @@ class Guild(models.Model):
 
 
 class Player(models.Model):
-    nickname = models.CharField(max_length=255)
+    nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
