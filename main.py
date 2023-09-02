042f2b975bf58caf70d44e53cc0b93c19e51fce4
VasylTurok <127683195+VasylTurok@users.noreply.github.com>
2023-09-02 15:01:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,41 +7,34 @@ from db.models import Race, Skill, Player, Guild
 def main() -> None:
     with open("players.json") as players_file:
         players_data = json.load(players_file)
-    race_set = set()
-    skill_set = set()
-    guild_set = set()
+
     for name, player in players_data.items():
         race = player["race"]
-        if rase["name"] not in race_set:
-            race_set.add(rase["name"])
-            Race.objects.create(
-                name=rase["name"],
-                description=rase["description"]
-            )
-        rase_from_db = Race.objects.get(name=rase["name"])
-        for skill in rase["skills"]:
-            if skill["name"] not in skill_set:
-                skill_set.add(skill["name"])
-                Skill.objects.create(
+        race_obj, created = Race.objects.get_or_create(
+            name=race["name"],
+            description=race["description"]
+        )
+        if created:
+            for skill in race["skills"]:
+
+                Skill.objects.get_or_create(
                     name=skill["name"],
                     bonus=skill["bonus"],
-                    race=rase_from_db
+                    race=race_obj
                 )
 
         guild = player["guild"]
-        print(guild)
-        if guild is not None:
-            if guild["name"] not in guild_set:
-                guild_set.add(guild["name"])
-                Guild.objects.create(
-                    name=guild["name"],
-                    description=guild["description"]
-                )
-            guild = Guild.objects.get(name=guild["name"])
-        Player.objects.create(
+        if guild:
+
+            guild, created = Guild.objects.get_or_create(
+                name=guild["name"],
+                description=guild["description"]
+            )
+
+        Player.objects.get_or_create(
             nickname=name,
             email=player["email"],
             bio=player["bio"],
-            race=rase_from_db,
+            race=race_obj,
             guild=guild
         )
