7f9b04da80f4aef22fa63c69f247b056633f8074
OlegatorLE <oleh.travin@gmail.com>
2023-09-07 17:14:06 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,35 @@
 from django.db import models
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name + self.description
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(
+        "This field describes what kind of bonus players can get from it.",
+        max_length=255
+    )
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True)
+
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField(
+       "It stores a short description provided by a user about himself/herself.",
+       max_length=255
+    )
+    race = models.ForeignKey(Race, models.CASCADE)
+    guild = models.ForeignKey(Guild, models.SET_NULL, null=True)
+    created_at = models.DateField(auto_now_add=True)
--- main.py
+++ main.py
@@ -1,10 +1,53 @@
+import json
+
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json") as file:
+        data_players = json.load(file)
+
+    for nickname, data_player in data_players.items():
+        email = data_player["email"]
+        bio = data_player["bio"]
+
+        race_name = data_player["race"]["name"]
+        race_desc = data_player["race"]["description"]
+        race = Race.objects.get_or_create(
+            name=race_name,
+            description=race_desc,
+        )[0]
+
+        skills = data_player["race"]["skills"]
+        for skill in skills:
+            skill_name = skill["name"]
+            skill_bonus = skill["bonus"]
+            Skill.objects.get_or_create(
+                name=skill_name,
+                bonus=skill_bonus,
+                race=race,
+            )
+
+        if data_player["guild"]:
+            guild_name = data_player["guild"]["name"]
+            guild_desc = data_player["guild"]["description"]
+            guild = Guild.objects.get_or_create(
+                name=guild_name,
+                description=guild_desc,
+            )[0]
+        else:
+            guild = None
+
+        player = Player(
+            nickname=nickname,
+            email=email,
+            bio=bio,
+            race=race,
+            guild=guild,
+        )
+        player.save()
 
 
 if __name__ == "__main__":
