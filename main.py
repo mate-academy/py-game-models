8179c4cfae376c82f2bdb67cd4a94e26239c647b
Bohdan Kolomiiets <kb907406@gmail.com>
2023-09-11 22:40:59 +0300

    Solution(updated)

--- main.py
+++ main.py
@@ -10,43 +10,30 @@ def main() -> None:
 
     for name, info in file_data.items():
 
-        if not Race.objects.filter(name=info["race"]["name"]).exists():
-            Race.objects.create(
+        Player.objects.create(
+            nickname=name,
+            email=info["email"],
+            bio=info["bio"],
+            race=Race.objects.get_or_create(
                 name=info["race"]["name"],
                 description=info["race"]["description"]
+            )[0],
+            guild=(
+                Guild.objects.get_or_create(
+                    name=info["guild"]["name"],
+                    description=info["guild"]["description"]
+                )[0]
+                if info["guild"] else None
             )
+        )
 
         for skill in info["race"]["skills"]:
-            if not Skill.objects.filter(name=skill["name"]).exists():
-                Skill.objects.create(
+            Skill.objects.get_or_create(
                     name=skill["name"],
                     bonus=skill["bonus"],
                     race=Race.objects.get(name=info["race"]["name"])
                 )
 
-        if not info["guild"] is None:
-            if not Guild.objects.filter(name=info["guild"]["name"]).exists():
-                Guild.objects.create(
-                    name=info["guild"]["name"],
-                    description=info["guild"]["description"]
-                )
-
-        Player.objects.create(
-            nickname=name,
-            email=info["email"],
-            bio=info["bio"],
-            race=Race.objects.get(name=info["race"]["name"]),
-            guild=(
-                Guild.objects.get(name=info["guild"]["name"])
-                if info["guild"] else None
-            )
-        )
-
 
 if __name__ == "__main__":
-    Race.objects.all().delete()
-    Skill.objects.all().delete()
-    Guild.objects.all().delete()
-    Player.objects.all().delete()
-
     main()
