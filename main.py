0c37870d85cb543970dd2fde04b42b88ef1db1ff
ORENYT <106102798+ORENYT@users.noreply.github.com>
2023-09-12 22:22:23 +0300

    Removed 2 redundant methods, used get_or_create instead

--- main.py
+++ main.py
@@ -1,52 +1,18 @@
 import json
-from typing import Union
 
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
-def analyse_race(input_race: dict) -> Race:
-    """Function analyse if race exists, adding new or returning existing"""
-    unique_name = input_race["name"]
-    try:
-        return Race.objects.get(name=unique_name)
-    except Race.DoesNotExist:
-        return Race.objects.create(
-            name=unique_name,
-            description=input_race["description"]
-        )
-
-
-def analyse_skills(input_skills: dict, input_race: Race) -> list:
-    """Function add new skills in DB and return list of skills instances"""
-    skills_set = []
+def analyse_skills(input_skills: dict, input_race: Race) -> None:
+    """Function add new skills in DB"""
     for skill in input_skills:
         unique_name = skill["name"]
-        if Skill.objects.filter(name=unique_name):
-            skills_set.append(Skill.objects.get(name=unique_name))
-        else:
-            skills_set.append(
-                Skill.objects.create(
-                    name=unique_name,
-                    bonus=skill["bonus"],
-                    race=input_race
-                )
+        if not Skill.objects.filter(name=unique_name):
+            Skill.objects.create(
+                name=unique_name, bonus=skill["bonus"], race=input_race
             )
-    return skills_set
-
-
-def analyse_guild(input_guild: dict) -> Union[Race | None]:
-    """Function analyse if guild exists, adding new or returning existing"""
-    if input_guild is None:
-        return
-    unique_name = input_guild["name"]
-    if Guild.objects.filter(name=unique_name).exists():
-        return Guild.objects.get(name=unique_name)
-    return Guild.objects.create(
-        name=unique_name,
-        description=input_guild["description"]
-    )
 
 
 def main() -> None:
@@ -54,15 +20,24 @@ def main() -> None:
         users = json.load(f)
     for username in users:
         user_data = users[username]
-        race = analyse_race(user_data["race"])
+        race, _ = Race.objects.get_or_create(
+            name=user_data["race"]["name"],
+            description=user_data["race"]["description"],
+        )
         analyse_skills(user_data["race"]["skills"], race)
-        guild = analyse_guild(user_data["guild"])
+        if user_data["guild"] is None:
+            guild = None
+        else:
+            guild, _ = Guild.objects.get_or_create(
+                name=user_data["guild"]["name"],
+                description=user_data["guild"]["description"],
+            )
         Player.objects.create(
             nickname=username,
             email=user_data["email"],
             bio=user_data["bio"],
             race=race,
-            guild=guild
+            guild=guild,
         )
 
 
