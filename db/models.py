db4f72a3a7476bfbceaec909f128e0a677d1d1c7
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-09-12 20:36:52 +0300

    refactored main.py, used get_or_create method

--- db/models.py
+++ db/models.py
@@ -21,6 +21,6 @@ class Player(models.Model):
     nickname = models.CharField(unique=True, max_length=255)
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.SET_NULL, null=True)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
     guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- main.py
+++ main.py
@@ -5,11 +5,6 @@ from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    Race.objects.all().delete()
-    Skill.objects.all().delete()
-    Guild.objects.all().delete()
-    Player.objects.all().delete()
-
     with open("players.json") as file:
         players = json.load(file)
 
@@ -23,40 +18,34 @@ def main() -> None:
 
         race_name, race_description = race["name"], race["description"]
 
-        if Race.objects.filter(name=race_name).exists():
-            pass
-        else:
-            race_obj = Race.objects.create(
-                name=race_name, description=race_description
-            )
+        race_obj, _ = Race.objects.get_or_create(
+            name=race_name, description=race_description
+        )
 
         skills = race["skills"]
         for skill in skills:
             name = skill["name"]
             bonus = skill["bonus"]
-            if Skill.objects.filter(name=name).exists():
-                pass
-            else:
-                Skill.objects.create(name=name, bonus=bonus, race=race_obj)
+
+            Skill.objects.get_or_create(name=name, bonus=bonus, race=race_obj)
 
         guild = values.get("guild")
         guild_obj = None
+
         if guild:
             guild_name = guild["name"]
             guild_description = guild["description"]
-            if Guild.objects.filter(name=guild_name).exists():
-                guild_obj = Guild.objects.get(name=guild_name)
-            else:
-                guild_obj = Guild.objects.create(
-                    name=guild_name, description=guild_description
-                )
+
+            guild_obj, _ = Guild.objects.get_or_create(
+                name=guild_name, description=guild_description
+            )
 
         Player.objects.create(
             nickname=nickname,
             email=email,
             bio=bio,
             race=race_obj,
-            guild=guild_obj if guild else None,
+            guild=guild_obj,
         )
 
 
