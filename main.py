03873efe3350a5396b23e1c542d848a3817a5ac0
Viktoriia Zaichenko <vikazaibuba@gmail.com>
2023-09-14 17:00:44 +0200

    updated variables

--- main.py
+++ main.py
@@ -10,35 +10,35 @@ def main() -> None:
         players = json.load(file)
 
     for player_name, player_info in players.items():
-        race_info = player_info["race"]
-        if race_info:
-            race_info, information = Race.objects.get_or_create(
+        race = player_info["race"]
+        if race:
+            race, information = Race.objects.get_or_create(
                 name=player_info["race"]["name"],
                 description=player_info["race"]["description"]
             )
 
-        guild_info = player_info["guild"]
-        if guild_info:
-            guild_info, information = Guild.objects.get_or_create(
-                name=guild_info["name"],
-                description=guild_info["description"]
+        guild = player_info["guild"]
+        if guild:
+            guild, information = Guild.objects.get_or_create(
+                name=guild["name"],
+                description=guild["description"]
             )
 
-        skills_info = player_info["race"]["skills"]
-        if skills_info:
-            for skill in skills_info:
+        skills = player_info["race"]["skills"]
+        if skills:
+            for skill in skills:
                 Skill.objects.get_or_create(
                     name=skill["name"],
                     bonus=skill["bonus"],
-                    race=race_info
+                    race=race
                 )
 
         Player.objects.get_or_create(
             nickname=player_name,
             email=player_info["email"],
             bio=player_info["bio"],
-            race=race_info,
-            guild=guild_info
+            race=race,
+            guild=guild
         )
 
 
