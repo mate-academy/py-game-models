6f6d414d76de221eee6b40a480377f884a89ef63
Andrii Kurta <andriykurta97@gmail,com>
2023-09-17 12:05:23 +0200

    Solution

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import init_django_orm  # noqa: F401
-
 import json
 
+import init_django_orm  # noqa: F401
 from db.models import Race, Skill, Player, Guild
 
 
@@ -17,35 +16,26 @@ def main() -> None:
         race_name = player_race["name"]
         race_description = player_race["description"]
 
-        if not Race.objects.filter(name=race_name).exists():
-            race_instance = Race.objects.create(
-                name=race_name,
-                description=race_description
-            )
-        else:
-            race_instance = Race.objects.get(name=race_name)
+        race_instance, created = Race.objects.get_or_create(
+            name=race_name,
+            defaults={"description": race_description}
+        )
 
         for skill in player_race["skills"]:
             skill_name = skill["name"]
             skill_bonus = skill["bonus"]
-            if not Skill.objects.filter(name=skill_name).exists():
-                Skill.objects.create(
-                    name=skill_name,
-                    bonus=skill_bonus,
-                    race=race_instance
-                )
+            skill_instance, created = Skill.objects.get_or_create(
+                name=skill_name,
+                defaults={"bonus": skill_bonus, "race": race_instance}
+            )
 
         guild_instance = None
         if player_data["guild"]:
             guild_name = player_data["guild"]["name"]
-            if not Guild.objects.filter(name=guild_name).exists():
-                guild_description = player_data["guild"]["description"]
-                guild_instance = Guild.objects.create(
-                    name=guild_name,
-                    description=guild_description
-                )
-            else:
-                guild_instance = Guild.objects.get(name=guild_name)
+            guild_instance, created = Guild.objects.get_or_create(
+                name=guild_name,
+                defaults={"description": player_data["guild"]["description"]}
+            )
 
         Player.objects.create(
             nickname=player_name,
