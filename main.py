c83238d66f0291230d7516be0e984bb23c912d50
Illia <kononenko2109@gmail.com>
2023-09-18 21:21:46 +0300

    solution used get_or_create method

--- main.py
+++ main.py
@@ -8,27 +8,25 @@ from db.models import Race, Skill, Player, Guild
 def create_race(race_info: dict) -> int:
     race, created = Race.objects.get_or_create(
         name=race_info["name"],
-        defaults={"description": race_info["description"]},
+        description=race_info["description"],
     )
 
     return race.id
 
 
 def create_skill(skill_info: dict, race_id: int) -> None:
-    if not Skill.objects.filter(name=skill_info["name"]).exists():
-        Skill.objects.create(
-            name=skill_info["name"], bonus=skill_info["bonus"], race_id=race_id
-        )
+    Skill.objects.get_or_create(
+        name=skill_info["name"], bonus=skill_info["bonus"], race_id=race_id
+    )
 
 
 def create_guild(guild: [dict, None]) -> None:
     if not guild:
         return None
-    if not Guild.objects.filter(name=guild["name"]).exists():
-        Guild.objects.create(
-            name=guild["name"], description=guild["description"]
-        )
 
+    Guild.objects.get_or_create(
+        name=guild["name"], description=guild["description"]
+    )
     return Guild.objects.get(name=guild["name"]).id
 
 
@@ -38,12 +36,14 @@ def create_player(nickname: str, info: dict) -> None:
     for skill in race["skills"]:
         create_skill(skill, race_id)
     guild = info["guild"]
-    Player.objects.create(
+    player_obj, created = Player.objects.get_or_create(
         nickname=nickname,
-        email=info["email"],
-        bio=info["bio"],
-        race_id=race_id,
-        guild_id=create_guild(guild),
+        defaults={
+            "email": info["email"],
+            "bio": info["bio"],
+            "race_id": race_id,
+            "guild_id": create_guild(guild),
+        },
     )
 
 
@@ -52,8 +52,7 @@ def main() -> None:
         players = json.load(file)
 
     for player, info in players.items():
-        if not Player.objects.filter(nickname=player).exists():
-            create_player(player, info)
+        create_player(player, info)
 
 
 if __name__ == "__main__":
