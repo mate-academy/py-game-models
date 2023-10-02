1b88b060f32442ca93afe52caaa5f8c601eac9dc
Elena Komarova <elenkomarova4@gmail.com>
2023-10-02 18:00:31 +0300

    'Solution_2'

--- main.py
+++ main.py
@@ -1,41 +1,45 @@
-import init_django_orm  # noqa: F401
-
 import json
+import init_django_orm  # noqa: F401
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    with open("players.json", "r") as file:
-        data = json.load(file)
-    for nickname, value in data.items():
+    pass
+    with open("players.json", "r") as json_file:
+        player_data = json.load(json_file)
 
-        guild = None
+    for player_name, player_info in player_data.items():
 
-        if isinstance(value["guild"], dict):
+        guild = player_info.get("guild")
+        if guild:
             guild, created = Guild.objects.get_or_create(
-                name=value["guild"]["name"],
-                description=value["guild"]["description"]
+                name=guild["name"],
+                description=guild["description"]
             )
 
-        race, created = Race.objects.get_or_create(
-            name=value["race"]["name"],
-            description=value["race"]["description"]
-        )
+        race = player_info.get("race")
+        if race:
+            race, created = Race.objects.get_or_create(
+                name=player_info["race"]["name"],
+                description=player_info["race"]["description"]
+            )
 
-        Player.objects.get_or_create(
-            nickname=nickname,
-            email=value["email"],
-            bio=value["bio"],
+        skills = player_info["race"]["skills"]
+        if skills:
+            for skill in skills:
+                Skill.objects.get_or_create(
+                    name=skill["name"],
+                    bonus=skill["bonus"],
+                    race=race
+                )
+
+        Player.objects.create(
+            nickname=player_name,
+            email=player_info["email"],
+            bio=player_info["bio"],
             race=race,
             guild=guild
         )
-
-        for skill in value["race"]["skills"]:
-            Skill.objects.get_or_create(
-                name=skill["name"],
-                bonus=skill["bonus"],
-                race=race
-            )
 
 
 if __name__ == "__main__":
