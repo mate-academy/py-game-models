9b5f7a6561217e7011c5cec492e65b61f233e398
serhii_loboda <serhiy.loboda@gmail.com>
2023-10-03 18:06:20 +0300

    Solution: Game models.

--- db/models.py
+++ db/models.py
@@ -5,16 +5,29 @@ class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
 
+    def __str__(self) -> str:
+        return (f"name of race: {self.name}, "
+                f"description: {self.description}")
+
 
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
 
+    def __str__(self) -> str:
+        return (f"name of skill: {self.name}, "
+                f"bonus: {self.bonus}, "
+                f"race: {self.race.name}")
+
 
 class Guild(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True)
+
+    def __str__(self) -> str:
+        return (f"guild name: {self.name}, "
+                f"description: {self.description}")
 
 
 class Player(models.Model):
@@ -24,3 +37,11 @@ class Player(models.Model):
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
     guild = models.ForeignKey(Guild, null=True, on_delete=models.SET_NULL)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return (f"nickname: {self.nickname}, "
+                f"email: {self.email}, "
+                f"bio: {self.bio}, "
+                f"race: {self.race.name}, "
+                f"guild: {self.guild.name}, "
+                f"creating date: {self.created_at}")
