e847db54c7443a14f15e43a108548c74ea9c0f74
Vitalii Safian <viisaf.prog.gt@gmail.com>
2023-10-04 18:18:13 +0300

    logic fix

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import os
 
 import init_django_orm  # noqa: F401
-
 from db.models import (
     Race, Skill, Player, Guild
 )
-
 from support_funcs import (
     load_json_data,
-    fill_class_attrs,
-    create_model_item
+    model_attrs,
+    create_model_item,
+
 )
 
 
@@ -18,15 +17,14 @@ def update_race_and_skill_tables(json_data: str) -> None:
     for player_name, player_data in players.items():
         race = player_data["race"]
         race_name, skills = race["name"], race["skills"]
-        player_race = fill_class_attrs(Race,
-                                       race)
+        player_race = model_attrs(Race, race)
         create_model_item(Race, player_race)
+
         for skill in skills:
-            player_skill = fill_class_attrs(Skill,
-                                            skill)
+            race_skill = model_attrs(Skill, skill)
             current_race = Race.objects.get(name=f"{race_name}")
-            player_skill["race"] = current_race
-            create_model_item(Skill, player_skill)
+            race_skill["race"] = current_race
+            create_model_item(Skill, race_skill)
 
 
 def update_guild_table(json_data: str) -> None:
@@ -34,25 +32,24 @@ def update_guild_table(json_data: str) -> None:
     for player_name, player_data in players.items():
         guild_info = player_data["guild"]
         if isinstance(guild_info, dict):
-            guild_info = fill_class_attrs(Guild, player_data["guild"])
-            create_model_item(Guild, guild_info)
+            guild = model_attrs(Guild, guild_info)
+            create_model_item(Guild, guild)
 
 
 def update_player_table(json_data: str) -> None:
     players = load_json_data(json_data)
     for player_name, player_data in players.items():
-        player_info = fill_class_attrs(Player, player_data,
-                                       rest_keys=["created_at"])
+        player = model_attrs(Player, player_data, rest_keys="created_at")
         race_name = player_data["race"]["name"]
         if not isinstance(player_data["guild"], dict):
-            player_info.pop("guild")
+            player.pop("guild")
         else:
             guild_name = player_data["guild"]["name"]
-            player_info["guild"] = Guild.objects.get(name=guild_name)
+            player["guild"] = Guild.objects.get(name=guild_name)
 
-        player_info["nickname"] = player_name
-        player_info["race"] = Race.objects.get(name=race_name)
-        create_model_item(Player, player_info)
+        player["nickname"] = player_name
+        player["race"] = Race.objects.get(name=race_name)
+        create_model_item(Player, player)
 
 
 def main() -> None:
--- support_funcs.py
+++ support_funcs.py
@@ -1,10 +1,11 @@
+from json import load
+from typing import Type, Any
+
 from django.db import transaction
 from django.db.utils import (
     DatabaseError,
     IntegrityError
 )
-from json import load
-from typing import Type
 
 
 def load_json_data(data: str) -> dict:
@@ -12,28 +13,20 @@ def load_json_data(data: str) -> dict:
         return load(json_data)
 
 
-def write_class_attrs(attrs_dict: dict, data: dict) -> dict:
-    for data_key, data_value in data.items():
-        if data_key in attrs_dict:
-            attrs_dict[data_key] = data_value
-    return attrs_dict
+def model_attrs(model: Type, data: dict[str: Any],
+                rest_keys: str = "") -> dict:
+    fields = dict.fromkeys(
+        field.name for field in model._meta.fields
+        if field.name != 'id'
+        and field.name not in rest_keys.split()
+    )
+    for key, value in data.items():
+        if key in fields:
+            fields[key] = value
+    return fields
 
 
-def fill_class_attrs(cls: Type,
-                     data: dict,
-                     rest_keys: list[str] | None = None) -> dict:
-    cls_name_len = len(cls.__name__)
-    attrs_string = cls.__dict__["__doc__"][cls_name_len:]
-    redundant = ["(", ")", ",", "id"]
-    all_redundant = redundant if not rest_keys else redundant + rest_keys
-    for simbl in all_redundant:
-        attrs_string = attrs_string.replace(simbl, "")
-    attrs = dict.fromkeys(attrs_string.split())
-    cls_params = write_class_attrs(attrs, data)
-    return cls_params
-
-
-def create_model_item(model: Type, item_data: dict) -> None:
+def create_model_item(model: Type, item_data: dict[str: Any]) -> None:
     try:
         with transaction.atomic():
             model.objects.create(**item_data)
