8acf0194bc617564b27b270817bd3382046d23e3
Marina <marishka28023@gmail.com>
2023-10-13 11:18:10 +0200

    'Solution2'

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main() -> None:
         players_data = json.load(file)
 
     for player, data in players_data.items():
-        race_info = Race.objects.get_or_create(
+        race, _ = Race.objects.get_or_create(
             name=data["race"]["name"],
             description=data["race"]["description"]
         )
@@ -22,24 +22,24 @@ def main() -> None:
             Skill.objects.get_or_create(
                 name=skill_name,
                 bonus=skill_bonus,
-                race=race_info[0] if race_info[0] else None
+                race=race
             )
 
         guild_data = data["guild"]
         if guild_data is not None:
-            guild_info = Guild.objects.get_or_create(
+            guild, _ = Guild.objects.get_or_create(
                 name=guild_data["name"],
                 description=guild_data["description"]
             )
         else:
-            guild_info = None
+            guild = None
 
         Player.objects.create(
             nickname=player,
             email=data["email"],
             bio=data["bio"],
-            race=race_info[0],
-            guild=guild_info[0] if guild_info is not None else None
+            race=race,
+            guild=guild
         )
 
 
