fea1967e6857bdf9109844dee5fe53469556b817
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2023-10-16 12:53:48 +0300

    feat: init

--- db/models.py
+++ db/models.py
@@ -1,1 +1,23 @@
 from django.db import models
+
+
+class Rase(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(
+        "Describes what kind of bonus players can get from it.", max_length=255)
+    race = models.ForeignKey(
+        Rase, related_name="skills", on_delete=models.CASCADE)
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True)
+
+
+class Player(models.Model):
+    nickname = models
