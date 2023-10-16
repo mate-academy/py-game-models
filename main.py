6e79cf9252695ebc2cf576a72ccee260ed902787
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2023-10-16 15:45:42 +0300

    fix: after review

--- main.py
+++ main.py
@@ -7,29 +7,30 @@ from db.models import Race, Skill, Player, Guild
 def main() -> None:
     Guild.objects.all().delete()
     Race.objects.all().delete()
+    data = {}
     with open("players.json", "r") as file:
         data = json.load(file)
-        for name in data:
-            player_data = data[name]
-            player_race = player_data["race"]
-            player_guild = player_data["guild"]
-            if player_guild:
-                player_guild, _ = Guild.objects.get_or_create(
-                    **player_guild)
-            if player_race:
-                race_skills = player_race.pop("skills")
-                player_race, _ = Race.objects.get_or_create(
-                    **player_race)
-                for skill in race_skills:
-                    Skill.objects.get_or_create(**skill, race=player_race)
+    for name in data:
+        player_data = data[name]
+        player_race = player_data["race"]
+        player_guild = player_data["guild"]
+        if player_guild:
+            player_guild, _ = Guild.objects.get_or_create(
+                **player_guild)
+        if player_race:
+            race_skills = player_race.pop("skills")
+            player_race, _ = Race.objects.get_or_create(
+                **player_race)
+            for skill in race_skills:
+                Skill.objects.get_or_create(**skill, race=player_race)
 
-            player_data.pop("race")
-            player_data.pop("guild")
-            Player.objects.create(
-                **player_data, nickname=name,
-                race=player_race,
-                guild=player_guild
-            )
+        player_data.pop("race")
+        player_data.pop("guild")
+        Player.objects.create(
+            **player_data, nickname=name,
+            race=player_race,
+            guild=player_guild
+        )
 
 
 if __name__ == "__main__":
