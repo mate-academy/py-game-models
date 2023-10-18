4c4c5832c290b042b43d140e8a67c34334a65768
hanovskyiv <140009361+hanovskyiv@users.noreply.github.com>
2023-10-18 22:49:02 +0300

    Update models.py

--- db/models.py
+++ db/models.py
@@ -1,13 +1,12 @@
 from django.db import models
-from django.db.models import CharField
 
 
 class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
 
-    def __str__(self) -> CharField:
-        return self.name
+    def __str__(self) -> str:
+        return str(self.name)
 
 
 class Skill(models.Model):
@@ -15,16 +14,16 @@ class Skill(models.Model):
     bonus = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
 
-    def __str__(self) -> CharField:
-        return self.name
+    def __str__(self) -> str:
+        return str(self.name)
 
 
 class Guild(models.Model):
     name = models.CharField(max_length=255, unique=True, null=True)
     description = models.TextField(null=True)
 
-    def __str__(self) -> CharField:
-        return self.name
+    def __str__(self) -> str:
+        return str(self.name)
 
 
 class Player(models.Model):
@@ -35,5 +34,5 @@ class Player(models.Model):
     guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self) -> CharField:
-        return self.nickname
+    def __str__(self) -> str:
+        return str(self.nickname)
