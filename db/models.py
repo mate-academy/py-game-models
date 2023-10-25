c142429ece6ffe2af613f5f574d364caf4630b38
Nadiia <nadiia.developer777@gmail.com>
2023-10-25 15:07:07 +0300

    Solution-three-py-game-models

--- db/models.py
+++ db/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Race(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
 
 
