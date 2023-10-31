e15689a583c2aceaf58fc370a05ce41ffe3df53d
Anton Tkachuk <tkacuk2291@gmail.com>
2023-10-31 14:49:23 +0200

    Solutions

--- main.py
+++ main.py
@@ -8,37 +8,33 @@ from db.models import Race, Skill, Player, Guild
 def main() -> None:
     with open("players.json", "r") as data_file:
         player_storages = json.load(data_file)
-    for player in player_storages:
-        race_info = player_storages.get(player).get("race")
-        race_name = race_info.get("name")
-        race_description = race_info.get("description")
+
+    for player_name, player_data in player_storages.items():
         race, created = Race.objects.get_or_create(
-            name=race_name,
-            description=race_description
+            name=player_data.get("race").get("name"),
+            description=player_data.get("race").get("description")
         )
-        skill_storages = race_info.get("skills", [])
+
+        skill_storages = player_data.get("race").get("skills", [])
         [Skill.objects.get_or_create(
             name=skill.get("name"),
             bonus=skill.get("bonus"),
             race=race
         )
             for skill in skill_storages]
-        guilds_info = player_storages.get(player).get("guild")
-        if guilds_info:
-            guilds_name = guilds_info.get("name")
-            guilds_description = guilds_info.get("description")
+
+        if player_data.get("guild"):
             guilds, created = Guild.objects.get_or_create(
-                name=guilds_name,
-                description=guilds_description
+                name=player_data.get("guild").get("name"),
+                description=player_data.get("guild").get("description")
             )
         else:
             guilds = None
-        human_email = player_storages.get(player).get("email")
-        human_bio = player_storages.get(player).get("bio")
+
         Player.objects.create(
-            nickname=player,
-            email=human_email,
-            bio=human_bio,
+            nickname=player_name,
+            email=player_data.get("email"),
+            bio=player_data.get("bio"),
             race=race,
             guild=guilds
         )
