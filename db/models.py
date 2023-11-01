69a0b5882f476dee9788a4a5113c7c2ff299f6e1
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-11-01 15:18:15 +0200

    sol

--- db/models.py
+++ db/models.py
@@ -9,12 +9,12 @@ class Race(m.Model):
 class Skill(m.Model):
     name = m.CharField(unique=True, max_length=255)
     bonus = m.CharField(max_length=255)
-    race = m.OneToOneField(Race, on_delete=m.CASCADE)
+    race = m.ForeignKey(Race, on_delete=m.CASCADE)
 
 
 class Guild(m.Model):
     name = m.CharField(unique=True, max_length=255)
-    description = m.TextField(blank=True)
+    description = m.TextField(null=True)
 
 
 class Player(m.Model):
@@ -22,5 +22,5 @@ class Player(m.Model):
     email = m.EmailField(max_length=255)
     bio = m.CharField(max_length=255)
     race = m.ForeignKey(Race, on_delete=m.CASCADE)
-    guild = m.ForeignKey(Guild, on_delete=m.SET_NULL)
+    guild = m.ForeignKey(Guild, on_delete=m.SET_NULL, null=True)
     created_at = m.DateTimeField(auto_now_add=True)
--- main.py
+++ main.py
@@ -5,59 +5,43 @@ import init_django_orm  # noqa: F401
 from db.models import Race, Skill, Player, Guild
 
 
-def get_user_info(user_data):
-    race = user_data.get("race", {}).get("name") if user_data.get("race") else None
-    guild = user_data.get("guild", {}).get("name") if user_data.get("guild") else None
-    skills = [skill["name"] for skill in user_data.get("race", {}).get("skills", [])] if user_data.get("race") else []
-    return race, guild, skills
-
-
 def main() -> None:
 
     with open("players.json") as file:
         data = json.load(file)
 
-    races = set()
-    guilds = set()
-    skills = set()
-    for username, user_data in data.items():
-        race, guild, skill = get_user_info(user_data)
+    for nickname, user_data in data.items():
 
+        race = user_data.get("race")
         if race:
-            races.add(race)
+            race, _ = Race.objects.get_or_create(
+                name=race.get("name"),
+                description=race.get("description")
+            )
+
+        guild = user_data.get("guild")
         if guild:
-            guilds.add(guild)
-        skills.update(skill)
+            guild, _ = Guild.objects.get_or_create(
+                name=guild.get("name"),
+                description=guild.get("description")
+            )
 
+        skills = user_data.get("race").get("skills")
+        if skills:
+            for skill in skills:
+                Skill.objects.get_or_create(
+                    name=skill.get("name"),
+                    bonus=skill.get("bonus"),
+                    race=race
+                )
 
-
-
-
-
-
-
-obj, created = Person.objects.get_or_create(
-    first_name="John",
-    last_name="Lennon",
-    defaults={"birthday": date(1940, 10, 9)},
-)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        Player.objects.create(
+            nickname=nickname,
+            email=user_data.get("email"),
+            bio=user_data.get("bio"),
+            race=race,
+            guild=guild
+        )
 
 
 if __name__ == "__main__":
