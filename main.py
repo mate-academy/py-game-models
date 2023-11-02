48f9fb1e6acf8720d1a6f85981d57dcd5e0f9021
Ruslan Misko <miskoruslan6@gmail.com>
2023-11-02 15:37:41 +0200

    Fix Solution

--- main.py
+++ main.py
@@ -10,36 +10,46 @@ def main() -> None:
         players = json.load(json_file)
 
     for key_nickname, value in players.items():
-        race = value.get("race")
-        if race:
-            race, unpack_bool = Race.objects.get_or_create(
-                name=race.get("name"),
-                description=race.get("description")
+        race_data = value.get("race")
+        if race_data:
+            race, created = Race.objects.get_or_create(
+                name=race_data.get("name"),
+                description=race_data.get("description")
             )
 
-        guild = value.get("guild")
-        if guild:
-            guild, unpack_bool = Guild.objects.get_or_create(
-                name=guild.get("name"),
-                description=guild.get("description")
+            if created:
+                race.save()
+
+        guild_data = value.get("guild")
+        if guild_data:
+            guild, created = Guild.objects.get_or_create(
+                name=guild_data.get("name"),
+                description=guild_data.get("description")
             )
+
+            if created:
+                guild.save()
 
         skills = value.get("race").get("skills")
         if skills:
-            for skill in skills:
-                Skill.objects.get_or_create(
-                    name=skill.get("name"),
-                    bonus=skill.get("bonus"),
+            for skill_data in skills:
+                skill, created = Skill.objects.get_or_create(
+                    name=skill_data.get("name"),
+                    bonus=skill_data.get("bonus"),
                     race=race
                 )
 
-        Player.objects.create(
+                if created:
+                    skill.save()
+
+        player = Player.objects.create(
             nickname=key_nickname,
             email=value.get("email"),
             bio=value.get("bio"),
             race=race,
-            guild=guild
+            guild=guild if guild_data else None
         )
+        player.save()
 
 
 if __name__ == "__main__":
