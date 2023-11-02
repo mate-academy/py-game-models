9bfa5468f2660392aa30effb17b94ea026e6787c
Severyn Krok <sevkrok@gmail.com>
2023-11-02 21:13:36 +0200

    Implement py-game-models

--- db/models.py
+++ db/models.py
@@ -14,8 +14,7 @@ class Skill(models.Model):
     bonus = models.CharField(max_length=255)
     race = models.ForeignKey(
         Race,
-        on_delete=models.CASCADE,
-        related_name="skills"
+        on_delete=models.CASCADE
     )
 
     def __str__(self) -> str:
