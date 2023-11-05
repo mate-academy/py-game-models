6d16c4294527748e83f2742e0b955657398773e6
Andrew <67094594+MishkaGAMII@users.noreply.github.com>
2023-11-05 13:18:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,36 +10,34 @@ def main() -> None:
         players = json.load(file)
 
     for nickname, player_data in players.items():
-        player_race = player_data.get("race")
-        player_guild = player_data.get("guild")
-        race_skills = player_race.get("skills")
+        email, bio, race, guild = player_data.values()
 
         race_instance, created = Race.objects.get_or_create(
-            name=player_race["name"],
-            description=player_race["description"]
+            name=race.get("name"),
+            description=race.get("description")
         )
 
-        if race_skills:
-            for skill in race_skills:
+        skills = race.get("skills")
+        if skills:
+            for skill in skills:
                 Skill.objects.get_or_create(
                     name=skill["name"],
                     bonus=skill["bonus"],
                     race=race_instance
                 )
 
-        if player_guild:
-            for guild in player_guild:
-                guild_instance, created = Guild.objects.get_or_create(
-                    name=guild["name"],
-                    description=guild["description"]
-                )
+        if guild:
+            guild_instance, created = Guild.objects.get_or_create(
+                name=guild.get("name"),
+                description=guild.get("description")
+            )
         else:
             guild_instance = None
 
         Player.objects.get_or_create(
             nickname=nickname,
-            email=player_data["email"],
-            bio=player_data["bio"],
+            email=email,
+            bio=bio,
             race=race_instance,
             guild=guild_instance
         )
