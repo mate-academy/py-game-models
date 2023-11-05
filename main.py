1aa4eac28472eeae0223816c64bced8581c13816
Mikhail <tiunovmixs@gmail.com>
2023-11-05 15:43:38 +0200

    Add unique field and auto_now_add options and use .get method

--- main.py
+++ main.py
@@ -4,38 +4,38 @@ import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
-with open("players.json", "r") as file:
-    data = json.load(file)
-
 
 def main() -> None:
+    with open("players.json", "r") as file:
+        data = json.load(file)
+
     for nickname, data_player in data.items():
-        race_data = data_player["race"]
+        race_data = data_player.get("race")
 
         race, _ = Race.objects.get_or_create(
-            name=race_data["name"],
-            defaults={"description": race_data["description"]}
+            name=race_data.get("name"),
+            defaults={"description": race_data.get("description")}
         )
 
         for skill in race_data["skills"]:
             Skill.objects.get_or_create(
-                name=skill["name"],
-                defaults={"bonus": skill["bonus"], "race": race}
+                name=skill.get("name"),
+                defaults={"bonus": skill.get("bonus"), "race": race}
             )
 
         guild = None
         if data_player["guild"] is not None:
-            guild_data = data_player["guild"]
+            guild_data = data_player.get("guild")
             guild, _ = Guild.objects.get_or_create(
-                name=guild_data["name"],
+                name=guild_data.get("name"),
                 defaults={"description": guild_data.get("description")}
             )
 
         Player.objects.get_or_create(
             nickname=nickname,
             defaults={
-                "email": data_player["email"],
-                "bio": data_player["bio"],
+                "email": data_player.get("email"),
+                "bio": data_player.get("bio"),
                 "race": race,
                 "guild": guild,
             }
