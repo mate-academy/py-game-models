95e71d1e16da305895f955a0a839ff796d6e8b7d
Andrew <67094594+MishkaGAMII@users.noreply.github.com>
2023-11-06 18:18:28 +0300

    Black.py

--- db/migrations/0001_initial.py
+++ db/migrations/0001_initial.py
@@ -5,48 +5,65 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
-
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Guild',
+            name="Guild",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=255, unique=True)),
-                ('description', models.TextField(null=True)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+                    ),
+                ),
+                ("name", models.CharField(max_length=255, unique=True)),
+                ("description", models.TextField(null=True)),
             ],
         ),
         migrations.CreateModel(
-            name='Race',
+            name="Race",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=255, unique=True)),
-                ('description', models.TextField(blank=True)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+                    ),
+                ),
+                ("name", models.CharField(max_length=255, unique=True)),
+                ("description", models.TextField(blank=True)),
             ],
         ),
         migrations.CreateModel(
-            name='Player',
+            name="Player",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('nickname', models.CharField(max_length=255, unique=True)),
-                ('email', models.EmailField(max_length=255)),
-                ('bio', models.CharField(max_length=255)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('guild', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='db.guild')),
-                ('race', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='db.race')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+                    ),
+                ),
+                ("nickname", models.CharField(max_length=255, unique=True)),
+                ("email", models.EmailField(max_length=255)),
+                ("bio", models.CharField(max_length=255)),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("guild", models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to="db.guild")),
+                ("race", models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to="db.race")),
             ],
         ),
         migrations.CreateModel(
-            name='Skill',
+            name="Skill",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=255, unique=True)),
-                ('bonus', models.CharField(max_length=255)),
-                ('race', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='db.race')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+                    ),
+                ),
+                ("name", models.CharField(max_length=255, unique=True)),
+                ("bonus", models.CharField(max_length=255)),
+                ("race", models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to="db.race")),
             ],
         ),
     ]
