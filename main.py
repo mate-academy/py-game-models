141ef8c289070e0561750be554bd14b7538a5085
Olgierrd <oolgierrd@gmail.com>
2023-11-20 16:53:52 +0100

    Solution

--- main.py
+++ main.py
@@ -10,13 +10,29 @@ def main() -> None:
         players_data = json.load(f)
 
     for player_name, player_info in players_data.items():
-        race, _ = Race.objects.get_or_create(name=player_info["race"]["name"], description=player_info["race"]["description"])
-        guild, _ = Guild.objects.get_or_create(name=player_info['guild']['name'], description=player_info["guild"]["description"]) if player_info['guild'] else (None, False)
+        race, _ = Race.objects.get_or_create(
+            name=player_info["race"]["name"],
+            description=player_info["race"]["description"]
+        )
+        guild, _ = Guild.objects.get_or_create(
+            name=player_info["guild"]["name"],
+            description=player_info["guild"]["description"]
+        ) if player_info["guild"] else (None, False)
 
         for skill in player_info["race"]["skills"]:
-            Skill.objects.get_or_create(name=skill["name"], bonus=skill["bonus"], race=race)
+            Skill.objects.get_or_create(
+                name=skill["name"],
+                bonus=skill["bonus"],
+                race=race
+            )
 
-        Player.objects.create(nickname=player_name, email=player_info["email"], bio=player_info["bio"], race=race, guild=guild)
+        Player.objects.create(
+            nickname=player_name,
+            email=player_info["email"],
+            bio=player_info["bio"],
+            race=race,
+            guild=guild
+        )
 
 
 if __name__ == "__main__":
