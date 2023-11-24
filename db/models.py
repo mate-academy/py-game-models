2d47655eefbe502f896abbd712392125f496c7f1
Andriy_Shumskyy <andshumskyy@gmail.com>
2023-11-24 21:02:06 +0200

    solution2

--- db/models.py
+++ db/models.py
@@ -12,16 +12,25 @@ class Race(models.Model):
     name = models.CharField(max_length=255, choices=RACE_CHOICES, unique=True)
     description = models.TextField(blank=True)
 
+    def __str__(self) -> str:
+        return f"{self.name} {self.description}"
+
 
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
 
+    def __str__(self) -> str:
+        return f"{self.name} {self.bonus} {self.race}"
+
 
 class Guild(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True)
+
+    def __str__(self) -> str:
+        return f"{self.name} {self.description}"
 
 
 class Player(models.Model):
@@ -31,3 +40,7 @@ class Player(models.Model):
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
     guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return (f"{self.nickname} {self.email} {self.bio} \n "
+                f"{self.race} {self.guild} {self.created_at}")
