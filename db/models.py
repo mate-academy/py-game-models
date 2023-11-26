743174b47b1b4dc8206454943dd0b8c66ea2b469
Vadym Tiron <tironvadim1@gmail.com>
2023-11-26 18:00:15 +0200

    migrations added

--- db/models.py
+++ db/models.py
@@ -5,16 +5,25 @@ class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
 
+    def __str__(self) -> str:
+        return f"Race: {self.name}, Description: {self.description}"
+
 
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
 
+    def __str__(self) -> str:
+        return f"Skill: {self.name}, Bonus: {self.bonus}, Race: {self.race.name}"
+
 
 class Guild(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True)
+
+    def __str__(self) -> str:
+        return f"Guild: {self.name}, Description: {self.description}"
 
 
 class Player(models.Model):
@@ -24,3 +33,7 @@ class Player(models.Model):
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
     guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return (f"{self.nickname}, {self.email}, {self.bio},"
+                f"{self.race.name}, {self.guild.name},{self.created_at}")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import init_django_orm  # noqa: F401
 import json
+
 from django.utils import timezone
 import datetime
 
@@ -12,22 +13,23 @@ def main() -> None:
         players_data = json.load(file)
 
     for player_name, player_data in players_data.items():
+
         race_data = player_data.get("race")
         if race_data:
-            race, created = Race.objects.get_or_create(
+            race, _ = Race.objects.get_or_create(
                 name=race_data.get("name"),
                 defaults={"description": race_data.get("description")}
             )
 
-            skills_data = race_data.get("skills")
-            for skill_data in skills_data:
-                Skill.objects.get_or_create(
-                    name=skill_data.get("name"),
-                    defaults={"bonus": skill_data.get("bonus"), "race": race})
+        skills_data = race_data.get("skills")
+        for skill_data in skills_data:
+            Skill.objects.get_or_create(
+                name=skill_data.get("name"),
+                defaults={"bonus": skill_data.get("bonus"), "race": race})
 
         guild_data = player_data.get("guild")
         if guild_data:
-            guild, created = Guild.objects.get_or_create(
+            guild, _ = Guild.objects.get_or_create(
                 name=guild_data.get("name"),
                 defaults={"description": guild_data.get("description")})
 
