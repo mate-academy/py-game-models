ff361c598309e679c5c884c2d9bc885e99a6920a
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-11-30 19:33:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,33 +8,33 @@ from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    with open('players.json') as f:
+    with open("players.json") as f:
         players_data = json.load(f)
 
     with transaction.atomic():
         for player_data in players_data:
             race, created = Race.objects.get_or_create(
-                name=player_data['race']['name'],
-                defaults={'description': player_data['race']['description']}
+                name=player_data["race"]["name"],
+                defaults={"description": player_data["race"]["description"]}
             )
 
             guild, created = Guild.objects.get_or_create(
-                name=player_data['guild']['name'],
-                defaults={'description': player_data['guild']['description']}
+                name=player_data["guild"]["name"],
+                defaults={"description": player_data["guild"]["description"]}
             )
 
             player, created = Player.objects.get_or_create(
-                nickname=player_data['nickname'],
-                email=player_data['email'],
-                bio=player_data['bio'],
+                nickname=player_data["nickname"],
+                email=player_data["email"],
+                bio=player_data["bio"],
                 race=race,
                 guild=guild
             )
 
-            for skill_data in player_data['skills']:
+            for skill_data in player_data["skills"]:
                 skill, created = Skill.objects.get_or_create(
-                    name=skill_data['name'],
-                    bonus=skill_data['bonus'],
+                    name=skill_data["name"],
+                    bonus=skill_data["bonus"],
                     race=race
                 )
 
