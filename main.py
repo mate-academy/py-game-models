06d33858b8c3cfd64b28f6f8d6e29a1dedddf3cc
Ostap <ostapbojko04@gmail.com>
2023-12-11 12:09:38 +0200

    py-game(1(not fixed))

--- main.py
+++ main.py
@@ -1,10 +1,54 @@
-import init_django_orm  # noqa: F401
-
-from db.models import Race, Skill, Player, Guild
+import json
+from db.models import Race, Skill, Guild, Player
+from django.db.utils import IntegrityError
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r") as file:
+        player_data = json.load(file)
+
+    data_list = [
+        {
+            "model": Guild,
+            "name": "Example Guild",
+            "defaults": {"description": "Example Guild Description"
+                         }},
+        {
+            "model": Race,
+            "name": "Example Race",
+            "defaults": {"description": "Example Race Description"}
+        },
+        {
+            "model": Skill,
+            "name": "Example Skill",
+            "bonus": "Example Bonus",
+            "race": "Example Race"
+        },
+    ]
+
+    instances = {}
+    for data in data_list:
+        model = data["model"]
+        name = data["name"]
+        defaults = data.get("defaults", {})
+        instance, created = (model.objects.get_or_create
+                             (name=name, defaults=defaults))
+        instances[model] = instance
+
+    for player_info in player_data:
+        try:
+            player = Player.objects.create(
+                nickname=player_info["nickname"],
+                email=player_info["email"],
+                bio=player_info["bio"],
+                race=instances[Race],
+                guild=instances[Guild]
+            )
+            player.save()
+            print(f"Player {player.nickname} added to the database.")
+        except IntegrityError:
+            print(f"Player {player_info['nickname']}"
+                  f" already exists in the database.")
 
 
 if __name__ == "__main__":
