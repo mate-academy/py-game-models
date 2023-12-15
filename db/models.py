749aee8a8fa5ac1c4f45ea3298b96708236c0a90
Ostap <ostapbojko04@gmail.com>
2023-12-15 20:56:04 +0200

    py-game(2(fixed))

--- db/models.py
+++ db/models.py
@@ -18,10 +18,7 @@ class Guild(models.Model):
 
 
 class Player(models.Model):
-    nickname = models.CharField(
-        max_length=255,
-        unique=True
-    )
+    nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
