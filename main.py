7eb985a94ce64b25ae2878961ba6f09967109fef
Bohdan Horbachov <bgbro.inc@gmail.com>
2023-12-25 12:47:01 +0200

    cleared task

--- main.py
+++ main.py
@@ -1,10 +1,52 @@
-import init_django_orm  # noqa: F401
+import init_django_orm  # noqa
 
-from db.models import Race, Skill, Player, Guild
+import json
+from db.models import Race, Skill, Guild, Player
+
+
+def create_race(data: dict) -> Race:
+    race_name = data["name"]
+    race, created = Race.objects.get_or_create(
+        name=race_name, defaults={"description": data.get("description", "")}
+    )
+
+    skills_data = data.get("skills", [])
+    for skill_data in skills_data:
+        skill, _ = Skill.objects.get_or_create(
+            name=skill_data["name"],
+            defaults={"bonus": skill_data["bonus"], "race": race}
+        )
+
+    return race
+
+
+def create_guild(data: dict) -> Guild:
+    if data is None:
+        return None
+    guild_name = data["name"]
+    guild, _ = Guild.objects.get_or_create(
+        name=guild_name, defaults={"description": data.get("description", "")}
+    )
+    return guild
 
 
 def main() -> None:
-    pass
+    with open("players.json") as f:
+        players_data = json.load(f)
+
+    for player_name, player_data in players_data.items():
+        race = create_race(player_data["race"])
+        guild = create_guild(player_data["guild"])
+
+        player, created = Player.objects.get_or_create(
+            nickname=player_name,
+            defaults={
+                "email": player_data["email"],
+                "bio": player_data["bio"],
+                "race": race,
+                "guild": guild
+            }
+        )
 
 
 if __name__ == "__main__":
