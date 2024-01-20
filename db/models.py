c099b754927f7bb59816881c312367f5cd085c59
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-01-20 19:11:54 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,29 @@
 from django.db import models
+
+RACE_ITEMS = (("E", "Elf"), ("D", "Dwarf"),
+              ("H", "Human"), ("O", "Ork"))
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=255, unique=True, choices=RACE_ITEMS)
+    description = models.TextField(blank=True)
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(max_length=255, unique=True)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True)
+
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now=True)
--- main.py
+++ main.py
@@ -1,10 +1,46 @@
 import init_django_orm  # noqa: F401
+import os
+import json
 
 from db.models import Race, Skill, Player, Guild
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 def main() -> None:
-    pass
+    with open(os.path.join(BASE_DIR, "db/tests/players.json")) as players:
+        list_of_items = json.load(players)
+
+        for nickname_, player_dict in list_of_items.items():
+            email_ = player_dict["email"]
+            bio_ = player_dict["bio"]
+
+            if player_dict["guild"]:
+                Guild.objects.get_or_create(**player_dict["guild"])
+                guild_ = Guild.objects.get(**player_dict["guild"])
+            else:
+                guild_ = None
+
+            Race.objects.get_or_create(
+                name=player_dict["race"]["name"],
+                description=player_dict["race"]["description"]
+            )
+
+            race_ = Race.objects.get(
+                name=player_dict["race"]["name"],
+                description=player_dict["race"]["description"]
+            )
+
+            for skill in player_dict["race"]["skills"]:
+                Skill.objects.get_or_create(**skill, race=race_)
+
+            Player.objects.get_or_create(
+                nickname=nickname_,
+                email=email_,
+                bio=bio_,
+                race=race_,
+                guild=guild_
+            )
 
 
 if __name__ == "__main__":
