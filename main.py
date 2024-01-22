5d4c9a9934e6a62658e5c12fa0ea4f64c0324ffc
Olha Rozdorozhna <olga.rozdorozhna@gmail.com>
2024-01-22 15:32:59 +0200

    solution

--- main.py
+++ main.py
@@ -13,40 +13,31 @@ def main() -> None:
         players_data = json.load(f)
 
     for player_name, player_data in players_data.items():
-        nickname = player_name
-        email = player_data["email"]
-        bio = player_data["bio"]
         race_data = player_data["race"]
-        race_name = race_data["name"]
-        race_description = race_data["description"]
         if player_data["guild"]:
             guild_data = player_data["guild"]
-            guild_name = guild_data["name"]
-            guild_description = guild_data["description"] \
-                if guild_data["description"]\
-                else None
         race_instance, _ = Race.objects.get_or_create(
-            name=race_name,
-            description=race_description
+            name=player_data["race"]["name"],
+            description=player_data["race"]["description"]
         )
         guild_instance, _ = Guild.objects.get_or_create(
-            name=guild_name,
-            description=guild_description
+            name=guild_data["name"],
+            description=guild_data["description"]
+            if guild_data["description"]
+            else None
         )
         if "skills" in race_data:
-            skills_data = race_data["skills"]
+            skills_data = player_data["race"]["skills"]
             for skill_data in skills_data:
-                skill_name = skill_data["name"]
-                skill_bonus = skill_data["bonus"]
                 skill, _ = Skill.objects.get_or_create(
-                    name=skill_name,
-                    bonus=skill_bonus,
+                    name=skill_data["name"],
+                    bonus=skill_data["bonus"],
                     race=race_instance
                 )
         Player.objects.get_or_create(
-            nickname=nickname,
-            email=email,
-            bio=bio,
+            nickname=player_name,
+            email=player_data["email"],
+            bio=player_data["bio"],
             race=race_instance,
             guild=guild_instance if player_data["guild"] else None,
             defaults={"created_at": datetime.now()}
