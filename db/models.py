7c166a96d3ce18023d56f68db2a4d2da2ff851c5
MaxymChyncha <max.chyncha@gmail.com>
2024-02-09 17:13:21 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,55 @@
 from django.db import models
+
+
+class Race(models.Model):
+    races = (
+        ("elf", "Elf"),
+        ("dwarf", "Dwarf"),
+        ("human", "Human"),
+        ("ork", "Ork"),
+    )
+    name = models.CharField(max_length=255, choices=races, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.name}. Description: {self.description}."
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(max_length=255)
+    race = models.ForeignKey(
+        Race,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self) -> str:
+        return f"{self.name} belongs to {self.race.name}."
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True)
+
+    def __str__(self) -> str:
+        return f"{self.name}. Description: {self.description}."
+
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField(max_length=255)
+    race = models.ForeignKey(
+        Race,
+        on_delete=models.CASCADE,
+        related_name="players"
+    )
+    guild = models.ForeignKey(
+        Guild,
+        on_delete=models.SET_NULL,
+        related_name="members", null=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.nickname}. Guild: {self.guild}."
--- main.py
+++ main.py
@@ -1,10 +1,44 @@
+import json
+
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
+def get_data_from_file() -> dict:
+    with open("players.json", "r") as source_file:
+        return json.load(source_file)
+
+
 def main() -> None:
-    pass
+    players_data = get_data_from_file()
+    for name, data in players_data.items():
+        race, _ = Race.objects.get_or_create(
+            name=data.get("race").get("name"),
+            description=data.get("race").get("description")
+        )
+
+        if skills := data.get("race").get("skills"):
+            for skill in skills:
+                Skill.objects.get_or_create(
+                    name=skill.get("name"),
+                    bonus=skill.get("bonus"),
+                    race=race
+                )
+
+        if guild := data.get("guild"):
+            guild, _ = Guild.objects.get_or_create(
+                name=guild.get("name"),
+                description=guild.get("description")
+            )
+
+        Player.objects.get_or_create(
+            nickname=name,
+            email=data.get("email"),
+            bio=data.get("bio"),
+            race=race,
+            guild=guild
+        )
 
 
 if __name__ == "__main__":
