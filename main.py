fc63de5a36536402d89e71b45afa684f55b7b1ae
Andrii Zubchenko <dissom1987@gmail.com>
2024-02-12 13:44:57 +0200

    Changed

--- main.py
+++ main.py
@@ -16,25 +16,22 @@ def main() -> None:
         guild_data = player_data.get("guild")
 
         race, created = Race.objects.get_or_create(
-                    name=race_data.get("name"),
-                    description=race_data.get("description"),
-                )
+            name=race_data.get("name"),
+            description=race_data.get("description"),
+        )
         if race_data.get("skills"):
-                    for skill_data in race_data.get("skills"):
-                        skill, created = Skill.objects.get_or_create(
-                            name=skill_data.get("name"),
-                            bonus=skill_data.get("bonus"),
-                            race=race,
-                        )
+            for skill_data in race_data.get("skills"):
+                skill, created = Skill.objects.get_or_create(
+                    name=skill_data.get("name"),
+                    bonus=skill_data.get("bonus"),
+                    race=race,
+                )
 
-
-        if guild_data:
-                guild, created = Guild.objects.get_or_create(
+        guild, created = (
+            Guild.objects.get_or_create(
                 name=guild_data.get("name"),
                 description=guild_data.get("description"),
-                    )
-        else:
-            guild = None
+            ) if guild_data else (None, True))
 
         player, created = Player.objects.get_or_create(
             nickname=player_name,
@@ -43,8 +40,6 @@ def main() -> None:
             race=race,
             guild=guild,
         )
-
-
 
 
 if __name__ == "__main__":
