0d5274c7329a778d878b90c467eb0f269a65c9b5
Andrii Zubchenko <dissom1987@gmail.com>
2024-02-12 13:37:01 +0200

    Changed

--- main.py
+++ main.py
@@ -8,17 +8,18 @@ def main() -> None:
     try:
         with open("players.json", "r") as file:
             players_data = json.load(file)
+    except FileNotFoundError:
+        raise Exception("File not found!")
 
-            for player_name, player_data in players_data.items():
-                race_data = player_data.get("race")
-                guild_data = player_data.get("guild")
+    for player_name, player_data in players_data.items():
+        race_data = player_data.get("race")
+        guild_data = player_data.get("guild")
 
-                race, created = Race.objects.get_or_create(
+        race, created = Race.objects.get_or_create(
                     name=race_data.get("name"),
                     description=race_data.get("description"),
                 )
-
-                if race_data.get("skills"):
+        if race_data.get("skills"):
                     for skill_data in race_data.get("skills"):
                         skill, created = Skill.objects.get_or_create(
                             name=skill_data.get("name"),
@@ -26,24 +27,24 @@ def main() -> None:
                             race=race,
                         )
 
-                if guild_data:
-                    guild, created = Guild.objects.get_or_create(
-                        name=guild_data.get("name"),
-                        description=guild_data.get("description"),
+
+        if guild_data:
+                guild, created = Guild.objects.get_or_create(
+                name=guild_data.get("name"),
+                description=guild_data.get("description"),
                     )
-                else:
-                    guild = None
+        else:
+            guild = None
 
-                player, created = Player.objects.get_or_create(
-                    nickname=player_name,
-                    email=player_data["email"],
-                    bio=player_data["bio"],
-                    race=race,
-                    guild=guild,
-                )
+        player, created = Player.objects.get_or_create(
+            nickname=player_name,
+            email=player_data["email"],
+            bio=player_data["bio"],
+            race=race,
+            guild=guild,
+        )
 
-    except FileNotFoundError:
-        raise Exception("File not found!")
+
 
 
 if __name__ == "__main__":
