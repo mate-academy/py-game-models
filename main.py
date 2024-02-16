f8a197f97fba004e5f562e4995dc7f3c5b0f6c8a
Valeriia Tymchenko <maksimova.tymchenko@gmail.com>
2024-02-16 18:37:14 +0200

    Solution

--- main.py
+++ main.py
@@ -9,27 +9,27 @@ def main() -> None:
     with open("players.json", "r") as f:
         data = json.load(f)
 
-        for key, value in data.items():
+        for player, player_data in data.items():
             race, _ = Race.objects.get_or_create(
-                name=value["race"]["name"],
-                description=value["race"].get("description"))
+                name=player_data["race"]["name"],
+                description=player_data["race"].get("description"))
 
-            if value["guild"]:
+            if player_data.get("guild"):
                 guild, _ = Guild.objects.get_or_create(
-                    name=value.get("guild").get("name"),
-                    description=value["guild"].get("description"))
+                    name=player_data.get("guild").get("name"),
+                    description=player_data["guild"].get("description"))
             else:
                 guild = None
 
-            for skill in value["race"]["skills"]:
+            for skill in player_data["race"]["skills"]:
                 Skill.objects.get_or_create(name=skill["name"],
                                             bonus=skill["bonus"],
                                             race=race)
 
             Player.objects.create(
-                nickname=key,
-                email=value["email"],
-                bio=value["bio"],
+                nickname=player,
+                email=player_data["email"],
+                bio=player_data["bio"],
                 race=race,
                 guild=guild if guild else None,
                 created_at=datetime.now())
