a283e0de2ae488eee56cb7860173b2512ee4ec72
Maksym Hovorunenko <govorunenko@gmail.com>
2024-02-18 17:40:56 +0200

    Solution

--- create_funcs/create_player.py
+++ create_funcs/create_player.py
@@ -1,21 +1,28 @@
 from datetime import datetime
 
-from data_from_players.get_player_data import get_player_data
-
-from db.models import Player
-from players_json import players_data
+from db.models import Player, Race, Guild
 
 
-def create_player() -> Player:
-    player_lst = get_player_data(players_data)
-    for player in player_lst:
-        print(player)
+def create_player(players: dict) -> Player:
+    guild = None
+    for player in players:
+        nickname = player
+        email = players[player]["email"]
+        bio = players[player]["bio"]
+        race = Race.objects.get(name=players[player]["race"]["name"])
+        if players[player]["guild"]:
+            if players[player]["guild"]["name"]:
+                guild = Guild.objects.get(
+                    name=players[player]["guild"]["name"]
+                )
+        else:
+            guild = None
         Player.objects.get_or_create(
-            nickname=player[0],
-            email=player[1],
-            bio=player[2],
-            race=player[3],
-            guild=player[4],
+            nickname=nickname,
+            email=email,
+            bio=bio,
+            race=race,
+            guild=guild,
             created_at=datetime.now(),
         )
     return Player.objects
--- main.py
+++ main.py
@@ -6,13 +6,14 @@ from create_funcs.create_race import create_race
 from create_funcs.create_skill import create_skill
 from create_funcs.create_guild import create_guild
 from create_funcs.create_player import create_player
+from players_json import players_data
 
 
 def main() -> None:
     create_race()
     create_skill()
     create_guild()
-    create_player()
+    create_player(players_data)
 
 
 if __name__ == "__main__":
