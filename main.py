469d1eb151f0a4988713eb712daf6f721f7924dc
Maksym Hovorunenko <govorunenko@gmail.com>
2024-02-18 11:38:44 +0200

    Solution

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import init_django_orm  # noqa: F401
 
-from db.models import Race, Skill, Player, Guild
+from db.models import Race, Guild, Skill, Player  # noqa: F401
+
+from create_funcs.create_race import create_race
+from create_funcs.create_skill import create_skill
+from create_funcs.create_guild import create_guild
+from create_funcs.create_player import create_player
 
 
 def main() -> None:
-    pass
+    create_race()
+    create_skill()
+    create_guild()
+    create_player()
 
 
 if __name__ == "__main__":
