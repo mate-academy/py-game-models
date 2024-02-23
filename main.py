f50edd74168b858f1a42ab45a33a9a416e0b3adf
Oleksii Muhyr <1.2.aznch@gmai.com>
2024-02-23 18:39:38 +0200

    implement-py-game-models changed

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ def main() -> None:
         race_name = race_data["name"]
         race_description = race_data["description"]
         if race_name not in races:
-            races[race_name] = Race.objects.get_or_create\
-                (name=race_name,
-                 description=race_description)[0]
+            races[race_name] = \
+                Race.objects.get_or_create(name=race_name,
+                                           description=race_description)[0]
 
         # Guild
         guild_data = player_data.get("guild")
@@ -29,19 +29,19 @@ def main() -> None:
             guild_name = guild_data["name"]
             guild_description = guild_data["description"]
             if guild_name not in guilds:
-                guilds[guild_name] = Guild.objects.get_or_create\
-                    (name=guild_name,
-                     description=guild_description)[0]
+                guilds[guild_name] = \
+                    Guild.objects.get_or_create(name=guild_name,
+                                            description=guild_description)[0]
 
         # Skills
         for skill_data in race_data["skills"]:
             skill_name = skill_data["name"]
             skill_bonus = skill_data["bonus"]
             if skill_name not in skills:
-                skills[skill_name] = Skill.objects.get_or_create\
-                    (name=skill_name,
-                     bonus=skill_bonus,
-                     race=races[race_name])[0]
+                skills[skill_name] = \
+                    Skill.objects.get_or_create(name=skill_name,
+                                                bonus=skill_bonus,
+                                                race=races[race_name])[0]
 
     # Create players
     for player_name, player_data in players_data.items():
