f0fabb8eb156a133ef80a65fbd8094afe5e720cb
Aeros72 <pashamelnyk2004@gmail.com>
2024-02-29 17:11:44 +0200

    Implemented py-game-models

--- db/models.py
+++ db/models.py
@@ -5,16 +5,25 @@ class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
 
+    def __str__(self):
+        return f"Race: {self.name} | Description: {self.description}"
+
 
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"Skill: {self.name} | Bonus: {self.bonus} | Race: {self.race}"
+
 
 class Guild(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(null=True)
+
+    def __str__(self):
+        return f"Guild: {self.name} | Description: {self.description}"
 
 
 class Player(models.Model):
@@ -24,3 +33,6 @@ class Player(models.Model):
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
     guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Nickname: {self.nickname} | Race: {self.race} | Guild: {self.guild}"
