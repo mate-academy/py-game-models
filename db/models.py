e7732b31eb91848bbee9a03f1a5b82bf1a0a2d5b
Aeros72 <pashamelnyk2004@gmail.com>
2024-02-29 17:13:36 +0200

    Implemented py-game-models

--- db/models.py
+++ db/models.py
@@ -5,7 +5,7 @@ class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Race: {self.name} | Description: {self.description}"
 
 
@@ -14,7 +14,7 @@ class Skill(models.Model):
     bonus = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Skill: {self.name} | Bonus: {self.bonus} | Race: {self.race}"
 
 
@@ -22,7 +22,7 @@ class Guild(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(null=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Guild: {self.name} | Description: {self.description}"
 
 
@@ -34,5 +34,6 @@ class Player(models.Model):
     guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"Nickname: {self.nickname} | Race: {self.race} | Guild: {self.guild}"
+    def __str__(self) -> str:
+        return (f"Nickname: {self.nickname} | Race: {self.race} | "
+                f"Guild: {self.guild}")
