6e778d0dee626bf40e16d8e48b725c5aa6c4d63b
TarasFirst <firsttaras@mail.com>
2024-03-11 17:50:15 +0200

    Solution_GTA_2

--- main.py
+++ main.py
@@ -8,29 +8,29 @@ def main() -> None:
     with open("players.json") as file:
         players = json.load(file)
 
-        for player_nick, player_data in players.items():
-            email, bio, race_data, guild_data = player_data.values()
-            race, _ = Race.objects.get_or_create(
-                name=race_data["name"],
-                description=race_data["description"]
+    for player_nick, player_data in players.items():
+        email, bio, race_data, guild_data = player_data.values()
+        race, _ = Race.objects.get_or_create(
+            name=race_data["name"],
+            description=race_data["description"]
+        )
+        for skill_data in race_data["skills"]:
+            skill, _ = Skill.objects.get_or_create(
+                name=skill_data["name"],
+                bonus=skill_data["bonus"],
+                race=race
             )
-            for skill_data in race_data["skills"]:
-                skill, _ = Skill.objects.get_or_create(
-                    name=skill_data["name"],
-                    bonus=skill_data["bonus"],
-                    race=race
-                )
-            guild = None
-            if guild_data:
-                guild, _ = Guild.objects.get_or_create(**guild_data)
+        guild = None
+        if guild_data:
+            guild, _ = Guild.objects.get_or_create(**guild_data)
 
-            player, _ = Player.objects.get_or_create(
-                nickname=player_nick,
-                email=email,
-                bio=bio,
-                race=race,
-                guild=guild
-            )
+        Player.objects.create(
+            nickname=player_nick,
+            email=email,
+            bio=bio,
+            race=race,
+            guild=guild
+        )
 
 
 if __name__ == "__main__":
