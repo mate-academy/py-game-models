38453c0e7eb36b065e4df57be2b07970cfd558a3
Ihor Tutak <igor2tak2212@gmail.com>
2024-03-16 17:26:39 +0200

    added [] instead of .get

--- main.py
+++ main.py
@@ -10,28 +10,28 @@ def main() -> None:
         for player, config in players.items():
 
             race, _ = Race.objects.get_or_create(
-                name=config.get("race").get("name"),
-                description=config.get("race").get("description")
+                name=config["race"]["name"],
+                description=config["race"]["description"]
             )
 
-            for skill in config.get("race").get("skills"):
+            for skill in config["race"]["skills"]:
                 Skill.objects.get_or_create(
-                    name=skill.get("name"),
-                    bonus=skill.get("bonus"),
+                    name=skill["name"],
+                    bonus=skill["bonus"],
                     race=race
                 )
 
             guild = None
             if config.get("guild"):
                 guild, _ = Guild.objects.get_or_create(
-                    name=config.get("guild").get("name"),
-                    description=config.get("guild").get("description")
+                    name=config["guild"]["name"],
+                    description=config["guild"]["description"]
                 )
 
             Player.objects.get_or_create(
                 nickname=player,
-                email=config.get("email"),
-                bio=config.get("bio"),
+                email=config["email"],
+                bio=config["bio"],
                 race=race,
                 guild=guild
             )
