5b59e899c00cd07013b1442d3537e93f56cd5283
Vasyl Kalish <vasyl.documents@yahoo.it>
2024-03-19 22:55:02 +0100

    Solution

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
 import init_django_orm  # noqa: F401
-
 from db.models import Race, Skill, Player, Guild
+import json
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r") as f:
+        data_file = json.load(f)
+    for user, user_info in data_file.items():
+        race_obj = Race.objects.get_or_create(
+            name=user_info["race"]["name"],
+            description=user_info["race"]["description"]
+        )   # Add new Race object in DB.
+        for skill in user_info["race"]["skills"]:
+            Skill.objects.get_or_create(
+                name=skill["name"],
+                bonus=skill["bonus"],
+                race=race_obj[0]
+            )  # Add new Skills object in DB.
+        if user_info["guild"] is not None:
+            guild_obj = Guild.objects.get_or_create(
+                name=user_info["guild"]["name"],
+                description=user_info["guild"]["description"]
+            )    # Add new Guild object in DB.
+            Player.objects.get_or_create(
+                nickname=user,
+                email=user_info["email"],
+                bio=user_info["bio"],
+                race=race_obj[0],
+                guild=guild_obj[0],
+            )   # Add new Players object in DB.
+        else:
+            Player.objects.get_or_create(
+                nickname=user,
+                email=user_info["email"],
+                bio=user_info["bio"],
+                race=race_obj[0],
+            )   # Add new Players object in DB.
 
 
 if __name__ == "__main__":
