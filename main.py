a67c06f16295a7869a04d3571331519482c75e96
Olena Kalitsinska <olena.kalitsinka@gmail.com>
2024-03-19 15:26:02 +0200

    Solution

--- main.py
+++ main.py
@@ -3,36 +3,38 @@ import json
 from db.models import Race, Skill, Player, Guild
 
 
-def main():
-    with open('players.json', 'r') as file:
+def main() -> None:
+    with open("players.json", "r") as file:
         players_data = json.load(file)
 
     for player_name, player_data in players_data.items():
         race, _ = Race.objects.get_or_create(
-            name=player_data['race']['name'],
-            defaults={'description': player_data['race'].get('description', '')}  # Allow empty description
+            name=player_data["race"]["name"],
+            defaults={
+                "description": player_data["race"].get("description", "")
+            }
         )
 
-        guild_data = player_data['guild']
+        guild_data = player_data["guild"]
         if guild_data:
             guild, _ = Guild.objects.get_or_create(
-                name=guild_data['name'],
-                defaults={'description': guild_data.get('description', '')}  # Allow empty description
+                name=guild_data["name"],
+                defaults={"description": guild_data.get("description", "")}
             )
         else:
             guild = None
 
-        for skill_data in player_data['race']['skills']:
+        for skill_data in player_data["race"]["skills"]:
             skill, _ = Skill.objects.get_or_create(
-                name=skill_data['name'],
-                bonus=skill_data['bonus'],
+                name=skill_data["name"],
+                bonus=skill_data["bonus"],
                 race=race
             )
 
         Player.objects.create(
             nickname=player_name,
-            email=player_data['email'],
-            bio=player_data['bio'],
+            email=player_data["email"],
+            bio=player_data["bio"],
             race=race,
             guild=guild
         )
