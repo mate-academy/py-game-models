ca0b0b30ab1fbabe1fff39e4ec9dd1b5f6f50015
Artom <artem.umantsev@gmail.com>
2024-03-24 17:53:17 +0200

    'Solution'

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    with open('players.json', 'r') as f:
+    with open("players.json", "r") as f:
         players_data = json.load(f)
 
     for player_name, player_data in players_data.items():
         race, _ = Race.objects.get_or_create(
             name=player_data["race"]["name"],
             description=player_data["race"]["description"]
-            )
+        )
         if player_data["guild"]:
             guild, _ = Guild.objects.get_or_create(
                 name=player_data["guild"]["name"],
