c57bf316b4b5aa9fbcfd0f9b41a4ceddcb0c2de8
Dede <specialized8393@gmail.com>
2024-04-04 11:08:55 +0000

    Solution

--- main.py
+++ main.py
@@ -1,10 +1,70 @@
 import init_django_orm  # noqa: F401
+import json
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json") as f:
+        data = json.load(f)
+
+        guild_archers = Guild.objects.create(
+            name=data["john"]["guild"]["name"],
+            description=data["john"]["guild"]["description"],
+        )
+        guild_mags = Guild.objects.create(
+            name=data["max"]["guild"]["name"],
+            description=data["max"]["guild"]["description"],
+        )
+        race_elf = Race.objects.create(
+            name=data["john"]["race"]["name"],
+            description=data["john"]["race"]["description"],
+        )
+        race_human = Race.objects.create(
+            name=data["nick"]["race"]["name"],
+            description=data["nick"]["race"]["description"],
+        )
+        skills = [Skill.objects.create(
+            name=skill["name"],
+            bonus=skill["bonus"],
+            race=race_elf
+        ) for skill in data["john"]["race"]["skills"]]
+        player_john = Player.objects.create(
+            nickname="john",
+            email=data["john"]["email"],
+            bio=data["john"]["bio"],
+            race=race_elf,
+            guild=guild_archers
+        )
+        player_max = Player.objects.create(
+            nickname="max",
+            email=data["max"]["email"],
+            bio=data["max"]["bio"],
+            race=race_elf,
+            guild=guild_mags
+        )
+        player_arthur = Player.objects.create(
+            nickname="arthur",
+            email=data["arthur"]["email"],
+            bio=data["arthur"]["bio"],
+            race=race_elf,
+            guild=guild_mags
+        )
+        player_andrew = Player.objects.create(
+            nickname="andrew",
+            email=data["andrew"]["email"],
+            bio=data["andrew"]["bio"],
+            race=race_human,
+            guild=Guild.objects.create(
+                name=data["andrew"]["guild"]["name"],
+                description=data["andrew"]["guild"]["description"])
+        )
+        player_nick = Player.objects.create(
+            nickname="nick",
+            email=data["nick"]["email"],
+            bio=data["nick"]["bio"],
+            race=race_human
+        )
 
 
 if __name__ == "__main__":
