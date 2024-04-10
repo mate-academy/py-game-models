104a083184940bb8ce1f59d7626259a52c0dd287
Illia <ilaruslanovich7@gmail.com>
2024-04-10 20:52:07 +0200

    Solution

--- main.py
+++ main.py
@@ -6,27 +6,28 @@ from db.models import Race, Skill, Player, Guild
 def main() -> None:
     with (open("players.json", "r") as file):
         players = json.load(file)
-        for player in players:
-            race, created = Race.objects.get_or_create(
-                name=players[player]["race"]["name"],
-                description=players[player]["race"]["description"])
-            for skill_ in players[player]["race"]["skills"]:
-                skill, created = Skill.objects.get_or_create(
-                    name=skill_["name"],
-                    bonus=skill_["bonus"],
-                    race=race)
-            try:
-                guild, created = Guild.objects.get_or_create(
-                    name=players[player]["guild"]["name"],
-                    description=players[player]["guild"]["description"])
-            except TypeError:
-                guild = None
-            player_, created = Player.objects.get_or_create(
-                nickname=player,
-                email=players[player]["email"],
-                bio=players[player]["bio"],
-                race=race,
-                guild=guild)
+    for player, player_info in players.items():
+        race, created = Race.objects.get_or_create(
+            name=player_info["race"]["name"],
+            description=player_info["race"]["description"])
+        for skill_ in player_info["race"]["skills"]:
+            skill, created = Skill.objects.get_or_create(
+                name=skill_["name"],
+                bonus=skill_["bonus"],
+                race=race)
+
+        guild = None
+        if player_info.get("guild"):
+            guild, created = Guild.objects.get_or_create(
+                name=player_info["guild"]["name"],
+                description=player_info["guild"]["description"])
+
+        player_, created = Player.objects.get_or_create(
+            nickname=player,
+            email=player_info["email"],
+            bio=player_info["bio"],
+            race=race,
+            guild=guild)
 
 
 if __name__ == "__main__":
