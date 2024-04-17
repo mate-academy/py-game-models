f36eed1df5c541d0dd2519469d1cfa40ff836006
Yaryna Hryniv <ygrinsv@gmail.com>
2024-04-17 12:17:36 +0300

    Solution

--- main.py
+++ main.py
@@ -1,38 +1,37 @@
-import init_django_orm  # noqa: F401
-
 import json
+
+import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    with open("players.json", "r") as players:
-        players = json.load(players)
+    with open("players.json", "r") as f:
+        players_data = json.load(f)
 
-    for player_name in players:
-        player = players[player_name]
+    for player_name, player_data in players_data.items():
         race, _ = Race.objects.get_or_create(
-            name=player["race"]["name"],
-            description=player["race"]["description"]
+            name=player_data["race"]["name"],
+            description=player_data["race"]["description"]
         )
-        for skill_data in player["race"]["skills"]:
+        for skill_data in player_data["race"]["skills"]:
             skill, _ = Skill.objects.get_or_create(
                 name=skill_data["name"],
                 bonus=skill_data["bonus"],
                 race=race
             )
-        if player["guild"] is not None:
+        if player_data["guild"] is not None:
             guild, _ = Guild.objects.get_or_create(
-                name=player["guild"]["name"],
-                description=player["guild"]["description"]
+                name=player_data["guild"]["name"],
+                description=player_data["guild"]["description"]
             )
         else:
             guild = None
 
         Player.objects.get_or_create(
             nickname=player_name,
-            email=player["email"],
-            bio=player["bio"],
+            email=player_data["email"],
+            bio=player_data["bio"],
             race=race,
             guild=guild
         )
