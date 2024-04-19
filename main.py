daf44afd673d13235b90f2818f4681395285ce59
kekW <panicua12@gmail.com>
2024-04-19 21:44:05 +0300

    game db solution

--- main.py
+++ main.py
@@ -1,11 +1,64 @@
-import init_django_orm  # noqa: F401
+import json
 
+import init_django_orm  # noqa: F401
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json") as file:
+        data = json.load(file)
+
+    for player_name in data:
+        player_data = data[player_name]
+        player_race = player_data["race"]
+        player_guild = player_data["guild"]
+
+        get_player_race = create_race(player_race=player_race)
+        create_skills(player_race=player_race, race_instance=get_player_race)
+        guild = create_guild(player_guild=player_guild)
+
+        Player.objects.get_or_create(
+            nickname=player_name,
+            email=player_data["email"],
+            bio=player_data["bio"],
+            race=get_player_race,
+            guild=guild
+        )
 
 
-if __name__ == "__main__":
-    main()
+def create_race(player_race: dict[str]) -> Race:
+    """
+    Creating unique race name, description of race and writes race to DB
+    :return: Race instance
+    """
+    get_player_race, _ = Race.objects.get_or_create(name=player_race["name"])
+    get_player_race.description = player_race["description"]
+    get_player_race.save()
+    return get_player_race
+
+
+def create_skills(player_race: dict[str], race_instance: Race) -> None:
+    """
+    Creating unique skills that are attached to the Race instance
+    """
+    for skill in player_race["skills"]:
+        skill, _ = Skill.objects.get_or_create(
+            name=skill["name"],
+            bonus=skill["bonus"],
+            race=race_instance
+        )
+
+
+def create_guild(player_guild: dict[str]) -> Guild:
+    """
+    Creating unique guild name if player has a guild,
+    description(not required) and writes guild to DB
+    :return: Guild instance
+    """
+    guild = None
+    if player_guild is not None:
+        guild, _ = Guild.objects.get_or_create(
+            name=player_guild["name"],
+            description=player_guild["description"]
+        )
+    return guild
