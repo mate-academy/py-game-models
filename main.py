4ae13db3482a7ed8975606e12c29d7e9cda960e3
Viesich <v.s.vesich@gmail.com>
2024-04-20 15:22:25 +0300

    fixed context manager and removed the unnecessary condition

--- main.py
+++ main.py
@@ -8,35 +8,35 @@ from db.models import Race, Skill, Player, Guild
 def main() -> None:
     with open("players.json", "r") as players_file:
         players = json.load(players_file)
-        for user_name, user_info in players.items():
+    for user_name, user_info in players.items():
 
-            race, _ = Race.objects.get_or_create(
-                name=user_info["race"]["name"],
-                description=user_info["race"]["description"],
-            )
-            for skill in user_info["race"]["skills"]:
-                if skill:
-                    skill, _ = Skill.objects.get_or_create(
-                        name=skill["name"],
-                        bonus=skill["bonus"],
-                        race=race,
-                    )
-
-            if user_info["guild"]:
-                guild, _ = Guild.objects.get_or_create(
-                    name=user_info["guild"]["name"],
-                    description=user_info["guild"]["description"],
+        race, _ = Race.objects.get_or_create(
+            name=user_info["race"]["name"],
+            description=user_info["race"]["description"],
+        )
+        for skill in user_info["race"]["skills"]:
+            if skill:
+                skill, _ = Skill.objects.get_or_create(
+                    name=skill["name"],
+                    bonus=skill["bonus"],
+                    race=race,
                 )
-            else:
-                guild = None
 
-            Player.objects.get_or_create(
-                nickname=user_name,
-                email=user_info["email"],
-                bio=user_info["bio"],
-                race=race,
-                guild=guild,
+        guild_info = user_info["guild"]
+        guild = None
+        if guild_info:
+            guild, _ = Guild.objects.get_or_create(
+                name=guild_info["name"],
+                description=guild_info["description"],
             )
+
+        Player.objects.get_or_create(
+            nickname=user_name,
+            email=user_info["email"],
+            bio=user_info["bio"],
+            race=race,
+            guild=guild,
+        )
 
 
 if __name__ == "__main__":
