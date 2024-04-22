cab452b46e79865284584dd8255ad5067464fff0
semermaks <semermaks@mail.com>
2024-04-22 16:00:23 +0300

    Solution 1

--- main.py
+++ main.py
@@ -4,7 +4,36 @@ from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r") as file:
+        data = json.load(file)
+
+        for nickname, info in data.items():
+            race_obj, _ = Race.objects.get_or_create(
+                name=info["race"]["name"],
+                description=info["race"]["description"]
+            )
+
+            for skills_info in info["race"]["skills"]:
+                skill, _ = Skill.objects.get_or_create(
+                    name=skills_info["name"],
+                    bonus=skills_info["bonus"],
+                    race=race_obj
+                )
+            if info["guild"] is not None:
+                guild_obj, _ = Guild.objects.get_or_create(
+                    name=info["guild"]["name"],
+                    description=info["guild"]["description"]
+                )
+            else:
+                guild_obj = None
+
+            Player.objects.get_or_create(
+                nickname=nickname,
+                email=info["email"],
+                bio=info["bio"],
+                race=race_obj,
+                guild=guild_obj
+            )
 
 
 if __name__ == "__main__":
