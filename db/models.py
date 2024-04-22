860971cfae54dd1eb3105de8733d4e9d8d55e63c
semermaks <semermaks@mail.com>
2024-04-22 16:13:25 +0300

    Solution 2 add migrations, fix bd

--- db/models.py
+++ db/models.py
@@ -2,15 +2,7 @@ from django.db import models
 
 
 class Race(models.Model):
-
-    RACE_NAME = (
-        ("Elf", "The magic race"),
-        ("Dwarf", "DWARF"),
-        ("Human", "Human race"),
-        ("Ork", "ORK")
-    )
-
-    name = models.CharField(max_length=255, choices=RACE_NAME)
+    name = models.CharField(unique=True, max_length=255)
     description = models.TextField(blank=True)
 
 
@@ -30,5 +22,5 @@ class Player(models.Model):
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
-    guild = models.ForeignKey(Guild, null=True, on_delete=models.SET_NULL)
-    created_at = models.DateTimeField(auto_now_add=True)
+    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
+    create_at = models.DateTimeField(auto_now=True)
