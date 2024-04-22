a172da7d91ee73c840003c07e5fcd208a16648b6
Valentin Pozniak <waliker448@gmail.com>
2024-04-22 14:50:21 +0300

    'Solution2'

--- main.py
+++ main.py
@@ -9,37 +9,30 @@ def main() -> None:
     with open("players.json") as players_file:
         players = json.load(players_file)
 
-    for player in players:
-        nickname = player
-        email = players[nickname]["email"]
-        bio = players[nickname]["bio"]
+    for nickname, player_data in players.items():
+        email = player_data["email"]
+        bio = player_data["bio"]
 
-        # CREATE RACE
-        race_info = players[nickname]["race"]
         race, _ = Race.objects.get_or_create(
-            name=race_info["name"],
-            description=race_info["description"]
+            name=player_data["race"]["name"],
+            description=player_data["race"]["description"]
         )
 
-        # CREATE SKILLS
-        for skill in race_info["skills"]:
+        for skill in player_data["race"]["skills"]:
             Skill.objects.get_or_create(
                 name=skill["name"],
                 bonus=skill["bonus"],
                 race=race
             )
 
-        # CREATE GUILD
-        if players[nickname]["guild"]:
-            guild_info = players[nickname]["guild"]
+        if player_data["guild"]:
             guild, _ = Guild.objects.get_or_create(
-                name=guild_info["name"],
-                description=guild_info["description"]
+                name=player_data["guild"]["name"],
+                description=player_data["guild"]["description"]
             )
         else:
             guild = None
 
-        # CREATE PLAYERS
         Player.objects.create(
             nickname=nickname,
             email=email,
