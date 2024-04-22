6663b7c81058d2709a28212a97fd4df5d0fd4a2d
Valentin Pozniak <waliker448@gmail.com>
2024-04-22 20:03:18 +0300

    'Solution4'

--- db/models.py
+++ db/models.py
@@ -5,16 +5,25 @@ class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
 
+    def __str__(self) -> str:
+        return f"name = {self.name}, description = {self.description}"
+
 
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
 
+    def __str__(self) -> str:
+        return f"Skill = {self.name}, bonus = {self.bonus}, race = {self.race}"
+
 
 class Guild(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True)
+
+    def __str__(self) -> str:
+        return f"name = {self.name}, description = {self.description}"
 
 
 class Player(models.Model):
@@ -24,3 +33,10 @@ class Player(models.Model):
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
     guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return (
+            f"nickname: {self.nickname}, email: {self.email}, "
+            f"bio: {self.bio}, race: {self.race}, guild: {self.guild}, "
+            f"created: {self.created_at}"
+        )
