7d7a0e5a84ae94ac9fb504c25f6370556a993192
dvkim28 <d.villarionovich@gmail.com>
2024-04-23 17:51:44 +0200

    solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,25 @@
 from django.db import models
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(max_length=255)
+    race = models.ForeignKey(to=Race, on_delete=models.CASCADE)
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+
+class Players(models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.CharField(max_length=255)
+    bio = models.CharField(max_length=255)
+    race = models.ForeignKey(to=Race, on_delete=models.CASCADE)
+    guild = models.ForeignKey(to=Guild, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
--- main.py
+++ main.py
@@ -1,10 +1,38 @@
-import init_django_orm  # noqa: F401
+import json
 
-from db.models import Race, Skill, Player, Guild
+import init_django_orm  # noqa: F401
+from db.models import Race, Guild, Players, Skill
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r") as file:
+        players_data = json.load(file)
+    for player_name, player_data in players_data.items():
+        race_data = player_data["race"]
+        skill_data = player_data["race"]["skills"]
+        race, created = Race.objects.get_or_create(
+            name=race_data["name"],
+            description=race_data["description"])
+        for skill in skill_data:
+            skill, created = Skill.objects.get_or_create(
+                name=skill["name"],
+                bonus=skill["bonus"],
+                race=race,
+            )
+
+        print(skill_data)
+        race.save()
+
+    #     skill_data = player_data["skills"]
+    # race, created = Race.objects.get_or_create(
+    #     name=player_data["name"],
+    #     description=player_data["description"]
+    #
+    # skill, created = Skill.objects.get_or_create(
+    #     name=skill_data["name"],
+    #     bonus=skill_data["bonus"],
+    #     race=race
+    # )
 
 
 if __name__ == "__main__":
