5dd91a4160b16b4469429e4bfa286be57b366611
Ruxxell <furman06228@gmail.com>
2024-04-24 14:27:08 +0300

    Solution

--- main.py
+++ main.py
@@ -1,11 +1,65 @@
-import init_django_orm  # noqa: F401
+import json
+from typing import Dict
 
+import init_django_orm  # noqa: F401
 from db.models import Race, Skill, Player, Guild
 
 
+def reader(filename: str) -> Dict:
+    with open(filename) as file:
+        return json.load(file)
+
+
+def create_or_update_race(players_race: dict) -> Race:
+    race, _ = Race.objects.get_or_create(
+        name=players_race["name"],
+        defaults={"description": players_race["description"]}
+    )
+    return race
+
+
+def create_or_update_skills(players_race: dict, race_instance: Race) -> None:
+    for skill in players_race:
+        skill_instance, _ = Skill.objects.get_or_create(
+            name=skill["name"],
+            bonus=skill["bonus"],
+            race=race_instance
+        )
+
+
+def create_or_update_guild(player_guild: dict) -> Guild | None:
+    if player_guild:
+        guild, _ = Guild.objects.get_or_create(
+            name=player_guild["name"],
+            defaults={"description": player_guild["description"]}
+        )
+        return guild
+
+
+def create_player(
+        player_name: str,
+        player_data: dict,
+        race: Race,
+        guild: Guild
+) -> None:
+    Player.objects.create(
+        nickname=player_name,
+        email=player_data["email"],
+        bio=player_data["bio"],
+        race=race,
+        guild=guild,
+    )
+
+
 def main() -> None:
-    pass
+    data = reader("players.json")
 
+    for player_name, player_data in data.items():
+        race_data = player_data["race"]
+        guild_data = player_data["guild"]
 
-if __name__ == "__main__":
-    main()
+        race = create_or_update_race(race_data)
+        create_or_update_skills(race_data["skills"], race)
+        guild = create_or_update_guild(guild_data)
+
+        create_player(player_name, player_data, race, guild)
