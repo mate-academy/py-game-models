91baada415afaa9068c93abf079c7ba0dbfac4ef
Vitalii <vitalikbashkiser@gmail.com>
2024-04-24 22:20:30 +0300

    olution#4(add related_name in models.py; remove extra strings, add walrus operator :=, e.t.c)

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import json
 
 import init_django_orm  # noqa: F401
 
-from django.utils.timezone import now
-
 from db.models import Race, Skill, Player, Guild
 
 
@@ -13,31 +11,23 @@ def main() -> None:
 
     for player_name, player_data in players_data.items():
         race_data = player_data["race"]
-        race_name = race_data["name"]
-        race_description = race_data.get("description", "")
         race, _ = Race.objects.get_or_create(
-            name=race_name,
-            defaults={"description": race_description}
+            name=race_data["name"],
+            defaults={"description": race_data.get("description", "")}
         )
 
-        guild_data = player_data.get("guild", None)
-        if guild_data:
-            guild_name = guild_data["name"]
-            guild_description = guild_data.get("description", None)
-            guild, _ = Guild.objects.get_or_create(
-                name=guild_name,
-                defaults={"description": guild_description}
-            )
-        else:
-            guild = None
+        guild = None
+        if (guild_data := player_data.get("guild", None)):
+            guild = Guild.objects.get_or_create(
+                name=guild_data["name"],
+                defaults={"description": guild_data.get("description", None)}
+            )[0]
 
         skills_data = race_data.get("skills", [])
         for skill_data in skills_data:
-            skill_name = skill_data["name"]
-            skill_bonus = skill_data["bonus"]
-            skill, _ = Skill.objects.get_or_create(
-                name=skill_name,
-                bonus=skill_bonus,
+            Skill.objects.get_or_create(
+                name=skill_data["name"],
+                bonus=skill_data["bonus"],
                 race=race
             )
 
@@ -46,8 +36,7 @@ def main() -> None:
             email=player_data["email"],
             bio=player_data["bio"],
             race=race,
-            guild=guild,
-            created_at=now()
+            guild=guild
         )
 
 
