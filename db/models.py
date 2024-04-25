2440ad9c5aac6c3e0282c02a30378096dedd67ce
Yuliia Dudych <juliadudych@gmail.com>
2024-04-25 15:21:04 +0300

    Create Race model

--- db/models.py
+++ db/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+
+
+class Race(models.Model):
+    RACES = (
+        ("elf", "Elf"),
+        ("dwarf", "Dwarf"),
+        ("human", "Human"),
+        ("ork", "Ork")
+    )
+    name = models.CharField(max_length=255, choices=RACES, unique=True)
+    description = models.TextField(blank=True)
