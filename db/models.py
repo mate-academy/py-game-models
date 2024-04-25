9f56c26fc1319478edc717e9fd8fad4b74fc821c
Yuliia Dudych <juliadudych@gmail.com>
2024-04-25 15:21:20 +0300

    Create Skill model

--- db/models.py
+++ db/models.py
@@ -10,3 +10,9 @@ class Race(models.Model):
     )
     name = models.CharField(max_length=255, choices=RACES, unique=True)
     description = models.TextField(blank=True)
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.SET_NULL, null=True)
