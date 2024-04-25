ed87d6a7a14e68d12190bba5788393bebf2fdd6f
Yuliia Dudych <juliadudych@gmail.com>
2024-04-25 15:21:42 +0300

    Create Player model

--- db/models.py
+++ db/models.py
@@ -21,3 +21,12 @@ class Skill(models.Model):
 class Guild(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True)
+
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.SET_NULL, null=True)
+    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
