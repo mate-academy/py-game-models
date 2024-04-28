b2f6a3406796d791d0b5db57b030c05ebac0b452
bwaah1 <chykivan@gmail.com>
2024-04-29 02:28:33 +0300

    Fix flake8 errors

--- main.py
+++ main.py
@@ -13,12 +13,19 @@ def main() -> None:
             if race_data:
                 race_name = race_data["name"]
                 race_description = race_data.get("description", "")
-                race, _ = Race.objects.get_or_create(name=race_name, description=race_description)
+                race, _ = Race.objects.get_or_create(
+                    name=race_name,
+                    description=race_description
+                )
                 skills_data = race_data.get("skills", [])
                 for skill_data in skills_data:
                     skill_name = skill_data["name"]
                     skill_bonus = skill_data["bonus"]
-                    Skill.objects.get_or_create(name=skill_name, bonus=skill_bonus, race=race)
+                    Skill.objects.get_or_create(
+                        name=skill_name,
+                        bonus=skill_bonus,
+                        race=race
+                    )
             else:
                 race = None
 
@@ -26,7 +33,9 @@ def main() -> None:
             if guild_data:
                 guild_name = guild_data["name"]
                 guild_description = guild_data.get("description", "")
-                guild, _ = Guild.objects.get_or_create(name=guild_name, description=guild_description)
+                guild, _ = Guild.objects.get_or_create(
+                    name=guild_name,
+                    description=guild_description)
             else:
                 guild = None
 
