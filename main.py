4e73e8621f1f4960979479a664e49958e72f8237
bwaah1 <chykivan@gmail.com>
2024-04-29 02:50:08 +0300

    Simplify the code

--- main.py
+++ main.py
@@ -5,37 +5,28 @@ from db.models import Race, Skill, Player, Guild
 
 def main() -> None:
     with open("players.json", "r") as f:
-        data = json.load(f)
+        players_data = json.load(f)
 
-        for player_name, player_data in data.items():
-            race_data = player_data.get("race")
-
+        for player_name, player_data in players_data.items():
+            race_data = player_data["race"]
             if race_data:
                 race_name = race_data["name"]
-                race_description = race_data.get("description", "")
-                race, _ = Race.objects.get_or_create(
-                    name=race_name,
-                    description=race_description
-                )
-                skills_data = race_data.get("skills", [])
+                race_description = race_data["description"]
+                race, _ = Race.objects.get_or_create(name=race_name, description=race_description)
+
+                skills_data = race_data["skills"]
                 for skill_data in skills_data:
                     skill_name = skill_data["name"]
                     skill_bonus = skill_data["bonus"]
-                    Skill.objects.get_or_create(
-                        name=skill_name,
-                        bonus=skill_bonus,
-                        race=race
-                    )
+                    Skill.objects.get_or_create(name=skill_name, bonus=skill_bonus, race=race)
             else:
                 race = None
 
-            guild_data = player_data.get("guild")
+            guild_data = player_data["guild"]
             if guild_data:
                 guild_name = guild_data["name"]
-                guild_description = guild_data.get("description", "")
-                guild, _ = Guild.objects.get_or_create(
-                    name=guild_name,
-                    description=guild_description)
+                guild_description = guild_data["description"]
+                guild, _ = Guild.objects.get_or_create(name=guild_name, description=guild_description)
             else:
                 guild = None
 
