376e7d9d73ff47c7b462a5f2d929183d2dc9b560
bwaah1 <chykivan@gmail.com>
2024-04-30 14:01:42 +0300

    Improve code structure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import init_django_orm  # noqa: F401
 import json
+
+import init_django_orm  # noqa: F401
 from db.models import Race, Skill, Player, Guild
 
 
@@ -7,46 +8,45 @@ def main() -> None:
     with open("players.json", "r") as f:
         players_data = json.load(f)
 
-        for player_name, player_data in players_data.items():
-            race_data = player_data["race"]
-            if race_data:
-                race_name = race_data["name"]
-                race_description = race_data["description"]
-                race, _ = Race.objects.get_or_create(
-                    name=race_name,
-                    description=race_description
-                )
+    for player_name, player_data in players_data.items():
+        race = None
+        guild = None
 
-                skills_data = race_data["skills"]
-                for skill_data in skills_data:
-                    skill_name = skill_data["name"]
-                    skill_bonus = skill_data["bonus"]
-                    Skill.objects.get_or_create(
-                        name=skill_name,
-                        bonus=skill_bonus,
-                        race=race
-                    )
-            else:
-                race = None
-
-            guild_data = player_data["guild"]
-            if guild_data:
-                guild_name = guild_data["name"]
-                guild_description = guild_data["description"]
-                guild, _ = Guild.objects.get_or_create(
-                    name=guild_name,
-                    description=guild_description
-                )
-            else:
-                guild = None
-
-            Player.objects.create(
-                nickname=player_name,
-                email=player_data["email"],
-                bio=player_data["bio"],
-                race=race,
-                guild=guild,
+        race_data = player_data["race"]
+        if race_data:
+            race_name = race_data["name"]
+            race_description = race_data["description"]
+            race, _ = Race.objects.get_or_create(
+                name=race_name,
+                description=race_description
             )
+
+            skills_data = race_data["skills"]
+            for skill_data in skills_data:
+                skill_name = skill_data["name"]
+                skill_bonus = skill_data["bonus"]
+                Skill.objects.get_or_create(
+                    name=skill_name,
+                    bonus=skill_bonus,
+                    race=race
+                )
+
+        guild_data = player_data["guild"]
+        if guild_data:
+            guild_name = guild_data["name"]
+            guild_description = guild_data["description"]
+            guild, _ = Guild.objects.get_or_create(
+                name=guild_name,
+                description=guild_description
+            )
+
+        Player.objects.create(
+            nickname=player_name,
+            email=player_data["email"],
+            bio=player_data["bio"],
+            race=race,
+            guild=guild,
+        )
 
 
 if __name__ == "__main__":
