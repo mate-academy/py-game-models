fd09fb0512b30d85ac6255b93fd08a3495f6d9d1
Eugene <eugenekozhenevskiy@gmail.com>
2024-05-01 10:12:14 +0300

    fixed flake8

--- db/models.py
+++ db/models.py
@@ -8,7 +8,7 @@ class Race(models.Model):
 
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    bonus = models.CharField(max_length=255)  # This field describes what kind of bonus players can get from it.
+    bonus = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
 
 
@@ -20,7 +20,7 @@ class Guild(models.Model):
 class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255)
-    bio = models.CharField(max_length=255)  # It stores a short description provided by a user about himself/herself.
+    bio = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
     guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
