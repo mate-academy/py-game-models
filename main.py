ac1803e0e27e29d414511985293f98a1cf82154e
Vasyl Rybalka <basilrybalka@gmail.com>
2024-05-05 10:53:03 +0200

    Solution

--- main.py
+++ main.py
@@ -8,28 +8,29 @@ def main() -> None:
     with open("players.json", "r") as file:
         players_data = json.load(file)
 
+    Guild.objects.all().delete()
+    Race.objects.all().delete()
+    Skill.objects.all().delete()
+    Player.objects.all().delete()
+
     for player_name, player_info in players_data.items():
         race, _ = Race.objects.get_or_create(
             name=player_info["race"]["name"],
-            defaults={
-                "description": player_info["race"].get("description", "")}
+            defaults={"description": player_info["race"].get("description", "")}
         )
 
         for skill in player_info["race"]["skills"]:
             Skill.objects.get_or_create(
                 name=skill["name"],
-                defaults={
-                    "bonus": skill["bonus"],
-                    "race": race
-                }
+                race=race,  # Attach skill directly to race
+                defaults={"bonus": skill["bonus"]}
             )
 
         guild = None
         if player_info.get("guild"):
             guild, _ = Guild.objects.get_or_create(
                 name=player_info["guild"]["name"],
-                defaults={
-                    "description": player_info["guild"].get("description")}
+                defaults={"description": player_info["guild"].get("description")}
             )
 
         Player.objects.get_or_create(
