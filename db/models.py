d88a55187f94a81f0b10062953cf80a5d8960482
pavlopro <pavlop@enapps.co.uk>
2024-06-01 09:48:48 +0300

    correct flake

--- db/models.py
+++ db/models.py
@@ -10,7 +10,10 @@ class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
     race = models.ForeignKey(
-        Race, on_delete=models.CASCADE, related_name="skill_set" # Do not add a related_name attribute here or it must be "skill_set" exactly
+        Race,
+        on_delete=models.CASCADE,
+        related_name="skill_set"  # Do not add a
+        # related_name attribute here or it must # be "skill_set" exactly
     )
 
 
@@ -27,7 +30,10 @@ class Player(models.Model):
         Race, on_delete=models.CASCADE, related_name="players"
     )
     guild = models.ForeignKey(
-        Guild, on_delete=models.SET_NULL, related_name="players", blank=True, null=True
+        Guild,
+        on_delete=models.SET_NULL,
+        related_name="players",
+        blank=True,
+        null=True
     )
     created_at = models.DateTimeField(auto_now_add=True)
-
