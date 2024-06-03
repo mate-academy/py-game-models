75a34d7ba8219477785aa90330284fdad5f2b362
Roman Sokolov <gnonasis@gmail.com>
2024-06-03 10:17:50 +0300

    Correction

--- main.py
+++ main.py
@@ -4,27 +4,26 @@ import json
 from db.models import Race, Skill, Player, Guild
 
 
-def create_or_update_race(race_data: dict) -> Race:
+def create_race(race_data: dict) -> Race:
     race_description = race_data.get("description")
     if race_description is None:
-        return (Race.objects.get_or_create(name=race_data["name"]))[0]
+        return None
     return (Race.objects.get_or_create(
         name=race_data["name"],
         description=race_description)
     )[0]
 
 
-def create_or_update_guild(guild_data: dict) -> Guild | None:
+def create_guild(guild_data: dict) -> Guild | None:
     if not guild_data:
         return None
-    guild_description = guild_data.get("description")
     return Guild.objects.get_or_create(
         name=guild_data["name"],
-        description=guild_description
+        description=guild_data.get("description")
     )[0]
 
 
-def create_or_update_skill(skill_data: dict, race: Race) -> None:
+def create_skill(skill_data: dict, race: Race) -> None:
     Skill.objects.get_or_create(
         name=skill_data["name"],
         bonus=skill_data["bonus"],
@@ -52,13 +51,11 @@ def main() -> None:
         players = json.load(file)
 
     for player_name, player_data in players.items():
-        print(player_data["race"]["skills"])
-
-        race = create_or_update_race(player_data["race"])
-        guild = create_or_update_guild(player_data.get("guild"))
+        race = create_race(player_data["race"])
+        guild = create_guild(player_data.get("guild"))
 
         for skill_data in player_data["race"]["skills"]:
-            create_or_update_skill(skill_data, race)
+            create_skill(skill_data, race)
 
         create_player(
             nickname=player_name,
