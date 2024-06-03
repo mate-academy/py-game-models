f66897addefe6050483a59b527572348046210ff
Olevksy Ivan <kiperdemiwan@gmail.com>
2024-06-03 16:58:06 +0300

    i added 3 methods str

--- db/models.py
+++ db/models.py
@@ -5,17 +5,22 @@ class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return self.name
 
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
 
 class Guild(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True, blank=True)
-
+    def __str__(self):
+        return self.name
 
 class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
@@ -24,3 +29,6 @@ class Player(models.Model):
     race = models.ForeignKey(Race, on_delete=models.SET_NULL, null=True)
     guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nickname
