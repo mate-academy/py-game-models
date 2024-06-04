85ae8a57eeb7504e0d7f9119c4ebb857470a3215
raychw <bladeryz@gmail.com>
2024-06-04 16:09:50 +0300

    Providing the solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,37 @@
 from django.db import models
+
+
+class Race(models.Model):
+    RACES = [
+        ("elf", "Elves - beautiful guardians of the woods"),
+        ("dwarf", "Dwarves - don't let their short height fool you!"),
+        ("human", "Humans - just like in real life"),
+        ("Ork", "Orks - a miserable race of green-skinned creatures")
+    ]
+
+    name = models.CharField(max_length=255, unique=True, choices=RACES)
+    description = models.TextField(blank=True)
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.SET_NULL, null=True)
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True)
+
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.SET_NULL, null=True)
+    guild = models.ForeignKey(
+        Guild,
+        on_delete=models.SET_NULL,
+        null=True,
+        default="none")
+    created_at = models.DateTimeField(auto_now_add=True)
