6fc8aef50ba713b6048b86a976f9f9a9b8ad87f0
Max <bidun.max@gmail.com>
2024-06-05 19:21:22 +0300

    Solution

--- main.py
+++ main.py
@@ -8,26 +8,26 @@ from db.models import Race, Skill, Player, Guild
 def main() -> None:
     with open("players.json", "r") as file:
         players = json.load(file)
-        for player, player_inf in players.items():
-            race, _ = Race.objects.get_or_create(
-                name=player_inf["race"]["name"],
-                description=player_inf["race"]["description"])
+    for player, player_inf in players.items():
+        race, _ = Race.objects.get_or_create(
+            name=player_inf["race"]["name"],
+            description=player_inf["race"]["description"])
 
-            guild = None
-            if player_inf["guild"]:
-                guild, _ = Guild.objects.get_or_create(
-                    name=player_inf["guild"]["name"],
-                    description=player_inf["guild"]["description"])
+        guild = None
+        if player_inf["guild"]:
+            guild, _ = Guild.objects.get_or_create(
+                name=player_inf["guild"]["name"],
+                description=player_inf["guild"]["description"])
 
-            Player.objects.create(nickname=player,
-                                  email=player_inf["email"],
-                                  bio=player_inf["bio"],
-                                  race=race,
-                                  guild=guild)
-            for skill in player_inf["race"]["skills"]:
-                Skill.objects.get_or_create(name=skill["name"],
-                                            bonus=skill["bonus"],
-                                            race=race)
+        Player.objects.create(nickname=player,
+                              email=player_inf["email"],
+                              bio=player_inf["bio"],
+                              race=race,
+                              guild=guild)
+        for skill in player_inf["race"]["skills"]:
+            Skill.objects.get_or_create(name=skill["name"],
+                                        bonus=skill["bonus"],
+                                        race=race)
 
 
 if __name__ == "__main__":
