b9abb76ac885e400f017c0ccf71e2af964779ac5
Anton Bilous <anton.bilosus@gmail.com>
2024-06-15 21:31:13 +0300

    Add code to main.py

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
 import init_django_orm  # noqa: F401
 
+import json
+
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json") as f:
+        data = json.load(f)
+
+    for nickname, properties in data.items():
+        race_data = properties["race"]
+        skills = race_data.pop("skills")
+
+        race = Race.objects.get_or_create(**race_data)[0]
+
+        for skill_data in skills:
+            Skill.objects.get_or_create(
+                **skill_data,
+                race=race,
+            )
+
+        guild_data = properties["guild"]
+        guild = (
+            Guild.objects.get_or_create(**guild_data)[0]
+            if guild_data
+            else None
+        )
+
+        Player.objects.create(
+            nickname=nickname,
+            bio=properties["bio"],
+            race=race,
+            guild=guild,
+        )
 
 
 if __name__ == "__main__":
