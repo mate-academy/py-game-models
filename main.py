4e52da085182e5f37f754f925ae40dd139651a3f
Dima Drozdenko <anonimchik920@gmail.com>
2024-06-18 13:41:28 +0300

    Solution

--- main.py
+++ main.py
@@ -6,36 +6,37 @@ from db.models import Race, Skill, Player, Guild
 def main() -> None:
     with open("players.json") as file:
         data = json.load(file)
-        for name, user_data in data.items():
-            player = Player(
-                nickname=name,
-                email=user_data["email"],
-                bio=user_data["bio"],
+
+    for name, user_data in data.items():
+        player = Player(
+            nickname=name,
+            email=user_data["email"],
+            bio=user_data["bio"],
+        )
+
+        race = Race.objects.get_or_create(
+            name=user_data["race"]["name"],
+            description=user_data["race"]["description"],
+        )
+
+        player.race = race[0]
+
+        for skill in user_data["race"]["skills"]:
+            Skill.objects.get_or_create(
+                name=skill["name"],
+                bonus=skill["bonus"],
+                race=race[0]
             )
 
-            race = Race.objects.get_or_create(
-                name=user_data["race"]["name"],
-                description=user_data["race"]["description"],
+        if user_data["guild"] is not None:
+            guild = Guild.objects.get_or_create(
+                name=user_data["guild"]["name"],
+                description=user_data["guild"]["description"],
             )
 
-            player.race = race[0]
+            player.guild = guild[0]
 
-            for skill in user_data["race"]["skills"]:
-                Skill.objects.get_or_create(
-                    name=skill["name"],
-                    bonus=skill["bonus"],
-                    race=race[0]
-                )
-
-            if user_data["guild"] is not None:
-                guild = Guild.objects.get_or_create(
-                    name=user_data["guild"]["name"],
-                    description=user_data["guild"]["description"],
-                )
-
-                player.guild = guild[0]
-
-            player.save()
+        player.save()
 
 
 if __name__ == "__main__":
