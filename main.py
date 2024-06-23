ffa7879c3022133f237bbf2a45b6d811309275c9
Bohdan Zinchenko <bogdan.zinchenko.2019@gmail.com>
2024-06-23 22:54:25 +0300

    py-game-models v3

--- main.py
+++ main.py
@@ -8,28 +8,24 @@ def main() -> None:
         players = json.load(file)
 
     for nickname, player_data in players.items():
-        # Get or create the race
-        race, created = Race.objects.get_or_create(
+        race, _ = Race.objects.get_or_create(
             name=player_data["race"]["name"],
             defaults={"description": player_data["race"]["description"]}
         )
 
-        # Create or get a skills
         for skill_data in player_data["race"]["skills"]:
             Skill.objects.get_or_create(
                 name=skill_data["name"],
                 defaults={"bonus": skill_data["bonus"], "race": race}
             )
 
-        # Get or create the guild if it exists
         guild = None
         if player_data["guild"]:
-            guild, created = Guild.objects.get_or_create(
+            guild, _ = Guild.objects.get_or_create(
                 name=player_data["guild"]["name"],
                 defaults={"description": player_data["guild"]["description"]}
             )
 
-        # Create the player
         Player.objects.create(
             nickname=nickname,
             email=player_data["email"],
