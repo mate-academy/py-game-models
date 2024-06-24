0857e3902734d672462367dead8f3114e1c4ada3
Andrii1000 <127493083+Andrii1000@users.noreply.github.com>
2024-06-24 15:13:59 +0300

    Solution

--- main.py
+++ main.py
@@ -9,36 +9,36 @@ def main() -> None:
     with open("players.json") as f:
         data = json.load(f)
 
-    for nickname, data in data.items():
-        email, bio, race_data, guild_data = data.values()
+    for nickname, info in data.items():
+        email, bio, race_data, guild_data = info.values()
 
-    race_instance, _ = Race.objects.get_or_create(
-        name=race_data["name"],
-        description=race_data["description"],
-    )
-
-    for skill in race_data["skills"]:
-        Skill.objects.get_or_create(
-            name=skill["name"],
-            bonus=skill["bonus"],
-            race=race_instance
+        race_instance, _ = Race.objects.get_or_create(
+            name=race_data["name"],
+            description=race_data["description"]
         )
 
-    if guild_data:
-        guild_instance, _ = Guild.objects.get_or_create(
-            name=guild_data["name"],
-            description=guild_data.get("description", "")
-        )
-    else:
-        guild_instance = None
+        for skill in race_data["skills"]:
+            Skill.objects.get_or_create(
+                name=skill["name"],
+                bonus=skill["bonus"],
+                race=race_instance
+            )
 
-    Player.objects.get_or_create(
-        nickname=nickname,
-        email=email,
-        bio=bio,
-        race=race_instance,
-        guild=guild_instance
-    )
+        if guild_data:
+            guild_instance, _ = Guild.objects.get_or_create(
+                name=guild_data["name"],
+                description=guild_data.get("description", "")
+            )
+        else:
+            guild_instance = None
+
+        Player.objects.get_or_create(
+            nickname=nickname,
+            email=email,
+            bio=bio,
+            race=race_instance,
+            guild=guild_instance
+        )
 
 
 if __name__ == "__main__":
