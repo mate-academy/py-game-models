2129a4b63f64cee73ccb18772761212ace5efeed
Ivan <ivan.seeker6@gmail.com>
2024-06-24 17:49:25 +0300

    sol2

--- db/models.py
+++ db/models.py
@@ -31,7 +31,11 @@ class Player(models.Model):
     email = models.EmailField(max_length=255, unique=True)
     bio = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
-    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True, blank=True)
+    guild = models.ForeignKey(
+        Guild,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
