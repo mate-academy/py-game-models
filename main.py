1ec93c993fddae165461ec0f5188760b41eb9e6d
TkachukTara <zaz42988@gmail.com>
2024-06-25 12:30:08 +0300

    Solved Task!

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    with (open('players.json', "r")) as players_file:
+    with (open("players.json", "r")) as players_file:
         players = json.load(players_file)
 
     for nickname, player in players.items():
@@ -17,27 +17,30 @@ def main() -> None:
 
         if guild:
             guild_db = Guild.objects.get_or_create(
-                name = guild["name"], description=guild["description"]
+                name = guild["name"],
+                description=guild["description"]
             )
             guild_id = guild_db[0].id
 
         if race:
             race_db = Race.objects.get_or_create(
-                name = race["name"], description=race["description"]
+                name = race["name"],
+                description=race["description"]
             )
             race_id = race_db[0].id
 
             if len(race["skills"]) > 0:
                 for skill in race["skills"]:
                     Skill.objects.get_or_create(
-                        name=skill["name"], bonus=skill["bonus"],
+                        name=skill["name"],
+                        bonus=skill["bonus"],
                         race_id=race_id)
 
         Player.objects.get_or_create(nickname=nickname,
-                                     email=player["email"], bio=player["bio"],
-                                     race_id=race_id, guild_id=guild_id)
-
-
+                                     email=player["email"],
+                                     bio=player["bio"],
+                                     race_id=race_id,
+                                     guild_id=guild_id)
 
 
 if __name__ == "__main__":
