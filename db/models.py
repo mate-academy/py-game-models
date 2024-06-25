862cf3a17ef487b3d3c5f90e73c7e1106be4f7f9
Yeva Yepifanova <yevayepifanova@gmail.com>
2024-06-26 02:34:08 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -24,23 +24,3 @@ class Player(models.Model):
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
     guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
-
-
-
-# class Car(models.Model):
-#     ENGINE_TYPE = (
-#         ("ICE", "internal combustion engine"),
-#         ("HYBRID", "hybrid engine"),
-#         ("ELECTRICAL", "electrical engine"),
-#     )
-#
-#     brand = models.CharField(max_length=255)
-#     horse_power = models.IntegerField()
-#     creation_date = models.DateField(null=True, blank=True)
-#     description = models.TextField(default="It is very good car")
-#     engine_type = models.CharField(max_length=20, choices=ENGINE_TYPE, null=True)
-#     record_created = models.DateTimeField(null=True, auto_now=True)
-#
-#     def __str__(self):
-#         return f"{self.id} {self.brand} (power: {self.horse_power})"
