ad1ef26f9d403a0d1f6164bf140a9bc12d33e6b9
Halyna Rohatska <rohatskahr@gmail.com>
2024-07-04 13:43:28 +0300

    class Guild

--- db/models.py
+++ db/models.py
@@ -10,3 +10,8 @@ class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="skills")
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
