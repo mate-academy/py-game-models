47524a84e6e8714ed91a6d0bc6b6ecc83b0be611
Halyna Rohatska <rohatskahr@gmail.com>
2024-07-04 14:22:17 +0300

    class Player

--- db/models.py
+++ db/models.py
@@ -14,4 +14,16 @@ class Skill(models.Model):
 
 class Guild(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    description = models.TextField(blank=True)
+    description = models.TextField(null=True, blank=True)
+
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="players_races")
+    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True, blank=True, related_name="players_guilds")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
