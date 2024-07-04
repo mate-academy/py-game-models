135a058997af80b8df8520cc9b4c575280d25949
mykyta <nikita.fedotov222@gmail.com>
2024-07-04 21:05:15 +0300

    solution

--- main.py
+++ main.py
@@ -8,35 +8,35 @@ from db.models import Race, Skill, Player, Guild
 def main() -> None:
     with open("players.json", "r") as players_file:
         players = json.load(players_file)
-        for player, player_data in players.items():
-            race_data = players[player]["race"]
-            race, _ = Race.objects.get_or_create(
-                name=race_data["name"],
-                description=race_data["description"]
+    for player, player_data in players.items():
+        race_data = players[player]["race"]
+        race, _ = Race.objects.get_or_create(
+            name=race_data["name"],
+            description=race_data["description"]
+        )
+
+        for skill in race_data.get("skills", []):
+            Skill.objects.get_or_create(
+                name=skill["name"],
+                bonus=skill["bonus"],
+                race=race
             )
 
-            for skill in race_data.get("skills", []):
-                Skill.objects.get_or_create(
-                    name=skill["name"],
-                    bonus=skill["bonus"],
-                    race=race
-                )
-
-            guild = None
-            guild_data = players[player]["guild"]
-            if guild_data:
-                guild, _ = Guild.objects.get_or_create(
-                    name=guild_data["name"],
-                    description=guild_data["description"]
-                )
-
-            Player.objects.create(
-                nickname=player,
-                email=player_data["email"],
-                bio=player_data["bio"],
-                race=race,
-                guild=guild
+        guild = None
+        guild_data = players[player]["guild"]
+        if guild_data:
+            guild, _ = Guild.objects.get_or_create(
+                name=guild_data["name"],
+                description=guild_data["description"]
             )
+
+        Player.objects.create(
+            nickname=player,
+            email=player_data["email"],
+            bio=player_data["bio"],
+            race=race,
+            guild=guild
+        )
 
 
 if __name__ == "__main__":
