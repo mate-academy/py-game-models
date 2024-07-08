892f1b6e66b6a1903464ee22e22ff9a82d43798f
Maksym Protsak <maksym.protsak@gmail.com>
2024-07-08 11:17:08 +0300

    Solution 08-07-2024 11-17

--- main.py
+++ main.py
@@ -8,7 +8,17 @@ from db.models import Race, Skill, Player, Guild
 def main() -> None:
     with open("players.json", "r") as player_file:
         data_in_file = json.load(player_file)
-    print(data_in_file)
+    for player in data_in_file:
+        player_nick_name = player
+        email = data_in_file[player]["email"]
+        bio = data_in_file[player]["bio"]
+        race = data_in_file[player]["race"]
+        race_name = race["name"]
+        race_description = race["description"]
+        race_skills = race["skills"]
+        guild = data_in_file[player]["guild"]
+        guild_name = guild["name"]
+        guild_description = guild["description"]
 
 
 if __name__ == "__main__":
