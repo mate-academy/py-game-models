489ac3a099826c82c0f5c5ae3fd9882606e01d30
Maksym Protsak <maksym.protsak@gmail.com>
2024-07-08 13:49:55 +0300

    Solution 08-07-2024 11-49

--- main.py
+++ main.py
@@ -6,18 +6,32 @@ from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
+    Race.objects.all().delete()
+    Skill.objects.all().delete()
+    Guild.objects.all().delete()
+    Player.objects.all().delete()
+
     with open("players.json", "r") as player_file:
         data_in_file = json.load(player_file)
+
     for player in data_in_file:
         email = data_in_file[player]["email"]
         bio = data_in_file[player]["bio"]
         race = data_in_file[player]["race"]
-        race_name = race["name"]
+        race_name = race["name"].capitalize()
         race_description = race["description"]
         race_skills = race["skills"]
         guild = data_in_file[player]["guild"]
-        guild_name = guild["name"]
-        guild_description = guild["description"]
+        guild_name = guild["name"] if guild else None
+        guild_description = guild["description"] if guild else None
+
+        Player.objects.create(
+            nickname=player,
+            email=email,
+            bio=bio,
+            race=Race(name=race_name, description=race_description).save(),
+            guild=Guild(name=guild_name, description=guild_description).save()
+        )
         break
 
 
