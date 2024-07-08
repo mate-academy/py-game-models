d89b799b1442c4d68899f6894e06afbb9e9b3a00
Maksym Protsak <maksym.protsak@gmail.com>
2024-07-08 23:05:46 +0300

    Solution 08-07-2024 23-05

--- main.py
+++ main.py
@@ -18,22 +18,35 @@ def main() -> None:
         email = data_in_file[player]["email"]
         bio = data_in_file[player]["bio"]
         race = data_in_file[player]["race"]
-        race_name = race["name"].capitalize()
+        race_name = race["name"]
         race_description = race["description"]
         race_skills = race["skills"]
         guild = data_in_file[player]["guild"]
         guild_name = guild["name"] if guild else None
         guild_description = guild["description"] if guild else None
 
+        obj_race, created_race = Race.objects.get_or_create(
+            name=race_name,
+            description=race_description
+        )
+
+        if guild is not None:
+            obj_guild, created_guild = Guild.objects.get_or_create(
+                name=guild_name,
+                description=guild_description
+            )
+
+        for skill in race_skills:
+            Skill.objects.get_or_create(
+                name=skill["name"],
+                bonus=skill["bonus"],
+                race=obj_race
+            )
+
         Player.objects.create(
             nickname=player,
             email=email,
             bio=bio,
-            race=Race(name=race_name, description=race_description).save(),
-            guild=Guild(name=guild_name, description=guild_description).save()
+            race=obj_race,
+            guild=obj_guild
         )
-        break
-
-
-if __name__ == "__main__":
-    main()
