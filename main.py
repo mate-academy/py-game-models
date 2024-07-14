5fbed261f6c21e08f22afacda6d11b14006e6cee
TsvetanKichuk <toys4babyodua@gmail.com>
2024-07-14 11:01:36 +0200

    'Solution'

--- main.py
+++ main.py
@@ -6,39 +6,39 @@ from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    with open('players.json') as f:
+    with open("players.json") as f:
         data = json.load(f)
 
-    player = data.get('john', {}).get('race')
+    player = data.get("john", {}).get("race")
     Race.objects.get_or_create(
-         name=player['name'],
-         description=player['description'],
-     )
+        name=player["name"],
+        description=player["description"],
+    )
     print(Race.objects.all())
 
-    guilds = data.get('john', {}).get('guild')
+    guilds = data.get("john", {}).get("guild")
     Guild.objects.get_or_create(
-        name=guilds['name'],
-        description=guilds['description'],
+        name=guilds["name"],
+        description=guilds["description"],
     )
     print(Guild.objects.all())
 
-    skills = data.get('john', {}).get('race').get('skills')
+    skills = data.get("john", {}).get("race").get("skills")
     skill = skills[1]
     Skill.objects.create(
-        name=skill['name'],
-        bonus=skill['bonus'],
+        name=skill["name"],
+        bonus=skill["bonus"],
         race_id=5
     )
     print(Skill.objects.all())
 
-    player_data = data['john']
+    player_data = data["john"]
     for player_name in data:
-        if player_name == 'john':
+        if player_name == "john":
             Player.objects.create(
                 nickname=player_name,
-                email=player_data.get('email'),
-                bio=player_data.get('bio'),
+                email=player_data.get("email"),
+                bio=player_data.get("bio"),
                 race_id=5,
                 guild_id=3,
             )
