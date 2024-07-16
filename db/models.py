c085dc7bd46b93c45ee9c091f93edfb9212eea93
Anton Bilous <anton.bilosus@gmail.com>
2024-07-16 12:05:31 +0300

    Fix custom related_name

--- db/models.py
+++ db/models.py
@@ -15,7 +15,7 @@ class Skill(models.Model):
     race = models.ForeignKey(
         Race,
         on_delete=models.CASCADE,
-        related_name="skills",
+        related_name="skill_set",
     )
 
     def __str__(self) -> str:
