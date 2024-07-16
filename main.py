89456eea50ab6189a11de6f392635842d6a6b5b1
bythewaters <bythewaters800@gmail.com>
2024-07-16 20:49:47 +0200

    orm

--- main.py
+++ main.py
@@ -8,30 +8,30 @@ from db.models import Race, Skill, Player, Guild
 def main() -> None:
     with open("players.json", "r") as players_file:
         players = json.load(players_file)
-        for nickname, data in players.items():
-            race, _ = Race.objects.get_or_create(
-                name=data["race"]["name"],
-                description=data["race"]["description"]
+    for nickname, data in players.items():
+        race, _ = Race.objects.get_or_create(
+            name=data["race"]["name"],
+            description=data["race"]["description"]
+        )
+        if data["guild"]:
+            guild, _ = Guild.objects.get_or_create(
+                name=data["guild"]["name"],
+                description=data["guild"]["description"]
             )
-            if data["guild"]:
-                guild, _ = Guild.objects.get_or_create(
-                    name=data["guild"]["name"],
-                    description=data["guild"]["description"]
-                )
-            else:
-                guild = None
-            [
-                Skill.objects.get_or_create(
-                    name=skill["name"], bonus=skill["bonus"], race=race
-                ) for skill in data["race"]["skills"]
-            ]
-            Player.objects.get_or_create(
-                nickname=nickname,
-                email=data["email"],
-                bio=data["bio"],
-                race=race,
-                guild=guild
-            )
+        else:
+            guild = None
+        [
+            Skill.objects.get_or_create(
+                name=skill["name"], bonus=skill["bonus"], race=race
+            ) for skill in data["race"]["skills"]
+        ]
+        Player.objects.get_or_create(
+            nickname=nickname,
+            email=data["email"],
+            bio=data["bio"],
+            race=race,
+            guild=guild
+        )
 
 
 if __name__ == "__main__":
