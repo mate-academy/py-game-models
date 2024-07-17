525824a852dc623e1abaee3ce9dbf678e047b862
Bohdan Bastiuk <bbastyuk@gmail.com>
2024-07-17 19:23:26 +0200

    fix flake

--- main.py
+++ main.py
@@ -17,20 +17,25 @@ def main() -> None:
         race_name = race_data.get("name")
         race_description = race_data.get("description", "")
 
-        race, _ = Race.objects.get_or_create(name=race_name, defaults={"description": race_description})
+        race, _ = (Race.objects.get_or_create
+                   (name=race_name, defaults={"description"
+                                              : race_description}))
 
         skills = race_data.get("skills", [])
         for skill_data in skills:
             skill_name = skill_data.get("name")
             skill_bonus = skill_data.get("bonus")
-            Skill.objects.get_or_create(name=skill_name, bonus=skill_bonus, race=race)
+            Skill.objects.get_or_create(name=skill_name,
+                                        bonus=skill_bonus, race=race)
 
         guild_data = player_data.get("guild")
         guild = None
         if guild_data:
             guild_name = guild_data.get("name")
             guild_description = guild_data.get("description", "")
-            guild, _ = Guild.objects.get_or_create(name=guild_name, defaults={"description": guild_description})
+            guild, _ = (Guild.objects.get_or_create
+                        (name=guild_name, defaults={"description"
+                                                    : guild_description}))
 
         Player.objects.create(
             nickname=player_name,
