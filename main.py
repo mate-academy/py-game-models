06a727247f5d242401b75c4bac41a874fd3ceb5c
Vsevoloddeez <143706389+Vsevoloddeez@users.noreply.github.com>
2024-07-28 16:27:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,8 @@
-import json
-from typing import Optional, Any
 import init_django_orm  # noqa: F401
-from db.models import Race, Skill, Player, Guild
 
-
-def load_players_from_file() -> dict[str, Any]:
-    with open("players.json", "r") as file:
-        players = json.load(file)
-    return players
-
-
-def create_race(race_info: dict[str, Any]) -> Race:
-    race, _ = Race.objects.get_or_create(
-        name=race_info["name"],
-        description=race_info["description"]
-    )
-    return race
-
-
-def create_skills(skills_info: list[dict[str, Any]], race: Race) -> None:
-    for skill in skills_info:
-        Skill.objects.get_or_create(
-            name=skill["name"],
-            bonus=skill["bonus"],
-            race=race
-        )
+from typing import Optional, Any
+from django.db.models import QuerySet
+from db.models import Actor, Genre, Guild
 
 
 def create_guild(guild_info: Optional[dict[str, Any]]) -> Optional[Guild]:
@@ -33,35 +11,39 @@ def create_guild(guild_info: Optional[dict[str, Any]]) -> Optional[Guild]:
             name=guild_info["name"],
             description=guild_info["description"]
         )
-    else:
-        guild = None
-    return guild
+        return guild
+    return None
 
 
-def create_player(
-    player: str,
-    player_info: dict[str, Any],
-    race: Race,
-    guild: Optional[Guild]
-) -> None:
-    Player.objects.create(
-        nickname=player,
-        email=player_info["email"],
-        bio=player_info["bio"],
-        race=race,
-        guild=guild,
+def main() -> QuerySet:
+    genres = [
+        "Western",
+        "Action",
+        "Dramma",
+    ]
+    for genre in genres:
+        Genre.objects.create(name=genre)
+
+    actors = [
+        ("George", "Clooney"),
+        ("Keanu", "Reeves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson"),
+    ]
+    for first_name, last_name in actors:
+        Actor.objects.create(first_name=first_name, last_name=last_name)
+
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(first_name="Kianu").update(
+        first_name="Keanu", last_name="Reeves"
     )
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
 
-
-def main() -> None:
-    players = load_players_from_file()
-
-    for player, player_info in players.items():
-        race = create_race(player_info.get("race"))
-        create_skills(player_info.get("race").get("skills"), race)
-        guild = create_guild(player_info.get("guild"))
-
-        create_player(player, player_info, race, guild)
+    return Actor.objects.filter(last_name="Smith").all().order_by("first_name")
 
 
 if __name__ == "__main__":
