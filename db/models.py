f91d795fbdc4a0d1121dcf370d94da15354f0979
Vsevoloddeez <143706389+Vsevoloddeez@users.noreply.github.com>
2024-07-28 16:28:52 +0300

    Update models.py

--- db/models.py
+++ db/models.py
@@ -1,26 +1,24 @@
 from django.db import models
 
 
-class Race(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    description = models.TextField(blank=True)
+class Actor(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 
-class Skill(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Guild(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    description = models.TextField(null=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
 
-
-class Player(models.Model):
-    nickname = models.CharField(max_length=255, unique=True)
-    email = models.EmailField(max_length=255)
-    bio = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE)
-    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
