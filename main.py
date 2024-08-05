a9b02fd9e5e79718576f152ce480fcd02ea105a0
Vsevoloddeez <143706389+Vsevoloddeez@users.noreply.github.com>
2024-08-05 19:03:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
+
 def main() -> None:
     with open("players.json", "r") as players_file:
         players = json.load(players_file)
@@ -13,33 +14,34 @@ def main() -> None:
         if player_guild_data:
             guild, _ = Guild.objects.get_or_create(
                 name=player_guild_data["name"],
-                defaults={'description': player_guild_data["description"]}
+                defaults={"description": player_guild_data["description"]}
             )
         else:
             guild = None
 
         race, _ = Race.objects.get_or_create(
             name=player_data["race"]["name"],
-            defaults={'description': player_data["race"]["description"]}
+            defaults={"description": player_data["race"]["description"]}
         )
 
         skills_to_add = []
         for skill in player_data["race"].get("skills", []):
             skill_instance, _ = Skill.objects.get_or_create(
                 name=skill["name"],
-                defaults={'bonus': skill["bonus"], 'race': race}
+                defaults={"bonus": skill["bonus"], "race": race}
             )
             skills_to_add.append(skill_instance)
 
         Player.objects.get_or_create(
             nickname=player_id,
             defaults={
-                'email': player_data["email"],
-                'bio': player_data["bio"],
-                'race': race,
-                'guild': guild
+                "email": player_data["email"],
+                "bio": player_data["bio"],
+                "race": race,
+                "guild": guild
             }
         )
+
 
 if __name__ == "__main__":
     main()
