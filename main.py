2f6969cbe2116944a3ed209ab6f58135e2338b29
Mykola Kokoiev <veokoknik@gmail.com>
2024-08-10 19:33:02 +0200

    SOL

--- main.py
+++ main.py
@@ -9,31 +9,31 @@ def main() -> None:
 
     for nickname, data in loaded_json.items():
 
-        obj, created = Race.objects.get_or_create(
+        race, created = Race.objects.get_or_create(
             name=data["race"].get("name"),
             description=data["race"].get("description")
         )
 
-        if data["guild"]:
-            obj, created = Guild.objects.get_or_create(
+        if data.get("guild"):
+            guild, created = Guild.objects.get_or_create(
                 name=data["guild"].get("name") if data["guild"] else None,
                 description=data["guild"].get("description")
             )
 
         for skill in data["race"]["skills"]:
-            obj, created = Skill.objects.get_or_create(
+            skill, created = Skill.objects.get_or_create(
                 name=skill.get("name"),
                 bonus=skill.get("bonus"),
                 race=Race.objects.get(name=data["race"]["name"])
             )
 
-        obj, created = Player.objects.get_or_create(
+        Player.objects.create(
             nickname=nickname,
             email=data["email"],
             bio=data["bio"],
             race=Race.objects.get(name=data["race"]["name"]),
             guild=(Guild.objects.get(name=data["guild"]["name"])
-                   if data["guild"] else None)
+                   if data.get("guild") else None)
         )
 
 
