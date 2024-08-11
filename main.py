144a942730ee4d44131c339fc8bb35c778eff17f
Stepan <oleksiukstepann@gmail.com>
2024-08-11 22:23:08 +0300

    "Solution"

--- main.py
+++ main.py
@@ -1,11 +1,65 @@
+import json
+from django.db import IntegrityError
+
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    try:
+        with open("players.json", "r") as file:
+            players = json.load(file)
+    except FileNotFoundError as e:
+        print(e)
+
+    for player_nickname, player_info in players.items():
+        # print(player_nickname, player_info.get("guild"))
+        try:
+            create_players(player_nickname, player_info)
+        except IntegrityError as e:
+            print(e)
+
+
+def create_players(player_nickname: str, player_info: dict) -> None:
+    race, created = Race.objects.get_or_create(
+        name=player_info.get("race").get("name"),
+        defaults={"description": player_info.get("race").get("description")},
+    )
+
+    for skill in player_info.get("race").get("skills"):
+        Skill.objects.get_or_create(
+            name=skill.get("name"),
+            race=race,
+            defaults={"bonus": skill.get("bonus")}
+        )
+
+    guild_name = player_info.get("guild")
+    if guild_name:
+        guild, created = Guild.objects.get_or_create(
+            name=guild_name.get("name"),
+            defaults={
+                "description": player_info.get("guild").get("description")
+            }
+        )
+    else:
+        guild = None
+
+    Player.objects.get_or_create(
+        nickname=player_nickname,
+        race=race,
+        guild=guild,
+        defaults={
+            "email": player_info.get("email"),
+            "bio": player_info.get("bio"),
+        }
+    )
 
 
 if __name__ == "__main__":
     main()
+
+    # Player.objects.all().delete()
+    # Guild.objects.all().delete()
+    # Race.objects.all().delete()
+    # Skill.objects.all().delete()
