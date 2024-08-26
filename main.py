e189c1fc3b810d49a88749e680dea33261b5ea67
Myroslava Nezbrytska <miroslavanezbricka@gmail.com>
2024-08-26 17:54:09 +0300

    "Solution"

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
+import json
+
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r") as file:
+        players_data = json.load(file)
+        print(players_data)
+
+    for nickname, player_data in players_data.items():
+        race_data = player_data["race"]
+        race, _ = Race.objects.get_or_create(
+            name=race_data["name"],
+            defaults={"description": race_data["description"]}
+        )
+
+        skills = []
+        for skill_data in race_data.get("skills", []):
+            skill, _ = Skill.objects.get_or_create(
+                name=skill_data["name"],
+                defaults={"bonus": skill_data["bonus"], "race": race}
+            )
+            skills.append(skill)
+
+        guild = None
+        if player_data["guild"]:
+            guild_data = player_data["guild"]
+            guild, _ = Guild.objects.get_or_create(
+                name=guild_data["name"],
+                defaults={"description": guild_data["description"]}
+            )
+
+        player, created = Player.objects.get_or_create(
+            nickname=nickname,
+            defaults={
+                "email": player_data["email"],
+                "bio": player_data["bio"],
+                "race": race,
+                "guild": guild,
+            }
+        )
+
+        if created:
+            player.skills.set(skills)
+            player.save()
 
 
 if __name__ == "__main__":
