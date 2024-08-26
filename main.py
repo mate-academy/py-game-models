fc351462ce626d7f328298dbebecae960f1058af
Valeriia Kyrylenko <valerya.kyrylenko@gmail.com>
2024-08-26 15:13:59 +0200

    implement py-game-models 3

--- main.py
+++ main.py
@@ -12,32 +12,32 @@ def load_players(file_path: str) -> dict:
         return json.load(file)
 
 
-def race(players: dict, person: str) -> Optional[Race]:
+def race(info: dict) -> Optional[Race]:
     race_obj, _ = Race.objects.get_or_create(
-        name=players[person]["race"]["name"],
-        description=players[person]["race"]["description"]
-        if players[person]["race"]["description"] else None,
-    ) if players[person]["race"] else None
+        name=info["race"]["name"],
+        description=info["race"]["description"]
+        if info["race"]["description"] else None,
+    ) if info["race"] else None
 
     return race_obj
 
 
-def skill(players: dict, person: str) -> None:
-    for skill in players[person]["race"]["skills"]:
+def skill(info: dict) -> None:
+    for skill in info["race"]["skills"]:
         Skill.objects.get_or_create(
             name=skill["name"],
             bonus=skill["bonus"],
-            race=race(players, person)
+            race=race(info)
         )
 
 
-def guild(players: dict, person: str) -> Optional[Guild]:
+def guild(info: dict) -> Optional[Guild]:
     guild_obj = None
-    if players[person]["guild"]:
+    if info["guild"]:
         guild_obj, _ = Guild.objects.get_or_create(
-            name=players[person]["guild"]["name"],
-            description=players[person]["guild"]["description"]
-            if players[person]["guild"]["description"] else None,
+            name=info["guild"]["name"],
+            description=info["guild"]["description"]
+            if info["guild"]["description"] else None,
         )
 
     return guild_obj
@@ -49,11 +49,11 @@ def main() -> None:
     for nickname, info in players.items():
         email = info["email"]
         bio = info["bio"]
-        race_for_person = race(players, nickname)
+        race_for_person = race(info)
 
-        skill(players, nickname)
+        skill(info)
 
-        guild_for_person = guild(players, nickname)
+        guild_for_person = guild(info)
 
         Player.objects.get_or_create(
             nickname=nickname,
