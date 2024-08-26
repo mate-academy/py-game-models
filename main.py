ae1d8b4329a6779d872d7b1ab3424d1736ca0b02
Stepan <oleksiukstepann@gmail.com>
2024-08-26 12:16:28 +0300

    "Divided into a few methods and moved Player creation to the main section"

--- main.py
+++ main.py
@@ -13,15 +13,30 @@ def main() -> None:
         print(e)
 
     for player_nickname, player_info in players.items():
-        create_players(player_nickname, player_info)
+        race = get_race(player_info)
+        create_skills(race, player_info)
+        guild = get_guild(player_info)
+
+        Player.objects.get_or_create(
+            nickname=player_nickname,
+            race=race,
+            guild=guild,
+            defaults={
+                "email": player_info.get("email"),
+                "bio": player_info.get("bio"),
+            }
+        )
 
 
-def create_players(player_nickname: str, player_info: dict) -> None:
-    race, created = Race.objects.get_or_create(
+def get_race(player_info: dict) -> Race:
+    race, _ = Race.objects.get_or_create(
         name=player_info.get("race").get("name"),
         defaults={"description": player_info.get("race").get("description")},
     )
+    return race
 
+
+def create_skills(race: Race, player_info: dict) -> None:
     for skill in player_info.get("race").get("skills"):
         Skill.objects.get_or_create(
             name=skill.get("name"),
@@ -29,23 +44,16 @@ def create_players(player_nickname: str, player_info: dict) -> None:
             defaults={"bonus": skill.get("bonus")}
         )
 
+
+def get_guild(player_info: dict) -> Guild | None:
+    guild = None
     guild_name = player_info.get("guild")
     if guild_name:
-        guild, created = Guild.objects.get_or_create(
+        guild, _ = Guild.objects.get_or_create(
             name=guild_name.get("name"),
             defaults={
                 "description": player_info.get("guild").get("description")
             }
         )
-    else:
-        guild = None
 
-    Player.objects.get_or_create(
-        nickname=player_nickname,
-        race=race,
-        guild=guild,
-        defaults={
-            "email": player_info.get("email"),
-            "bio": player_info.get("bio"),
-        }
-    )
+    return guild
