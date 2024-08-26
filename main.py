29d987d6db737bee5eac3a819a193dc30b5af7b5
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-08-26 13:12:20 +0300

    Fix

--- main.py
+++ main.py
@@ -1,34 +1,51 @@
-import init_django_orm  # noqa: F401
 import json
+from typing import List
+
+import init_django_orm  # noqa: F401
 from db.models import Race, Skill, Player, Guild
 
 
-def main() -> None:
-    with open("players.json", "r") as file:
-        data = json.load(file)
-    for user, user_data in data.items():
-        race_name = user_data["race"]["name"]
-        race_description = user_data["race"]["description"]
-        race, created_race = Race.objects.get_or_create(
-            name=race_name,
-            description=race_description
+def create_races(data_values: List[dict]) -> None:
+    for user_data in data_values:
+        Race.objects.get_or_create(
+            name=user_data["race"]["name"],
+            description=user_data["race"]["description"]
+        )
+
+
+def create_skills(data_values: List[dict]) -> None:
+    for user_data in data_values:
+        race = Race.objects.get(
+            name=user_data["race"]["name"]
         )
         for skill in user_data["race"]["skills"]:
-            skill_name = skill["name"]
-            skill_bonus = skill["bonus"]
             Skill.objects.get_or_create(
-                name=skill_name,
-                bonus=skill_bonus,
+                name=skill["name"],
+                bonus=skill["bonus"],
                 race=race
             )
+
+
+def create_guilds(data_values: List[dict]) -> None:
+    for user_data in data_values:
+        if user_data["guild"]:
+            guild_data = user_data.get("guild")
+            Guild.objects.get_or_create(
+                name=guild_data.get("name"),
+                description=guild_data.get("description", None)
+            )
+
+
+def create_players(data: dict) -> None:
+    for user, user_data in data.items():
+        race = Race.objects.get(
+            name=user_data["race"]["name"]
+        )
         guild = None
         if user_data["guild"]:
-            guild_data = user_data.get("guild", {})
-            guild_name = guild_data.get("name", None)
-            guild_description = guild_data.get("description", None)
-            guild, create = Guild.objects.get_or_create(
-                name=guild_name,
-                description=guild_description
+            guild = Guild.objects.get(
+                name=user_data.get("guild")
+                .get("name")
             )
         Player.objects.create(
             nickname=user,
@@ -37,6 +54,15 @@ def main() -> None:
             race=race,
             guild=guild
         )
+
+
+def main() -> None:
+    with open("players.json", "r") as file:
+        data = json.load(file)
+    create_races(data.values())
+    create_skills(data.values())
+    create_guilds(data.values())
+    create_players(data)
 
 
 if __name__ == "__main__":
