3731578a42b5cc841ec250262f5df1df8eb43b21
Halyna Rohatska <rohatskahr@gmail.com>
2024-08-26 14:16:37 +0300

    split to small func

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import json
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
-import json
 
 
 def load_players(file_path: str) -> dict:
@@ -11,41 +11,50 @@ def load_players(file_path: str) -> dict:
     return data
 
 
+def get_or_create_race(race: dict) -> Race:
+    get_race, created = Race.objects.get_or_create(
+        name=race["name"],
+        description=race["description"]
+    )
+    return get_race if get_race else created
+
+
+def get_or_create_skills(skills: list, race: Race) -> None:
+    for skill in skills:
+        Skill.objects.get_or_create(
+            name=skill["name"],
+            bonus=skill["bonus"],
+            race=race
+        )
+
+
+def get_or_create_guild(guild) -> Guild:
+    get_guild, created = Guild.objects.get_or_create(
+        name=guild["name"],
+        description=guild["description"]
+    )
+
+    return get_guild if get_guild else created
+
+
 def main() -> None:
     path_to_file = "players.json"
     data_players = load_players(path_to_file)
 
-    for key, value in data_players.items():
+    for name, attribute in data_players.items():
+        data_race = attribute["race"]
+        get_race = get_or_create_race(data_race)
 
-        data_race = value["race"]
-        get_race, created = Race.objects.get_or_create(
-            name=data_race["name"],
-            description=data_race["description"]
-        )
-
-        for skill in data_race["skills"]:
-            Skill.objects.get_or_create(
-                name=skill["name"],
-                bonus=skill["bonus"],
-                race=get_race
-            )
+        get_or_create_skills(data_race["skills"], get_race)
 
         get_guild = None
-        if value["guild"]:
-            data_guild = value["guild"]
-            get_guild, created = Guild.objects.get_or_create(
-                name=data_guild["name"],
-                description=data_guild["description"]
-            )
+        if attribute["guild"]:
+            get_guild = get_or_create_guild(attribute["guild"])
 
         Player.objects.get_or_create(
-            nickname=key,
-            email=value["email"],
-            bio=value["bio"],
+            nickname=name,
+            email=attribute["email"],
+            bio=attribute["bio"],
             race=get_race,
             guild=get_guild
         )
-
-
-if __name__ == "__main__":
-    main()
