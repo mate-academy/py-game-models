ed767555ee7045d9aabc14426aaca9c4e2bdd841
DSahalatyi <dsahalatyi@gmail.com>
2024-08-26 16:15:11 +0300

    fix arguments positioning

--- db/models.py
+++ db/models.py
@@ -2,23 +2,23 @@ from django.db import models
 
 
 class Race(models.Model):
-    name = models.CharField(unique=True, max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
 
 
 class Skill(models.Model):
-    name = models.CharField(unique=True, max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
 
 
 class Guild(models.Model):
-    name = models.CharField(unique=True, max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True)
 
 
 class Player(models.Model):
-    nickname = models.CharField(unique=True, max_length=255)
+    nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
