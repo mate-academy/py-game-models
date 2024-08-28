3dc9467ae38ff458bc58cef08b2cf71be147f6d1
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-08-28 08:47:06 +0300

    fix

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ def main() -> None:
     for player_name, player_data in players.items():
         race_info = player_data["race"]["description"] or ""
         race_obj, _ = Race.objects.get_or_create(
-            name = player_data["race"]["name"],
-            description = race_info,
+            name=player_data["race"]["name"],
+            description=race_info,
         )
 
         for skill in player_data["race"]["skills"]:
             Skill.objects.get_or_create(
-                name = skill["name"],
-                race = race_obj,
-                bonus = skill["bonus"],
+                name=skill["name"],
+                race=race_obj,
+                bonus=skill["bonus"],
             )
 
         guild_obj = None
@@ -28,13 +28,13 @@ def main() -> None:
             guild_name = player_data["guilds"]["name"]
             guild_description = player_data["guilds"]["description"]
             guild_obj, _ = Guild.objects.get_or_create(
-                name = guild_name,
-                description = guild_description,
+                name=guild_name,
+                description=guild_description,
             )
 
         Player.objects.update_or_create(
-            nickname = player_name,
-            defaults = {
+            nickname=player_name,
+            defaults={
                 "email": player_data["email"],
                 "bio": player_data["bio"],
                 "race": race_obj,
