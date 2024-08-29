3684704e1e4f52916342a777345e709712fbd285
Anton Bilous <anton.bilosus@gmail.com>
2024-08-29 09:35:43 +0300

    Make race description nullable

--- db/models.py
+++ db/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    description = models.TextField(blank=True)
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self) -> str:
         return self.name
