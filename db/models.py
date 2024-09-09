fb7d5358079b88559d3a3cd7c0c887eab97b13a3
Valeriy Morkvych <morkvych@gmail.com>
2024-09-09 18:16:04 +0300

    models

--- db/models.py
+++ db/models.py
@@ -1,1 +1,32 @@
 from django.db import models
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+
+
+class Skill(models.Model):
+    name = models.CharField(unique=True, max_length=255)
+    bonus = models.CharField(max_length=255)
+    race = models.ForeignKey(
+        Race, on_delete=models.CASCADE, related_name="skills"
+    )
+
+
+class Guild(models.Model):
+    name = models.CharField(unique=True, max_length=255)
+    description = models.TextField(null=True)
+
+
+class Player(models.Model):
+    nickname = models.CharField(unique=True, max_length=255)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField(max_length=255)
+    race = models.ForeignKey(
+        Race, on_delete=models.PROTECT, related_name="players"
+    )
+    guild = models.ForeignKey(
+        Guild, on_delete=models.SET_NULL, null=True, related_name="players"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
