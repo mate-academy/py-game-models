7d25097d3a866b4f55264640b8cb227316f47165
andreykolomiec <akolomiec982@gmail.com>
2024-09-18 01:23:07 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,53 @@
 from django.db import models
+from django.db.models import CharField
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=255, unique=True,)
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> CharField:
+        return self.name
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(max_length=255)
+    race = models.ForeignKey(
+        Race,
+        on_delete=models.CASCADE,
+        related_name="skill_set"
+    )
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.race})"
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True,)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> CharField:
+        return self.name
+
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255, unique=True,)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField(max_length=255)
+    race = models.ForeignKey(
+        Race,
+        on_delete=models.CASCADE,
+        related_name="players_race"
+    )
+    guild = models.ForeignKey(
+        Guild,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="guild_members"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> CharField:
+        return self.nickname
--- main.py
+++ main.py
@@ -1,10 +1,48 @@
 import init_django_orm  # noqa: F401
+import json
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    # Зчитуємо дані з файлу players.json
+    with open("players.json", "r") as file:
+        players_data = json.load(file)
+        print(players_data)
+
+        for nickname, player_data in players_data.items():
+            # Спочатку отримуємо або створюємо расу
+            race_data = player_data["race"]
+            race, _ = Race.objects.get_or_create(
+                name=race_data["name"],
+                defaults={"description": race_data.get("description", "")}
+            )
+            # Отримуємо або створюємо гільдію
+            guild_data = player_data.get("guild", None)
+            guild = None
+            if guild_data:
+                guild, _ = Guild.objects.get_or_create(
+                    name=guild_data["name"],
+                    defaults={"description": guild_data.get("description", "")}
+                )
+
+            # Додаємо навички гравця
+            skills_data = race_data.get("skills", [])
+            for skill_data in skills_data:
+                Skill.objects.get_or_create(
+                    name=skill_data["name"],
+                    race=race,
+                    defaults={"bonus": skill_data.get("bonus", "")}
+                )
+
+            # Додаємо або отримуємо гравця
+            Player.objects.get_or_create(
+                nickname=nickname,
+                email=player_data["email"],
+                bio=player_data.get("bio", ""),
+                race=race,
+                guild=guild
+            )
 
 
 if __name__ == "__main__":
