89f8ca83f862c8af2d436cc2e7078c61dfc7c7af
Igor Kolisnyk <moonbosszzz@gmail.com>
2024-10-01 12:34:24 +0200

    The solution of the task

--- db/models.py
+++ db/models.py
@@ -5,31 +5,38 @@ class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.name}"
+
 
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name='skills')
+    race = models.ForeignKey(Race, on_delete=models.CASCADE,
+                             related_name="skills")
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.name}: {self.race}"
+
 
 class Guild(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True, null=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.name}"
+
 
 class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255, unique=True)
     bio = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name='players')
-    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True, related_name='players')
+    race = models.ForeignKey(Race, on_delete=models.CASCADE,
+                             related_name="players")
+    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL,
+                              null=True, related_name="players")
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"{self.nickname}: {self.race}, in {self.guild} guild. ({self.created_at})"
+    def __str__(self) -> str:
+        return (f"{self.nickname}: {self.race}, in "
+                f"{self.guild} guild. ({self.created_at})")
