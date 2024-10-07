1dd49a168c1320cd591611627bc9d08c298675aa
Viacheslav417865 <sfinks417865@gmail.com>
2024-10-07 21:59:42 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -12,7 +12,8 @@ class Race(models.Model):
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="skill_set")
+    race = models.ForeignKey(Race, on_delete=models.CASCADE,
+                             related_name="skill_set")
 
     def __str__(self) -> str:
         return self.name
