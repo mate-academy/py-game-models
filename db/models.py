64d811e29a01b867c218f3c3c7f5fbd417e2ac86
Andrew Chugreev <andrey.4.1985@gmail.com>
2024-10-16 00:06:11 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,29 @@
+from tkinter.constants import CASCADE
+
 from django.db import models
+from django.db.models import SET_NULL
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True)
+
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    bio = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.PROTECT)
+    guild = models.ForeignKey(Guild, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(null=True, auto_now=True)
