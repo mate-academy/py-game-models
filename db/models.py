14cf15189df127d2fbd0f65d1c61413a099cebf0
Max <mxbely98@gmail.com>
2024-10-21 19:14:50 +0300

    unpacking player.json file to db

--- db/models.py
+++ db/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.db.models import CharField, TextField, ForeignKey, DateTimeField, EmailField
+from django.db.models import (CharField, TextField,
+                              ForeignKey, DateTimeField, EmailField)
 
 
 class Race(models.Model):
@@ -15,14 +16,16 @@ class Guild(models.Model):
 class Skill(models.Model):
     name = CharField(max_length=255, unique=True)
     bonus = CharField(max_length=255)
-    race = ForeignKey(Race, on_delete=models.DO_NOTHING, related_name="skills")
+    race = ForeignKey(Race, on_delete=models.DO_NOTHING,
+                      related_name="skills")
 
 
 class Player(models.Model):
     nickname = CharField(max_length=255, unique=True)
     email = EmailField(max_length=255)
     bio = CharField(max_length=255)
-    race = ForeignKey(Race, on_delete=models.DO_NOTHING, related_name="players")
-    guild = ForeignKey(Guild, on_delete=models.DO_NOTHING, null=True, related_name="guilds")
+    race = ForeignKey(Race, on_delete=models.DO_NOTHING,
+                      related_name="players")
+    guild = ForeignKey(Guild, on_delete=models.DO_NOTHING,
+                       null=True, related_name="guilds")
     created_at = DateTimeField(auto_now=True)
-
