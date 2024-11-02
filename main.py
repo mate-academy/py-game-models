b0452a36fda854af9fbfb3192ba2219024d45c36
user <nikitaskrypets@gmail.com>
2024-11-02 10:06:23 +0200

    Improved

--- main.py
+++ main.py
@@ -9,29 +9,22 @@ def main() -> None:
     with open("players.json") as players_data:
         players_info = json.load(players_data)
     for player_name, player_data in players_info.items():
-        race, _ = Race.objects.get_or_create(
-            name=player_data["race"]["name"],
-            defaults={"description": player_data["race"]["description"]}
-        )
-        skills = []
-        for skill_data in player_data["race"]["skills"]:
-            skill, _ = Skill.objects.get_or_create(
-                name=skill_data["name"],
-                defaults={"bonus": skill_data["bonus"], "race": race}
-            )
-            skills.append(skill)
-        if player_data.get("guild"):
-            guild, _ = Guild.objects.get_or_create(
-                name=player_data["guild"]["name"],
-                defaults={"description": player_data["guild"]["description"]}
-            )
-        else:
-            guild = None
         player = Player(
             nickname=player_name,
             email=player_data["email"],
             bio=player_data["bio"],
-            race=race,
-            guild=guild,
+            race=Race.objects.get_or_create(
+                name=player_data["race"]["name"],
+                defaults={"description": player_data["race"]["description"]}
+            )[0],
+            guild=Guild.objects.get_or_create(
+                name=player_data["guild"]["name"],
+                defaults={"description": player_data["guild"]["description"]}
+            )[0] if player_data.get("guild") else None,
         )
+        for skill_data in player_data["race"]["skills"]:
+            Skill.objects.get_or_create(
+                name=skill_data["name"],
+                defaults={"bonus": skill_data["bonus"], "race": player.race}
+            )
         player.save()
