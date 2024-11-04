da6dea91a01c275f9bad8b397fccbbc933d9986d
mregorlite <egor.infinity@gmail.com>
2024-11-04 12:35:07 +0200

    Solution 2

--- main.py
+++ main.py
@@ -6,45 +6,33 @@ from db.models import Race, Skill, Player, Guild
 
 
 def get_or_create_race(race: dict) -> Race:
-    try:
-        obj = Race.objects.get(name=race.get("name"))
-        return obj
-    except Race.DoesNotExist:
-        obj = Race.objects.create(
-            name=race.get("name"),
-            description=race.get("description")
-        )
-        obj.save()
-        return obj
+    obj, _ = Race.objects.get_or_create(
+        name=race.get("name"),
+        description=race.get("description")
+    )
+    obj.save()
+    return obj
 
 
 def get_or_create_skill(skill: dict, race: Race) -> Skill:
-    try:
-        obj = Skill.objects.get(name=skill.get("name"))
-        return obj
-    except Skill.DoesNotExist:
-        obj = Skill.objects.create(
-            name=skill.get("name"),
-            bonus=skill.get("bonus"),
-            race=race
-        )
-        obj.save()
-        return obj
+    obj, _ = Skill.objects.get_or_create(
+        name=skill.get("name"),
+        bonus=skill.get("bonus"),
+        race=race
+    )
+    obj.save()
+    return obj
 
 
 def get_or_create_guild(guild: dict | None) -> Guild | None:
     if guild is None:
         return
-    try:
-        obj = Guild.objects.get(name=guild.get("name"))
-        return obj
-    except Guild.DoesNotExist:
-        obj = Guild.objects.create(
-            name=guild.get("name"),
-            description=guild.get("description")
-        )
-        obj.save()
-        return obj
+    obj, _ = Guild.objects.get_or_create(
+        name=guild.get("name"),
+        description=guild.get("description")
+    )
+    obj.save()
+    return obj
 
 
 def add_player(
@@ -52,20 +40,16 @@ def add_player(
         nickname: str,
         race: Race,
         guild: Guild
-) -> Player:
-    try:
-        obj = Player.objects.get(nickname=player.get("name"))
-        return obj
-    except Player.DoesNotExist:
-        obj = Player.objects.create(
-            nickname=nickname,
-            email=player.get("email"),
-            bio=player.get("bio"),
-            race=race,
-            guild=guild
+) -> None:
+    obj, _ = Player.objects.get_or_create(
+        nickname=nickname,
+        email=player.get("email"),
+        bio=player.get("bio"),
+        race=race,
+        guild=guild
 
-        )
-        obj.save()
+    )
+    obj.save()
 
 
 def main() -> None:
