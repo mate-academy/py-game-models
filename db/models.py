830cfb4f218460cd3615536f0381db667e4404d0
Rudge0 <maksim.pariychuk07@gmail.com>
2024-11-19 17:39:44 +0100

    Solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,29 @@
+from typing import ForwardRef
+
 from django.db import models
+from django.db.models import DO_NOTHING, CASCADE
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=CASCADE)
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True, )
+    description = models.TextField(null=True)
+
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=CASCADE)
+    guild = models.ForeignKey(Guild, on_delete=DO_NOTHING)
+    created_at = models.DateTimeField(auto_now=True)
--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import init_django_orm  # noqa: F401
+import json
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r") as file:
+        players = json.load(file)
+
+    for player_name, player_info in players.items():
+        print(player_info)
+        race, create = Race.objects.get_or_create(name=player_info["race"]["name"],
+                                   description=player_info["race"]["description"])
+
+
+        for skill in player_info["race"]["skills"]:
+            Skill.objects.get_or_create(name=skill["name"],
+                                            bonus=skill["bonus"],
+                                            race=race)
+        if player_info["guild"]:
+            Guild.objects.get_or_create(name=player_info["guild"]["name"],
+                                 description=player_info["guild"]["description"])
+
 
 
 if __name__ == "__main__":
