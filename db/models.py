e4ded6aa6fa6fd1ff023335008fbb9f77029eb8c
Yuriy Komar <omnigun85@gmail.com>
2024-11-23 23:57:55 +0200

    'Solution5'

--- db/models.py
+++ db/models.py
@@ -9,30 +9,18 @@ class Race(models.Model):
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(
-        Race,
-        on_delete=models.CASCADE
-    )
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
 
 
 class Guild(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    description = models.TextField(null=True, blank=True)
+    description = models.TextField(blank=True)
 
 
 class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255)
-    bio = models.CharField(max_length=255)
-    race = models.ForeignKey(
-        Race, on_delete=models.CASCADE,
-        related_name="races"
-    )
-    guild = models.ForeignKey(
-        Guild,
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        related_name="guilds"
-    )
+    bio = models.CharField(max_length=255, default="Nothing about me")
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
