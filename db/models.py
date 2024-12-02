41f1cbe5bce6b99d7f38738fb3fef0d9d1c05709
kkkkkkkkatya <vasyabarilyuk@gmail.com>
2024-12-02 21:40:36 +0200

    solution02

--- db/models.py
+++ db/models.py
@@ -9,7 +9,7 @@ class Race(models.Model):
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name='skill_set')
 
 
 class Guild(models.Model):
@@ -19,7 +19,7 @@ class Guild(models.Model):
 
 class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
-    email = models.EmailField(max_length=255, null=True, blank=True)
+    email = models.EmailField(max_length=255, null=False, blank=False)
     bio = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE,
                              related_name="players")
