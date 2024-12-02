9af86bda30fb1941ba0085eb26ef9bb52f626e17
Evgeniya <didovichevgeniya@gmail.com>
2024-12-02 10:41:50 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -7,7 +7,7 @@ class Race(models.Model):
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Rase, on_delete=models.CASCADE)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
 
 class  Guild(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -17,6 +17,6 @@ class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255)
-    race = models.ForeignKey(Rase, on_delete=models.CASCADE)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
     guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateField(auto_now_add=True)
--- main.py
+++ main.py
@@ -8,26 +8,28 @@ from db.models import Guild, Race, Skill, Player
 
 def main() -> None:
     with open("players.json", "r") as f:
-        players_data = json.load(f)
+        players_data=json.load(f)
     for player_data in players_data:
-        race,_ = Race.objects.get_or_create(name=player_data["race"]["name"],
-                                            defaults = {"description": player_data["race"].get("description", "")}
+        race,_=Race.objects.get_or_create(name=player_data["race"]["name"],
+                                            defaults={"description": player_data["race"].get("description", "")}
         )
 
 
         guild = None
         if "guild" in player_data and player_data["guild"]:
-            guild,_ = Guild.objects.get_or_create(
-                name = player_data["guild"]["name"],
-                defaults = {"description": player_data["guild"].get("description", "")}
+            guild,_=Guild.objects.get_or_create(
+                name=player_data["guild"]["name"],
+                defaults={"description": player_data["guild"].get("description", "")}
             )
 
         skills = []
         for skill_data in player_data.get("skills", []):
-            skill,_ = Skill.objects.get_or_create(name = skill_data["name"],defaults = {"bonus": skill_data["bonus"], "race": race})
+            skill,_=Skill.objects.get_or_create(name=skill_data["name"],
+                                                defaults={"bonus": skill_data["bonus"], "race": race})
             skills.append(skill)
 
-        Player.objects.get_or_create(nickname=player_data["nickname"],defaults={
+        Player.objects.get_or_create(nickname=player_data["nickname"],
+                                     defaults={
             "email": player_data["email"],
             "bio": player_data["bio"],
             "race": race,
