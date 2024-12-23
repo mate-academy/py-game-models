c961ad25db2c2b3635caa3f2b92d4bd678b6e16d
Ruslan Toister <rtoister.cv@gmail.com>
2024-12-23 16:06:47 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -1,31 +1,32 @@
 from django.db import models
 
+
 # Model for Race
 class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
-# Model for Skill
+
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name='skills')
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="skills")
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
-# Model for Guild
+
 class Guild(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
-# Model for Player
+
 class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255, null=False)
@@ -34,5 +35,6 @@ class Player(models.Model):
     guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
+
+    def __str__(self) -> str:
         return self.nickname
