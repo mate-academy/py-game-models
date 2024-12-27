b279ccefe7e9ba5b4931de05ed563a0d19813052
Danulo <danulo1337@gmail.com>
2024-12-27 15:06:49 +0200

    Fixed solution

--- main.py
+++ main.py
@@ -6,24 +6,33 @@ from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    with open('players.json', 'r') as f:
+    with open("players.json", "r") as f:
         data = json.load(f)
 
     for player_name, player_data in data.items():
         race_name = player_data["race"]["name"]
         race_description = player_data["race"]["description"]
-        race, _ = Race.objects.get_or_create(name=race_name, description=race_description)
+        race, _ = Race.objects.get_or_create(
+            name=race_name,
+            description=race_description
+        )
 
         for skill_data in player_data["race"]["skills"]:
             skill_name = skill_data["name"]
             skill_bonus = skill_data["bonus"]
-            Skill.objects.get_or_create(name=skill_name, bonus=skill_bonus, race=race)
+            Skill.objects.get_or_create(
+                name=skill_name,
+                bonus=skill_bonus, race=race
+            )
 
         guild = None
         if player_data["guild"]:
             guild_name = player_data["guild"]["name"]
             guild_description = player_data["guild"]["description"]
-            guild, _ = Guild.objects.get_or_create(name=guild_name, description=guild_description)
+            guild, _ = Guild.objects.get_or_create(
+                name=guild_name,
+                description=guild_description
+            )
 
         Player.objects.get_or_create(
             nickname=player_name,
@@ -32,6 +41,7 @@ def main() -> None:
             race=race,
             guild=guild
         )
+
 
 if __name__ == "__main__":
     Player.objects.all().delete()
