12d651f3bb920611171d394d3a256fa57be6f45b
Maksym Kryvsha <KryivshaMaxim@gmail.com>
2025-01-06 22:33:41 +0200

    solution

--- db/models.py
+++ db/models.py
@@ -2,14 +2,15 @@ from django.db import models
 
 
 class Race(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
 
 
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE,
+                             related_name="skill_set")
 
 
 class Guild(models.Model):
@@ -25,4 +26,4 @@ class Player(models.Model):
                              related_name="players")
     guild = models.ForeignKey(Guild, null=True, on_delete=models.SET_NULL,
                               related_name="players")
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
