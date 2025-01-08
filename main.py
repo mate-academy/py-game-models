8e824950664ad9c83ce4ee5712da6e06ed7a714f
Oleksandr Barabanov <imtence@icloud.com>
2025-01-08 22:23:03 +0200

    Solution

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
 import init_django_orm  # noqa: F401
-
+from django.utils.timezone import now
 from db.models import Race, Skill, Player, Guild
+import json
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    for nickname, player_data in data.items():
+        race_data = player_data["race"]
+        race, _ = Race.objects.get_or_create(
+            name=race_data["name"],
+            defaults={"description": race_data.get("description", "")}
+        )
+
+        for skill_data in race_data["skills"]:
+            Skill.objects.get_or_create(
+                name=skill_data["name"],
+                defaults={"bonus": skill_data["bonus"], "race": race}
+            )
+
+        guild = None
+        guild_data = player_data.get("guild")
+        if guild_data:
+            guild, _ = Guild.objects.get_or_create(
+                name=guild_data["name"],
+                defaults={"description": guild_data.get("description")}
+            )
+
+        Player.objects.create(
+            nickname=nickname,
+            email=player_data["email"],
+            bio=player_data["bio"],
+            race=race,
+            guild=guild,
+            created_at=now()
+        )
 
 
 if __name__ == "__main__":
