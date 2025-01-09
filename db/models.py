e4583361acdd083d29eb0dc95e9dd5aeafee43a5
Bogdan Gavriluk <bogdangavriluk12345@gmail.com>
2025-01-09 17:00:26 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,34 @@
 from django.db import models
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(max_length=255)
+    race = models.ForeignKey(
+        Race, on_delete=models.CASCADE, related_name="skill_set"
+    )
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField(
+        max_length=255, blank=True, null=True
+    )
+    race = models.ForeignKey(
+        Race, on_delete=models.CASCADE, related_name="players"
+    )
+    guild = models.ForeignKey(
+        Guild, on_delete=models.SET_NULL, null=True, related_name="members"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
--- main.py
+++ main.py
@@ -1,10 +1,40 @@
+import json
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json") as file:
+        players = json.load(file)
+
+    for nickname, data in players.items():
+        race, _ = Race.objects.get_or_create(
+            name=data.get("race").get("name"),
+            description=data.get("race").get("description")
+        )
+
+        for skill in data.get("race").get("skills"):
+            Skill.objects.get_or_create(
+                name=skill.get("name"),
+                bonus=skill.get("bonus"),
+                race=race
+            )
+
+        guild = data["guild"] if data["guild"] else None
+        if guild:
+            guild, _ = Guild.objects.get_or_create(
+                name=data.get("guild").get("name"),
+                description=data.get("guild").get("description", "")
+            )
+
+        Player.objects.get_or_create(
+            nickname=nickname,
+            email=data.get("email"),
+            bio=data.get("bio"),
+            race=race,
+            guild=guild
+        )
 
 
 if __name__ == "__main__":
