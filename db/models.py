e930ff229be952efce69b5b39f4dd1b6156e8d91
Aleksey Pastukhov <mindflux99@gmail.com>
2025-01-09 17:40:08 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,29 @@
 from django.db import models
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+
+class Skill(models.Model):
+    name = models.CharField(unique=True, max_length=255)
+    bonus = models.TextField(blank=True)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+
+
+class Guild(models.Model):
+    name = models.CharField(unique=True, max_length=255)
+    description = models.TextField(null=True)
+
+
+class Player(models.Model):
+    nickname = models.CharField(unique=True, max_length=255)
+    email = models.EmailField(max_length=255)
+    bio = models.TextField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, null=True)
+    guild = models.ForeignKey(Guild, on_delete=models.DO_NOTHING, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nickname
--- main.py
+++ main.py
@@ -1,10 +1,49 @@
+import json
+
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("db/data/races.json") as f:
+        players_data = json.load(f)
+
+    for player_data in players_data:
+        race, _ = Race.objects.get_or_create(
+            name=player_data["race"]["name"],
+            defaults={"description": player_data["race"].get("description", "")}
+        )
+
+        guild = None
+        if player_data.get("guild"):
+            guild, _ = Guild.objects.get_or_create(
+                name=player_data["guild"]["name"],
+                defaults={"description": player_data["guild"].get("description", None)}
+            )
+
+        skill_objects = []
+        for skill_data in player_data.get("skills", []):
+            skill, _ = Skill.objects.get_or_create(
+                name=skill_data["name"],
+                defaults={
+                    "bonus": skill_data.get("bonus", ""),
+                    "race": race,
+                }
+            )
+            skill_objects.append(skill)
+
+        player, created = Player.objects.get_or_create(
+            nickname=player_data["nickname"],
+            defaults={
+                "email": player_data["email"],
+                "bio": player_data.get("bio", ""),
+                "race": race,
+                "guild": guild,
+            }
+        )
+
+    print("Database population complete!")
 
 
 if __name__ == "__main__":
