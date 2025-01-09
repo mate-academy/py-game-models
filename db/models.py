82adee424c94b6e177e06e43e846cfd80e438ada
Arsenii Aristov <arseniyaristov07@gmail.com>
2025-01-09 13:48:21 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,38 @@
 from django.db import models
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+    bonus = models.TextField(blank=True)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return  f"{self.name} ({self.race.name})"
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=100)
+    email = models.EmailField()
+    bio = models.TextField(blank=True)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nickname
--- main.py
+++ main.py
@@ -1,11 +1,44 @@
+import json
+
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
-def main() -> None:
-    pass
+def main():
+    with open('players.json', 'r') as file:
+        data = json.load(file)
 
+    for player_data in data:
+        # Process Race
+        race_name = player_data["race"]["name"]
+        race_description = player_data["race"].get("description", "")
+        race, _ = Race.objects.get_or_create(name=race_name, defaults={"description": race_description})
 
-if __name__ == "__main__":
-    main()
+        # Process Skills
+        skills = player_data["race"].get("skills", [])
+        for skill_data in skills:
+            Skill.objects.get_or_create(
+                name=skill_data["name"],
+                defaults={"bonus": skill_data["bonus"], "race": race}
+            )
+
+        # Process Guild
+        guild_name = player_data.get("guild", {}).get("name")
+        guild_description = player_data.get("guild", {}).get("description", None)
+        guild = None
+        if guild_name:
+            guild, _ = Guild.objects.get_or_create(
+                name=guild_name, defaults={"description": guild_description}
+            )
+
+        # Process Player
+        Player.objects.get_or_create(
+            nickname=player_data["nickname"],
+            defaults={
+                "email": player_data["email"],
+                "bio": player_data["bio"],
+                "race": race,
+                "guild": guild,
+            },
+        )
