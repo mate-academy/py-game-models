2ba28c0f28d34ba8c6ebf61b14fb4ee962226897
Bohdan Khudiakov <b.khudiakov@gmail.com>
2025-01-09 19:23:31 +0200

    Solution

--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "NAME": os.path.join(BASE_DIR, "db.sqlite"),
     }
 }
 
