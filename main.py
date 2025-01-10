831c88b61b2831a6377485e12129636307a836d2
Lisa <zhyganiuk.elizaveta@gmail.com>
2025-01-11 01:50:35 +0200

    solution_update_flake8

--- main.py
+++ main.py
@@ -11,29 +11,30 @@ def main() -> None:
 
     for player_data in data:
         race, _ = Race.objects.get_or_create(
-            name=player_data['race']['name'],
-            description=player_data['race'].get('description', '')
+            name=player_data["race"]["name"],
+            description=player_data["race"].get("description", "")
         )
 
-        for skill_data in player_data['race']['skills']:
+        for skill_data in player_data["race"]["skills"]:
             Skill.objects.get_or_create(
-                name=skill_data['name'],
-                bonus=skill_data['bonus'],
+                name=skill_data["name"],
+                bonus=skill_data["bonus"],
                 race=race
             )
 
         guild, _ = Guild.objects.get_or_create(
-            name=player_data['guild']['name'],
-            description=player_data['guild'].get('description', None)
+            name=player_data["guild"]["name"],
+            description=player_data["guild"].get("description", None)
         )
 
         Player.objects.get_or_create(
-            nickname=player_data['nickname'],
-            email=player_data['email'],
-            bio=player_data['bio'],
+            nickname=player_data["nickname"],
+            email=player_data["email"],
+            bio=player_data["bio"],
             race=race,
             guild=guild,
         )
+
 
 if __name__ == "__main__":
     main()
