4012e61aaaebc193ed442413d352fa3c16a3d91d
Sergii Hrytchyn <ichbinjagerrr@gmail.com>
2025-01-22 04:12:44 +0200

    implement py-game-models

--- db/models.py
+++ db/models.py
@@ -35,4 +35,3 @@ class Player(models.Model):
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
     guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(null=True, auto_now_add=True)
-
