371415910a32174da8ef93573524e0959e619d5b
Dell <gyrkalomas@gmail.com>
2025-01-31 18:17:59 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,26 @@
 from django.db import models
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField("Это описание бонуса который игроки могут получить.", max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField("Хранит описание пользователя про себя", max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    guild = models.ForeignKey(Guild, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 import init_django_orm  # noqa: F401
+import json
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r") as json_file:
+        data = json.load(json_file)
+
+    for dict_name in data:
+        Player.objects.get_or_create(
+            nickname = dict_name,
+            email = dict_name["email"],
+            bio = dict_name["bio"]
+        )
+        Race.objects.get_or_create(
+            name = dict_name["race"]["name"],
+            description = dict_name["race"]["description"]
+        )
+        for i in dict_name["race"]["skills"]:
+            Skill.objects.get_or_create(
+                name = i["name"],
+                bonus = i["bonus"]
+            )
+        Guild.objects.get_or_create(
+            name = dict_name["guild"]["name"],
+            description = dict_name["guild"]["description"]
+        )
 
 
 if __name__ == "__main__":
