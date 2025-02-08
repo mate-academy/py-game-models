ea885801eb17058120ff5a8d84f3a9d9b04204a2
shafran <shafran.andriy@gmail.com>
2025-02-08 20:51:59 +0200

    Created all models

--- main.py
+++ main.py
@@ -8,25 +8,25 @@ from db.models import Race, Skill, Player, Guild
 def main() -> None:
     with open("players.json", "r") as players_file:
         players = json.load(players_file)
-    for player in players:
+    for player_name, player_data in players.items():
         race = None
-        if players[player]["race"]:
+        if player_data["race"]:
             race, _ = (Race.objects.get_or_create(
-                name=players[player]["race"]["name"],
-                description=players[player]["race"]["description"],))
+                name=player_data["race"]["name"],
+                description=player_data["race"]["description"],))
         guild = None
-        if players[player]["guild"]:
+        if player_data["guild"]:
             guild, _ = (Guild.objects.get_or_create(
-                name=players[player]["guild"]["name"],
-                description=players[player]["guild"]["description"], ))
-        for skill in players[player]["race"]["skills"]:
-            if players[player]["race"]["skills"]:
+                name=player_data["guild"]["name"],
+                description=player_data["guild"]["description"], ))
+        for skill in player_data["race"]["skills"]:
+            if player_data["race"]["skills"]:
                 Skill.objects.get_or_create(name=skill["name"],
                                             bonus=skill["bonus"],
                                             race=race)
-        Player.objects.get_or_create(nickname=player,
-                                     email=players[player]["email"],
-                                     bio=players[player]["bio"],
+        Player.objects.get_or_create(nickname=player_name,
+                                     email=player_data["email"],
+                                     bio=player_data["bio"],
                                      race=race,
                                      guild=guild,)
 
