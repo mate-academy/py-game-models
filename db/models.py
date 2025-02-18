710b6c828370f1597f0a641e1002fb24f8a17229
Sviatoslav Zabegin <zabegins777@gmail.com>
2025-02-18 10:11:30 +0100

    'Solution'

--- db/models.py
+++ db/models.py
@@ -1,33 +1,34 @@
 from django.db import models
-from django.db.models.fields import TextField
 
 
 class Race(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    RACE_CHOICES = [
+        ("elf", "Elf"),
+        ("dwarf", "Dwarf"),
+        ("human", "Human"),
+        ("ork", "Ork")
+    ]
+    name = models.CharField(max_length=255,
+                            unique=True,
+                            choices=RACE_CHOICES)
     description = models.TextField(blank=True)
 
 
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="skills")
 
 
 class Guild(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    description = TextField(null=True)
+    description = models.TextField(null=True)
 
 
 class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
-    email = models.EmailField(max_length=255, unique=False)
+    email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE)
-    guild = models.ForeignKey(
-        Guild,
-        related_name="players",
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True
-    )
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="players")
+    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- main.py
+++ main.py
@@ -1,42 +1,42 @@
-import init_django_orm  # noqa: F401
 import json
-
+import init_django_orm  # noqa: F401
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    Race.objects.all().delete()
-    Skill.objects.all().delete()
-    Guild.objects.all().delete()
-    Player.objects.all().delete()
-    with open("players.json", "r") as f:
-        json_data = json.load(f)
-    players = json_data.items()
-    for i, (nickname, details) in enumerate(players):
+    with open("players.json", "r") as file:
+        players = json.load(file)
+
+    for nickname, data in players.items():
+        race_data = data["race"]
         race, _ = Race.objects.get_or_create(
-            name=details["race"]["name"],
-            defaults={"description": details["race"]["description"]}
+            name=race_data["name"],
+            defaults={"description": race_data["description"]}
         )
-        guild = None
-        if details.get("guild"):
-            guild, _ = Guild.objects.get_or_create(
-                name=details["guild"]["name"],
-                defaults={"description": details["guild"]["description"]}
-            )
-        Player.objects.create(
-            nickname=nickname,
-            email=details["email"],
-            bio=details["bio"],
-            race=race,
-            guild=guild
-        )
-        skills = details["race"].get("skills", [])
-        for skill in skills:
+
+        for skill_data in race_data["skills"]:
             Skill.objects.get_or_create(
-                name=skill["name"],
-                bonus=skill["bonus"],
-                race=race
+                name=skill_data["name"],
+                defaults={"bonus": skill_data["bonus"], "race": race}
             )
+
+        guild = None
+        guild_data = data.get("guild")
+        if guild_data:
+            guild, _ = Guild.objects.get_or_create(
+                name=guild_data["name"],
+                defaults={"description": guild_data["description"]}
+            )
+
+        Player.objects.get_or_create(
+            nickname=nickname,
+            defaults={
+                "email": data["email"],
+                "bio": data["bio"],
+                "race": race,
+                "guild": guild
+            }
+        )
 
 
 if __name__ == "__main__":
