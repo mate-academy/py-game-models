fcdf2f27e53784bff5b1286ecf883ff84ea75d3b
Yasha Lapin <yasha.lapin@gmail.com>
2025-02-18 11:38:35 +0200

    GameModels

--- main.py
+++ main.py
@@ -1,10 +1,56 @@
 import init_django_orm  # noqa: F401
-
+import json
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r") as file:
+        players = json.load(file)
+
+    for nickname, player in players.items():
+        player["nickname"] = nickname
+        print(f"Player structure: {player}")
+
+        if not isinstance(player.get("race"), dict):
+            print(f"Error: race is not a dictionary: {player["race"]}")
+            continue
+
+        race, _ = Race.objects.get_or_create(
+            name=player["race"]["name"],
+            defaults={"description": player["race"].get("description", "")}
+        )
+
+        for skill in player["race"].get("skills", []):
+            Skill.objects.get_or_create(
+                name=skill["name"],
+                defaults={
+                    "bonus": skill.get("bonus", ""),
+                    "race": race
+                }
+            )
+
+        guild_name = player.get("guild")["name"] \
+            if player.get("guild") and isinstance(player.get("guild"), dict) \
+            else None
+        if guild_name:
+            guild_description = player["guild"].get("description")
+            guild, _ = Guild.objects.get_or_create(
+                name=guild_name,
+                defaults={"description": guild_description or ""}
+            )
+        else:
+            guild = None
+
+        # Создаем игрока
+        Player.objects.get_or_create(
+            nickname=player["nickname"],
+            defaults={
+                "email": player.get("email", ""),
+                "bio": player.get("bio", ""),
+                "race": race,
+                "guild": guild
+            }
+        )
 
 
 if __name__ == "__main__":
