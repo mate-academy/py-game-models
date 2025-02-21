417aacbf3c2cad4fa8dc4443133060efc2cff2ef
Marek Smolana <msmolana@gmail.com>
2025-02-21 16:10:13 +0100

    draft

--- db/models.py
+++ db/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Race(models.Model):
-    id = models.AutoField(primary_key=True)
+    race_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
 
@@ -14,7 +14,7 @@ class Skill(models.Model):
 
 
 class Guild(models.Model):
-    id = models.AutoField(primary_key=True)
+    guild_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, null=False, blank=False)
     description = models.TextField(null=True)
 
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import init_django_orm  # noqa: F401
 from db.models import Race, Skill, Player, Guild
 
 
-def get_or_create_race(name) -> Race:
+def get_or_create_race(name: str) -> Race:
     try:
         race = Race.objects.get(name=name)
     except Race.DoesNotExist:
@@ -12,7 +12,8 @@ def get_or_create_race(name) -> Race:
         race.save()
     return race
 
-def get_or_create_guild(name) -> Guild:
+
+def get_or_create_guild(name: str) -> Guild:
     try:
         guild = Guild.objects.get(name=name)
     except Guild.DoesNotExist:
@@ -21,21 +22,22 @@ def get_or_create_guild(name) -> Guild:
     return guild
 
 
-def main(filo=None) -> None:
+def main() -> None:
     players_data = json.load(open("players.json", "r"))
 
     elf_race = Race.objects.create(name="elf", description="The magic race")
     Skill.objects.create(
         name="Teleportation",
-        bonus="The ability to move so fast they look like they're teleporting. Could be considered to technically be Teleportation.",
+        bonus="The ability to move so fast they look like they're "
+              "teleporting. Could be considered to technically be "
+              "Teleportation.",
         race_id=elf_race.id)
     Skill.objects.create(
         name="Reality warping",
-        bonus="The ability to Warp Reality. Make the impossible become possible but can't warp anything containing the structure that holds everything together (Which are many creatures.)",
+        bonus="The ability to Warp Reality. Make the impossible become"
+              " possible but can't warp anything containing the structure"
+              " that holds everything together (Which are many creatures.)",
         race_id=elf_race.id)
-
-    # Guild.objects.create(name="mags", description="A community of the elf mags")
-    # Guild.objects.create(name="blacksmiths", description="A community of the blacksmiths")
 
     for player_key in players_data.keys():
         player = players_data[player_key]
@@ -44,7 +46,6 @@ def main(filo=None) -> None:
                               bio=player["bio"],
                               race=get_or_create_race(player["race"]),
                               guild=get_or_create_guild(player["guild"]))
-
 
 
 if __name__ == "__main__":
