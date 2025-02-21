93d45ac5de3fb6090f1d069f15b6ba0965948ab7
Marek Smolana <msmolana@gmail.com>
2025-02-21 16:00:11 +0100

    draft

--- main.py
+++ main.py
@@ -1,10 +1,50 @@
+import json
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
-def main() -> None:
-    pass
+def get_or_create_race(name) -> Race:
+    try:
+        race = Race.objects.get(name=name)
+    except Race.DoesNotExist:
+        race = Race.objects.create(name=name)
+        race.save()
+    return race
+
+def get_or_create_guild(name) -> Guild:
+    try:
+        guild = Guild.objects.get(name=name)
+    except Guild.DoesNotExist:
+        guild = Guild.objects.create(name=name)
+        guild.save()
+    return guild
+
+
+def main(filo=None) -> None:
+    players_data = json.load(open("players.json", "r"))
+
+    elf_race = Race.objects.create(name="elf", description="The magic race")
+    Skill.objects.create(
+        name="Teleportation",
+        bonus="The ability to move so fast they look like they're teleporting. Could be considered to technically be Teleportation.",
+        race_id=elf_race.id)
+    Skill.objects.create(
+        name="Reality warping",
+        bonus="The ability to Warp Reality. Make the impossible become possible but can't warp anything containing the structure that holds everything together (Which are many creatures.)",
+        race_id=elf_race.id)
+
+    # Guild.objects.create(name="mags", description="A community of the elf mags")
+    # Guild.objects.create(name="blacksmiths", description="A community of the blacksmiths")
+
+    for player_key in players_data.keys():
+        player = players_data[player_key]
+        Player.objects.create(nickname=player_key,
+                              email=player["email"],
+                              bio=player["bio"],
+                              race=get_or_create_race(player["race"]),
+                              guild=get_or_create_guild(player["guild"]))
+
 
 
 if __name__ == "__main__":
