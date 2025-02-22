3a94ccd0467e6a071765410fe96bf38fb3c96377
Marek Smolana <msmolana@gmail.com>
2025-02-22 05:24:39 +0100

    solution

--- main.py
+++ main.py
@@ -5,52 +5,31 @@ import init_django_orm  # noqa: F401
 from db.models import Race, Skill, Player, Guild
 
 
-def get_or_create_race(name: str) -> Race:
-    try:
-        race = Race.objects.get(name=name)
-    except Race.DoesNotExist:
-        race = Race.objects.create(name=name)
-        race.save()
-    return race
-
-
-def get_or_create_guild(guild: dict) -> Guild:
-    try:
-        guild = Guild.objects.get(name=guild["name"])
-    except Guild.DoesNotExist:
-        guild = Guild.objects.create(name=guild["name"])
-        guild.save()
-    return guild
-
-
 def main() -> None:
     with open("players.json", "r") as file:
         players_data = json.load(file)
 
-    elf_race = Race.objects.create(name="elf", description="The magic race")
-    Skill.objects.create(
-        name="Teleportation",
-        bonus="The ability to move so fast they look like they're "
-              "teleporting. Could be considered to technically be "
-              "Teleportation.",
-        race=elf_race)
-    Skill.objects.create(
-        name="Reality warping",
-        bonus="The ability to Warp Reality. Make the impossible become"
-              " possible but can't warp anything containing the structure"
-              " that holds everything together (Which are many creatures.)",
-        race=elf_race)
+    for name, player in players_data.items():
+        guild = None
+        if player["guild"] is not None:
+            guild = Guild.objects.get_or_create(
+                name=player["guild"]["name"],
+                description=player["guild"]["description"])[0]
+        race = Race.objects.get_or_create(
+            name=player["race"]["name"],
+            description=player["race"]["description"])[0]
 
-    for player_key in players_data.keys():
-        player = players_data[player_key]
-        Player.objects.create(nickname=player_key,
+        for skill_data in player["race"]["skills"]:
+            Skill.objects.get_or_create(
+                race=race,
+                name=skill_data["name"],
+                bonus=skill_data["bonus"])
+
+        Player.objects.create(nickname=name,
                               email=player["email"],
                               bio=player["bio"],
-                              race=get_or_create_race(player["race"])
-                              )
-        if player["guild"] is not None:
-            guild = get_or_create_guild(player["guild"])
-            Player.objects.filter(guild=guild.guild_id).update(guild=guild)
+                              race=race,
+                              guild=guild)
 
 
 if __name__ == "__main__":
