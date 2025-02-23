7bcbe5fddda7d095c79638b241d06aa96f50ab60
Volodymyr Vinohradov <kram3ko@gmail.com>
2025-02-23 19:35:34 +0200

    Solution

--- main.py
+++ main.py
@@ -1,10 +1,50 @@
-import init_django_orm  # noqa: F401
+import json
+from pathlib import Path
 
-from db.models import Race, Skill, Player, Guild
+import init_django_orm  # noqa: F401
+from db.models import Guild, Player, Race, Skill
+
+BASE_DIR = Path(__file__).parent
+FILE_NAME = BASE_DIR / "players.json"
+
+
+def make_race(race: dict) -> Race:
+    race_obj, _ = Race.objects.get_or_create(
+        name=race["name"],
+        defaults={"description": race.get("description", "")},
+    )
+    for skill in race.get("skills", []):
+        Skill.objects.get_or_create(
+            name=skill["name"],
+            defaults={"bonus": skill.get("bonus"), "race": race_obj},
+        )
+    return race_obj
+
+
+def make_guild(guild: dict) -> Guild | None:
+    if not guild:
+        return None
+    guild_obj, _ = Guild.objects.get_or_create(
+        name=guild["name"],
+        defaults={"description": guild.get("description", "")},
+    )
+    return guild_obj
 
 
 def main() -> None:
-    pass
+    with open(FILE_NAME) as file:
+        data: dict = json.load(file)
+
+    for name, players_data in data.items():
+        Player.objects.get_or_create(
+            nickname=name,
+            defaults={
+                "email": players_data["email"],
+                "bio": players_data["bio"],
+                "race": make_race(players_data["race"]),
+                "guild": make_guild(players_data["guild"]),
+            },
+        )
 
 
 if __name__ == "__main__":
--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
-import os
+from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = Path(__file__).parent
 
 # SECURITY WARNING: Modify this secret key if using in production!
 SECRET_KEY = "6few3nci_q_o@l1dlbk81%wcxe!*6r29yu629&d97!hiqat9fa"
@@ -11,7 +11,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "NAME": BASE_DIR / "game_db.sqlite3",
     }
 }
 
