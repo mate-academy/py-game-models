64a9ded262fd4b139631cdad87ccc474c925b2bd
Svitlana <svetapruta@gmail.com>
2025-02-27 15:24:23 +0100

    Solution

--- main.py
+++ main.py
@@ -1,10 +1,53 @@
+from django.db.models.fields import json
+
+import json
+
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
+from django.utils.timezone import now
+
 
 def main() -> None:
-    pass
+    Race.objects.all().delete()
+    Skill.objects.all().delete()
+    Player.objects.all().delete()
+    Guild.objects.all().delete()
+
+    with open("players.json") as file:
+        players = json.load(file)
+
+    for key, player in players.items():
+        # print(key, player["race"]["name"])
+        race, created = Race.objects.get_or_create(
+            name=player["race"]["name"],
+            defaults={"description": player["race"].get("description", "")}
+        )
+
+        if player["guild"]:
+            guild, created = Guild.objects.get_or_create(
+                name=player["guild"]["name"],
+                defaults={"description": player["guild"].get("description", "")}
+            )
+
+        if player["race"]["skills"]:
+            for skill in player["race"]["skills"]:
+
+                Skill.objects.get_or_create(
+                    name=skill["name"],
+                    bonus=skill["bonus"],
+                    race=race
+                )
+
+        Player.objects.create(
+            nickname=key,
+            email=player["email"],
+            bio=player["bio"],
+            race=race,
+            guild=guild if player["guild"] else None,
+            created_at=now()
+    )
 
 
 if __name__ == "__main__":
