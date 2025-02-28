7d63839e1dfeb6569745d39bf2db5e5a4f26fd2d
Liza Shulika <liza.shulika2009@gmail.com>
2025-02-28 14:25:42 +0200

    game1

--- db/models.py
+++ db/models.py
@@ -19,7 +19,7 @@ class Guild(models.Model):
 
 class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
-    email = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
     guild = models.ForeignKey(Guild, on_delete=models.DO_NOTHING)
--- main.py
+++ main.py
@@ -11,15 +11,13 @@ def main() -> None:
     for player in players.values():
         race, created = Race.objects.get_or_create(name=player["race"]["name"],
                                                    description=player["race"]["description"])
-        guild = None
-        if player.get("guild"):
-            guild, created = Guild.objects.get(name=player["guild"]["name"],
+        guild, created = Guild.objects.get(name=player["guild"]["name"],
                                                description=player["guild"]["description"])
 
-        play = Player.objects.create(nickname=player["nickname"], email=player["email"],
+        play, created = Player.objects.create(nickname=player["nickname"], email=player["email"],
                                      bio=player["bio"], race=race, guild=guild)
         for skill in player["race"]["skills"]:
-            Skill.objects.get_or_create(name=skill["name"], bonus=skill["bonus"],)
+            Skill.objects.get_or_create(name=skill["name"], race=race, bonus=skill["bonus"])
 
 
 if __name__ == "__main__":
