2edea36914cf4daafefe99a92642108ece3c7639
Liza Shulika <liza.shulika2009@gmail.com>
2025-03-01 14:04:59 +0200

    game2

--- main.py
+++ main.py
@@ -7,15 +7,18 @@ from db.models import Race, Skill, Player, Guild
 def main() -> None:
     with open("players.json", "r") as file:
         players = json.load(file)
-        print(players)
-    for player in players.values():
-        race, created = Race.objects.get_or_create(name=player["race"]["name"],
-                                                   description=player["race"]["description"])
-        guild, created = Guild.objects.get(name=player["guild"]["name"],
-                                               description=player["guild"]["description"])
 
-        play, created = Player.objects.create(nickname=player["nickname"], email=player["email"],
+    for nickname, player in players.items():
+        race, created = Race.objects.get_or_create(name=player["race"]["name"],
+                                                   defaults={"description": player["race"]["description"] or ""})
+
+        if player.get("guild"):
+            guild, created = Guild.objects.get_or_create(name=player["guild"]["name"],
+                                               defaults={"description": player["guild"]["description"] or ""})
+
+        play, created = Player.objects.get_or_create(nickname=nickname, email=player["email"],
                                      bio=player["bio"], race=race, guild=guild)
+
         for skill in player["race"]["skills"]:
             Skill.objects.get_or_create(name=skill["name"], race=race, bonus=skill["bonus"])
 
