3db2168dabb7b6107d11bd81057459a039cf64f6
Oleksandr Kurin <profysss8712@gmail.com>
2025-03-20 14:59:00 +0200

    Solution2

--- setup_django.py
+++ setup_django.py
@@ -2,9 +2,13 @@ import os
 import django
 
 
-def setup_django():
+def setup_django() -> None:
     """
     Настройка Django-окружения.
+
+    Returns:
+        None
     """
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                          "your_project_name.settings")
     django.setup()
