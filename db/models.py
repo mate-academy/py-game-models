d9b7cefe8647f7aba5c4cb9767074b4d30739caa
AndyStar <andy_star@ukr.net>
2025-03-21 18:52:18 +0200

    'Solution'

--- db/models.py
+++ db/models.py
@@ -15,7 +15,9 @@ class Skill(models.Model):
                              blank=True,
                              null=True,
                              default=None)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    race = models.ForeignKey(Race,
+                             on_delete=models.CASCADE,
+                             related_name="skill")
 
 
 class Guild(models.Model):
@@ -27,8 +29,13 @@ class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255, blank=True)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE,
+                             related_name="player_race")
     guild = models.ForeignKey(Guild, on_delete=models.SET_NULL,
                               null=True,
-                              default=None)
+                              default=None,
+                              related_name="player_guild")
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.nickname
