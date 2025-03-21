bc2e871b689d4a69dfdaff061b816feaa50b7ebe
Liubomyr Levkovych <liubomyrlevkovich@gmail.com>
2025-03-21 17:21:36 +0200

    Solution

--- main.py
+++ main.py
@@ -6,39 +6,55 @@ from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    global guild_data
-    with open("players.json", "r") as file:
+    with open("players.json") as file:
         data = json.load(file)
 
-    for person, info in data.items():
-        guild_data = info.get("guild")
+    for player_str, player_json in data.items():
+        guild_json = player_json.get("guild")
+        if guild_json:
+            description = guild_json.get("description")
+            if not description:
+                guild_obj, _ = (
+                    Guild.objects.get_or_create(name=guild_json.get("name")))
+            else:
+                guild_obj, _ = (
+                    Guild.objects.get_or_create(name=guild_json.get("name"),
+                                                description=description))
 
-    guild = None
-    if guild:
-        guild, _ = Guild.objects.get_or_create(
-            name=guild_data["name"],
-            description=guild_data["description"],
-        )
+        race_js = player_json.get("race")
+        if race_js:
+            description = race_js.get("description")
+            if not description:
+                description = ""
+            race_obj, _ = Race.objects.get_or_create(name=race_js.get("name"),
+                                                     description=description)
+            skills_json = player_json.get("race").get("skills")
+            fill_skills(skills_json, race_obj)
 
-    race, _ = Race.objects.get_or_create(
-        name=info["race"]["name"],
-        description=info["race"]["description"],
-    )
+        nickname = player_str
+        email = player_json.get("email")
+        bio = player_json.get("bio")
+        if guild_json:
+            Player.objects.get_or_create(nickname=nickname,
+                                         email=email, bio=bio,
+                                         race=race_obj,
+                                         guild=guild_obj)
+        else:
+            Player.objects.get_or_create(nickname=nickname,
+                                         email=email,
+                                         bio=bio,
+                                         race=race_obj)
 
-    for skill in info["race"]["skills"]:
-        Skill.objects.get_or_create(
-            name=skill["name"],
-            bonus=skill["bonus"],
-            race=race,
-        )
 
-    Player.objects.create(
-        nickname=person,
-        email=info["email"],
-        bio=info["bio"],
-        guild=guild,
-        race=race,
-    )
+def fill_skills(skills_json: list[dict], race_obj: Race) -> None:
+    for skill in skills_json:
+        bonus_prepared = skill.get("bonus")
+        # if not bonus_prepared:
+        #     bonus_prepared = ""
+        skill_obj, _ = Skill.objects.get_or_create(name=skill.get("name"),
+                                                   bonus=bonus_prepared,
+                                                   race=race_obj)
+
 
 
 if __name__ == "__main__":
