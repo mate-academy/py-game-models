c266068648476faff198e44d9e7574abe34f0c8d
Khrystyna Sen <anollina@ukr.net>
2025-03-21 21:05:51 +0200

    solution

--- main.py
+++ main.py
@@ -8,37 +8,37 @@ def main() -> None:
         players_data = json.load(file)
 
     for player_name, player_data in players_data.items():
-        race_name = player_data["race"]["name"]
-        race_description = player_data["race"]["description"]
-        race, _ = Race.objects.get_or_create(
-            name=race_name,
-            defaults={"description": race_description}
+        race_data = player_data["race"]
+        race, created = Race.objects.get_or_create(
+            name=race_data["name"],
+            defaults={"description": race_data["description"]}
         )
 
-        for skill_data in player_data["race"]["skills"]:
+        for skill_data in race_data["skills"]:
             Skill.objects.get_or_create(
                 name=skill_data["name"],
                 bonus=skill_data["bonus"],
                 race=race
             )
 
-            guild_data = player_data["guild"]
-            guild = None
-            if guild_data:
-                guild, _ = Guild.objects.get_or_create(
-                    name=guild_data["name"],
-                    defaults={"description": guild_data.get("description")}
-                )
-
-            Player.objects.get_or_create(
-                nickname=player_name,
-                defaults={
-                    "email": player_data["email"],
-                    "bio": player_data["bio"],
-                    "race": race,
-                    "guild": guild
-                }
+        guild_data = player_data.get("guild", None)
+        if guild_data:
+            guild, created = Guild.objects.get_or_create(
+                name=guild_data["name"],
+                defaults={"description": guild_data.get("description", "")}
             )
+        else:
+            guild = None
+
+        Player.objects.get_or_create(
+            nickname=player_name,
+            defaults={
+                "email": player_data["email"],
+                "bio": player_data["bio"],
+                "race": race,
+                "guild": guild
+            }
+        )
 
 
 if __name__ == "__main__":
