2e0700029c01fc932cf9559581dfe2f4120defea
Ngayka <ngayka@gmail.com>
2025-03-22 10:42:04 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,32 @@
 from django.db import models
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+    def __str__(self):
+        return f"{self.name} ({self.description})"
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(max_length=255, blank=True)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} ({self.race})"
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.description})"
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField(max_length=255, blank=True)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    guild = models.ForeignKey(Guild, on_delete=models.SET_DEFAULT, default=None)
+    created_at = models.DateTimeField(auto_now_add=True)
+
--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+import json
+
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
+    with open("players.json", "r", encoding="utf-8") as json_file:
+        players = json.load(json_file)
+
+    for player in players:
+        guild, _ = Guild.objects.get_or_create(name=player["guild"])
+        race, _ = Race.objects.get_or_create(name=player["race"])
+        skill, _ = Skill.objects.get_or_create(name=player["skill"])
+
+        player_obj, _ = Player.objects.get_or_create(nickname=player["nickname"],
+                                                     email=player["email"],
+                                                     bio=player["bio"],
+                                                     race=race,
+                                                     guild=guild
+                                                     )
     pass
 
 
