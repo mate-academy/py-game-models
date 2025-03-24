80721bfe4372ab1ea2f5b16624a347ecafcb3258
Liubomyr Levkovych <liubomyrlevkovich@gmail.com>
2025-03-24 13:49:46 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -8,11 +8,8 @@ class Race(models.Model):
 
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    bonus = models.CharField(max_length=255,
-                             blank=True,
-                             null=True,
-                             default=None)
-    race = models.ForeignKey(Race, related_name="race" ,on_delete=models.CASCADE)
+    bonus = models.CharField(max_length=255, blank=True, null=True, default=None)
+    race = models.ForeignKey(Race, related_name="skills", on_delete=models.CASCADE)  # Updated related_name
 
 
 class Guild(models.Model):
@@ -24,8 +21,6 @@ class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255, blank=True)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE)
-    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL,
-                              null=True,
-                              default=None)
+    race = models.ForeignKey(Race, related_name="players", on_delete=models.CASCADE)  # Updated related_name
+    guild = models.ForeignKey(Guild, related_name="players", on_delete=models.SET_NULL, null=True, default=None)  # Updated related_name
     created_at = models.DateTimeField(auto_now_add=True)
