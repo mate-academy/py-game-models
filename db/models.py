d873ed56609b335ddd4b3897818136c16e536ba2
Liubomyr Levkovych <liubomyrlevkovich@gmail.com>
2025-03-24 13:52:06 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -8,8 +8,15 @@ class Race(models.Model):
 
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    bonus = models.CharField(max_length=255, blank=True, null=True, default=None)
-    race = models.ForeignKey(Race, related_name="skills", on_delete=models.CASCADE)  # Updated related_name
+    bonus = models.CharField(max_length=255,
+                             blank=True,
+                             null=True,
+                             default=None
+                             )
+    race = models.ForeignKey(Race,
+                             related_name="skills",
+                             on_delete=models.CASCADE
+                             )
 
 
 class Guild(models.Model):
@@ -21,6 +28,13 @@ class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255, blank=True)
-    race = models.ForeignKey(Race, related_name="players", on_delete=models.CASCADE)  # Updated related_name
-    guild = models.ForeignKey(Guild, related_name="players", on_delete=models.SET_NULL, null=True, default=None)  # Updated related_name
+    race = models.ForeignKey(Race,
+                             related_name="players",
+                             on_delete=models.CASCADE
+                             )
+    guild = models.ForeignKey(Guild, related_name="players",
+                              on_delete=models.SET_NULL,
+                              null=True,
+                              default=None
+                              )
     created_at = models.DateTimeField(auto_now_add=True)
