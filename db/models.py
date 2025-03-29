4a5bcf774571686af1997d1196542991d1b12292
Roksolana Kalinska <woops359@gmail.com>
2025-03-29 17:56:56 +0200

    Solution corrected flake8

--- db/models.py
+++ db/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
@@ -8,7 +9,9 @@ class Race(models.Model):
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name='skills')
+    race = models.ForeignKey(Race,
+                             on_delete=models.CASCADE,
+                             related_name='skills')
 
 
 class Guild(models.Model):
@@ -20,6 +23,11 @@ class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name='players')
-    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, related_name='members', null=True)
+    race = models.ForeignKey(Race,
+                             on_delete=models.CASCADE,
+                             related_name='players')
+    guild = models.ForeignKey(Guild,
+                              on_delete=models.SET_NULL,
+                              related_name='members',
+                              null=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- main.py
+++ main.py
@@ -38,6 +38,5 @@ def main() -> None:
             )
 
 
-
 if __name__ == "__main__":
     main()
